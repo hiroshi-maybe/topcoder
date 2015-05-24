136be727e3efba571de65c6c671bfa83a93cce3a
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-05-24 11:31:56 -0700

    solve EightRooks

--- EightRooks/solve/cpp/EightRooks.cpp
+++ EightRooks/solve/cpp/EightRooks.cpp
@@ -2,9 +2,31 @@
 #include <string>
 using namespace std;
 
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
 class EightRooks {
 public:
     string isCorrect(vector<string> const &board) {
-        return "$";
+      int rows[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+      int cols[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+      REP(i, 8) {
+	string row = board[i];
+	REP(j, 8) {
+	  if (row[j]=='R') {
+	    rows[i] += 1;
+	    cols[j] += 1;
+	  }
+	}
+      }
+
+      for (int row : rows) {
+	if (row != 1) return "Incorrect";
+      }
+      for (int col : cols) {
+	if (col != 1) return "Incorrect";
+      }
+      return "Correct";
     }
 };
