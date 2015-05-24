bbda5170c77d37159f80f89320e2c22041f8e148
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-05-23 23:03:28 -0700

    add solution of FilipTheFrog

--- FilipTheFrog/solve/cpp/FilipTheFrog.cpp
+++ FilipTheFrog/solve/cpp/FilipTheFrog.cpp
@@ -1,11 +1,42 @@
+#include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
 using namespace std;
+
+//#define SORT(c) sort((c).begin(),(c).end())
+//bool compare(int i,int j){return (i>j);}
 
 class FilipTheFrog {
 public:
     int countReachableIslands(vector<int> const &positions,
                               int L) {
-        return 0;
+      int start = positions[0];
+      sort(positions.rbegin(), positions.rend());
+
+      int res = 1;
+      
+      // forward
+      int current = start;
+      for (int p : positions) {
+	if (p <= start) continue;
+	int dif = p - current;
+	if (dif > L) break;
+	++res;
+	current = p;
+      }
+
+      // backward
+      current = start;
+      for (int i=positions.size()-1; i>0; --i) {
+	int p = positions[i];
+	if (p >= start) continue;
+	int dif = current - p;
+	if (dif > L) break;
+	++res;
+	current = p;
+      }
+
+      return res;
     }
 };
