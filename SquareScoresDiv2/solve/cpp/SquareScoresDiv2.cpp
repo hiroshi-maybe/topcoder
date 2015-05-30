65213620e4688671ad6c37581b3a77cf88113f58
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-05-30 16:37:00 -0700

    solve SquareScoresDiv2

--- SquareScoresDiv2/solve/cpp/SquareScoresDiv2.cpp
+++ SquareScoresDiv2/solve/cpp/SquareScoresDiv2.cpp
@@ -2,9 +2,30 @@
 #include <string>
 using namespace std;
 
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+
+int match(string const &s, char c, int n) {
+  int count = 0;
+  string x = string(n, c);
+  REP(i, s.length()-n+1) {
+    if (s.substr(i).find(x)==0) count+=1;
+  }
+  return count;
+}
+
 class SquareScoresDiv2 {
 public:
     int getscore(string const &s) {
-        return 0;
+      int count=0;
+      for (char c = 'a'; c <= 'z'; c+=1) {
+	if (s.find(c)==std::string::npos) continue;
+	FOR(n, 1, s.length()+1) {
+	  count += match(s, c, n);
+	}
+      }
+
+      return count;
     }
 };
