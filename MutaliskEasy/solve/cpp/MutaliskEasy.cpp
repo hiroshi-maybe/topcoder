990af077a234eccbe4f7acc02d06a9cf9e8bcbe4
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-06-04 01:24:19 -0700

    solve MutaliskEasy by memoized recursion

--- MutaliskEasy/solve/cpp/MutaliskEasy.cpp
+++ MutaliskEasy/solve/cpp/MutaliskEasy.cpp
@@ -1,52 +1,55 @@
+#include <algorithm>
 #include <vector>
 #include <string>
 using namespace std;
 
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP(i,n)  FOR(i,0,n)
+#define SORT(a) a.sort(a.begin(), a.end());
 
-#define dump(x)  cerr << #x << " = " << (x) << endl;
-
-bool defeated(vector <int> const &x, vector <int> const &org) {
+bool defeated(vector<int> const &x) {
   REP(i, x.size()) {
-    if (x[i] < org[i]) return false;
+    if (x[i] > 0) return false;
   }
   return true;
 }
 
-vector<vector<vector<int>>> attack_pat = {
-  {{9}},
-  {{9,3},{3,9}},
-  {{9,3,1},{9,1,3},{1,3,9},{1,9,3},{3,1,9},{3,9,1}}
-};
+vector<vector<int>> attack_pat = {{9,3,1},{9,1,3},{1,3,9},{1,9,3},{3,1,9},{3,9,1}};
+
+int dp[61][61][61];
+
+vector<int> attack(vector<int> &current, vector<int> const &atk) {
+  REP(i, atk.size()) {
+    current[i] = max(current[i]-atk[i], 0);
+  }
+  return current;
+}
+
+int min_dp(vector<int> const &_current, vector<vector<int>> const &pat) {
+  int res = 60;
+  vector<int> current = _current;
+  sort(current.begin(), current.end());
+  int dp_v = dp[current[0]][current[1]][current[2]];
+  if (dp_v != 60) return dp_v;
+
+  REP(i, 6) {
+    vector<int> cur = current;
+    auto post_atk = attack(cur, pat[i]);
+    if (defeated(post_atk)) return 1;
+    int c = min_dp(post_atk, pat);
+    res = min(c, res);
+  }
+  return dp[current[0]][current[1]][current[2]] = res + 1;
+}
 
 class MutaliskEasy {
 public:
     int minimalAttacks(vector<int> const &x) {
-      auto size = x.size();
-      auto attacks = attack_pat[x.size()-1];
-      vector<vector<int>> history = attacks;
-
-      int c = 0;
-      while(1) {
-	dump(c);
-	vector<vector<int>> next_history;
-	REP(i, history.size()) {
-	  auto c_stat = history[i];
-	  if (defeated(c_stat, x)) return c+1;
-	  REP(j, attacks.size()) {
-	    auto attack = attacks[j];
-	    auto stat = c_stat;
-	    REP(k, attack.size()) {
-	      stat[k] += attack[k];
-	      //	    cerr << stat[k] << ',';
-	    }
-	    //	    	    cerr << endl;
-	    next_history.push_back(stat);
-	  }
-	}
-	history = next_history;
-	c+=1;
+      REP(i,61) REP(j,61) REP(k,61) dp[i][j][k] = 60;
+      vector<int> y = x;
+      while(y.size() < 3) {
+	y.push_back(0);
       }
+      return min_dp(y, attack_pat);
     }
 };
