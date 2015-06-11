574b2bbe31df70ac11196337555058170fbc1a76
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-06-11 01:42:33 -0700

    add macro for lower_bound() and upper_bound()

--- cpp-macro.h
+++ cpp-macro.h
@@ -13,6 +13,10 @@ using namespace std;
 #define EXIST(s,e) ((s).find(e)!=(s).end())
 #define SORT(c) sort((c).begin(),(c).end())
 
+// binary search
+#define LB(a,n,k) lower_bound(a,a+(n),(k))
+#define UB(a,n,k) upper_bound(a,a+(n),(k))
+
 // debug
 #define dump(x)  cerr << #x << " = " << (x) << endl;
 #define dumpAR(ar,n) REP(i,(n)) { cerr << ar[i] << ','; } cerr << endl;
