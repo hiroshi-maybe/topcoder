9b9e260ad016ea26813e4321bea9e125942b1bb1
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-08-09 16:50:13 -0700

    referred to the answer of RandomPancakeStackDiv2

--- RandomPancakeStackDiv2/solve/cpp/RandomPancakeStackDiv2.cpp
+++ RandomPancakeStackDiv2/solve/cpp/RandomPancakeStackDiv2.cpp
@@ -2,9 +2,30 @@
 #include <string>
 using namespace std;
 
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+
+double dp[100][100];
+
+double expect(int n, int j, vector<int> const &d) {
+  double res = d[j];
+  int size = d.size();
+  REP(i, j) {
+    res += (1.0 / (double)n) * expect(n-1, i, d);
+  }
+  return res;
+}
+
 class RandomPancakeStackDiv2 {
 public:
     double expectedDeliciousness(vector<int> const &d) {
-        return 0;
+      int n = d.size();
+      double res = 0.0;
+      REP(i, n) {
+	res += (1.0 / (double)n) * expect(n-1, i, d);
+      }
+      return res;
     }
 };
