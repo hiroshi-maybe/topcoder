6c8f57b3c468b122d42eedacffa2e99393c402b3
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-09-27 19:58:01 -0700

    brute force solution

--- ChessFloor/solve/cpp/ChessFloor.cpp
+++ ChessFloor/solve/cpp/ChessFloor.cpp
@@ -11,48 +11,36 @@ bool myfunction (pair<char, int> i,pair<char, int> j) { return (i.second < j.sec
 
 class ChessFloor {
 public:
+   /**
+     56 cases checked, 0 failures, 0 errors
+             Total time taken: 228 ms
+           Average time taken: 4 ms
+         Slowest running case: 10 ms (case 29)
+     56 cases checked, 0 failures, 0 errors
+             Total time taken: 539 ms
+           Average time taken: 9 ms
+         Slowest running case: 19 ms (case 14)
+     */
     int minimumChanges(vector<string> const &floor) {
-      map<char, int> charA;
-      map<char, int> charB;
-      REP(i,floor.size()) {
-	REP(j,floor[i].size()) {
-	  char curChar = floor[i][j];
-	  if ((!(i % 2) && !(j % 2)) || ((i % 2) && (j % 2))) {
-	    if (charA.find(curChar) == charA.end()) {
-	      charA[curChar] = 0;
+      int minCount = 401;
+      for(char c1='a'; c1<='z'; c1++) {
+	for(char c2='a'; c2<='z'; c2++) {
+	  if (c1==c2) continue;
+	  int changeCount = 0;
+	  REP(i,floor.size()) {
+	    REP(j,floor[i].size()) {
+	      char curChar = floor[i][j];
+	      if ((!(i % 2) && !(j % 2)) || ((i % 2) && (j % 2))) {
+		if (curChar != c1) changeCount++;
+	      } else {
+		if (curChar != c2) changeCount++;
+	      }
 	    }
-	    charA[curChar]++;
-	  } else {
-	    if (charB.find(curChar) == charB.end()) {
-	      charB[curChar] = 0;
-	    }
-	    charB[curChar]++;
 	  }
+	  minCount = min(minCount, changeCount);
 	}
       }
 
-      vector<pair<char, int> > pairsA(charA.begin(), charA.end());
-      vector<pair<char, int> > pairsB(charB.begin(), charB.end());
-      sort(pairsA.begin(), pairsA.end(), myfunction);
-      sort(pairsB.begin(), pairsB.end(), myfunction);
-
-      int floorCount = floor.size()*floor.size();
-      int countA = std::ceil(floorCount/2.0);
-      int countB = std::floor(floorCount/2.0);
-
-      auto maxA1 = pairsA[pairsA.size()-1];
-      auto maxA2 = pairsA.size() > 1 ? pairsA[pairsA.size()-2] : make_pair('x', 0);
-      
-      auto maxB1 = pairsB[pairsB.size()-1];
-      auto maxB2 = pairsB.size() > 1 ? pairsB[pairsB.size()-2] : make_pair('x', 0);
-
-      if (maxA1.first != maxB1.first) {
-	return countA - maxA1.second + countB - maxB1.second;
-      }
-      if ((countA - maxA1.second) > (countB - maxB1.second)) {
-	return countA - maxA2.second + countB - maxB1.second;
-      } else {
-	return countA - maxA1.second + countB - maxB2.second;
-      }
+      return minCount;
     }
 };
