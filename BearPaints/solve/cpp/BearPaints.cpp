f760ada71cbf0a553402c7eebfb9e803875df316
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-10-21 01:04:20 -0700

    solve BearPaints

--- BearPaints/solve/cpp/BearPaints.cpp
+++ BearPaints/solve/cpp/BearPaints.cpp
@@ -1,12 +1,28 @@
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <math.h>
 using namespace std;
+
+#define FOR(i,a,b) for(int64 i=(a);i>(b);--i)
+#define REP(i,n)  FOR(i,n,0)
 
 class BearPaints {
 public:
-    int64 maxArea(int W,
-                  int H,
+    int64 maxArea(int64 W,
+                  int64 H,
                   int64 M) {
-        return 0;
+      int64 res = 0;
+
+      if (W*H<=M) return W*H;
+      
+      REP(i,W) {
+	int64 j = (int64)floor(M/i);
+	int64 x = i*j;
+	if (x>res && j<=H) {
+	  res = x;
+	}
+      }      
+      return res;
     }
 };
