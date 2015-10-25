7294f086ab354d4bcc11cf0f784925f816ff97e2
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-10-24 18:06:56 -0700

    naiive bruteforce with TLE

--- BearDartsDiv2/solve/cpp/BearDartsDiv2.cpp
+++ BearDartsDiv2/solve/cpp/BearDartsDiv2.cpp
@@ -2,9 +2,25 @@
 #include <string>
 using namespace std;
 
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
 class BearDartsDiv2 {
 public:
     int64 count(vector<int> const &w) {
-        return 0;
+      int64 res = 0; 
+      REP(ai,w.size()) {
+	int64 a = w[ai];
+	FOR(bi,ai+1,w.size()) {
+	  int64 ab = a * w[bi];
+	  FOR(ci,bi+1,w.size()) {
+	    int64 abc = ab * w[ci];
+	    FOR(di,ci+1,w.size()) {
+	      if (w[di]==abc) ++res;
+	    }
+	  }
+	}
+      }
+      return res;
     }
 };
