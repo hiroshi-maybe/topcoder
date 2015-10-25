9285806a18869a6972eab99207b9eac45b00ea49
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-10-24 18:19:25 -0700

    3.5 mins solution
        Total time taken: 212469 ms
        Average time taken: 2213 ms
        Slowest running case: 7440 ms (case 79)

--- BearDartsDiv2/solve/cpp/BearDartsDiv2.cpp
+++ BearDartsDiv2/solve/cpp/BearDartsDiv2.cpp
@@ -2,21 +2,23 @@
 #include <string>
 using namespace std;
 
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n)  FOR(i,0,n)
+#define FOR(i,a,b) for(int i=(a);i>=(b);--i)
+#define REP(i,n)  FOR(i,n,0)
 
 class BearDartsDiv2 {
 public:
     int64 count(vector<int> const &w) {
       int64 res = 0; 
-      REP(ai,w.size()) {
-	int64 a = w[ai];
-	FOR(bi,ai+1,w.size()) {
-	  int64 ab = a * w[bi];
-	  FOR(ci,bi+1,w.size()) {
-	    int64 abc = ab * w[ci];
-	    FOR(di,ci+1,w.size()) {
-	      if (w[di]==abc) ++res;
+      REP(di,w.size()-1) {
+	int64 d = w[di];
+	REP(ci,di-1) {
+	  if (w[ci] > d || d % w[ci] != 0) continue;
+	  int64 ab = (int64)(d / w[ci]);
+	  REP(bi,ci-1) {
+	    if (w[bi] > ab || ab % w[bi] != 0) continue;
+	    int64 a = (int64)(ab / w[bi]);
+	    REP(ai,bi-1) {
+	      if (w[ai]==a) ++res;
 	    }
 	  }
 	}
