d0a24b0723b01c3a17902a40a17e9c908e963662
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-10-24 17:44:53 -0700

    solve SetPartialOrder

--- SetPartialOrder/solve/cpp/SetPartialOrder.cpp
+++ SetPartialOrder/solve/cpp/SetPartialOrder.cpp
@@ -2,10 +2,30 @@
 #include <string>
 using namespace std;
 
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
 class SetPartialOrder {
 public:
     string compareSets(vector<int> const &a,
                        vector<int> const &b) {
-        return "$";
+      bool onlya = false;
+      bool onlyb = false;
+      int len = max(a.size(), b.size());
+      FOR(n,1,101) {
+	bool aHas = false;
+	bool bHas = false;
+	REP(i, len) {
+	  aHas = aHas || (i<a.size() && a[i]==n);
+	  bHas = bHas || (i<b.size() && b[i]==n);
+	}
+	if (aHas &&  bHas) { onlya = onlya||false; onlyb = onlyb||false; }
+	if (aHas && !bHas) { onlya = true; }
+	if (!aHas && bHas) { onlyb = true; }
+      }
+      if ( onlya && onlyb)  return "INCOMPARABLE";
+      if (!onlya && onlyb)  return "LESS";
+      if ( onlya && !onlyb) return "GREATER";
+      return "EQUAL";
     }
 };
