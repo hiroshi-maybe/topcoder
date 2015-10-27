a8a943a15c28f3eb700d33726fd63e556694017f
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-10-27 01:16:49 -0700

    solve Drbalance

--- Drbalance/solve/cpp/Drbalance.cpp
+++ Drbalance/solve/cpp/Drbalance.cpp
@@ -2,10 +2,40 @@
 #include <string>
 using namespace std;
 
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+
+int score(vector<int> const &pref) {
+  int res = 0;
+  REP(i, pref.size()) {
+    res += pref[i] < 0 ? 1 : 0;
+  }
+  return res;
+}
+
 class Drbalance {
 public:
-    int lesscng(string const &s,
+    int lesscng(string &s,
                 int k) {
-        return 0;
+      vector<int> pref(s.size());
+      REP(i, s.size()) {
+	int prev = i>0 ? pref[i-1] : 0;
+	pref[i] = s[i] == '-' ? prev - 1 : prev + 1;
+      }
+      int res = 0;
+      while (int x = score(pref) > k) {
+	REP(i, s.size()) {
+	  if (s[i] == '-') {
+	    s[i] = '+';
+	    FOR(j, i, pref.size()) {
+	      pref[j] += 2;
+	    }
+	    res += 1;
+	    break;
+	  }
+	}
+      }
+      
+      return res; 
     }
 };
