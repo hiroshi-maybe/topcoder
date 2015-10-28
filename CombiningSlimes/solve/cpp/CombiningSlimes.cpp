b4ded11b96da4223075fe8247ac30e09365f2f80
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-10-27 22:58:50 -0700

    solve CombiningSlimes

--- CombiningSlimes/solve/cpp/CombiningSlimes.cpp
+++ CombiningSlimes/solve/cpp/CombiningSlimes.cpp
@@ -1,10 +1,23 @@
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
+
+#define SORT(c) sort((c).begin(),(c).end())
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
 
 class CombiningSlimes {
 public:
-    int maxMascots(vector<int> const &a) {
-        return 0;
+    int maxMascots(vector<int> &a) {
+      SORT(a);
+
+      int res = 0;
+      int prevw = 0;
+      REP(i, a.size()) {
+	res += prevw * a[i];
+	prevw += a[i];
+      }
+      return res;
     }
 };
