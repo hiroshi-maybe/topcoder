c95487ef205369bb58fe75a4ad7cdaaad9d54c87
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-10-28 00:28:03 -0700

    solve IsItASquare with reference
    http://torus711.hatenablog.com/entry/2015/09/17/233727
    http://kmjp.hatenablog.jp/entry/2015/09/17/1000
    https://topcoder.g.hatena.ne.jp/yambe2002/20150916/1442373452

--- IsItASquare/solve/cpp/IsItASquare.cpp
+++ IsItASquare/solve/cpp/IsItASquare.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP(i,n)  FOR(i,0,n)
+#define SORT(c) sort((c).begin(),(c).end())
 
-int line(int x1, int y1, int x2, int y2) {
+int dist(int x1, int y1, int x2, int y2) {
   return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2);
 }
 
@@ -14,25 +15,22 @@ class IsItASquare {
 public:
     string isSquare(vector<int> const &x,
                     vector<int> const &y) {
-      int orders[] = {0,1,2,3};
-      do {
-	int linelen = -1;
-	bool samelen = true;
-	REP(i, 4) {
-	  int order1 = orders[i];
-	  int order2 = i!=3 ? orders[i+1] : orders[0];
-	  if (linelen == -1) {
-	    linelen = line(x[order1],y[order1],x[order2],y[order2]);
-	    continue;
-	  }			 
-	  int len = line(x[order1],y[order1],x[order2],y[order2]);
-	  if (len != linelen) {
-	    samelen = false;
-	    break;
-	  }
-	}
-	if (samelen) return "It's a square";
-      } while ( std::next_permutation(orders,orders+4) );
-      return "Not a square";
+      vector<int> lines = {
+	dist(x[0],y[0],x[1],y[1]),
+	dist(x[1],y[1],x[2],y[2]),
+	dist(x[2],y[2],x[3],y[3]),
+	dist(x[3],y[3],x[0],y[0]),
+      	dist(x[0],y[0],x[2],y[2]),
+      	dist(x[1],y[1],x[3],y[3])};
+
+      SORT(lines);
+      if (lines[0]==lines[1]
+	  && lines[1]==lines[2]
+	  && lines[2]==lines[3]
+	  && lines[4]==lines[5]) {
+	return "It's a square";
+      } else {
+	return "Not a square";
+      }
     }
 };
