6ca0fdb18fc7fa2d1906df283847bdfc8d6b1f30
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-09-22 22:42:01 -0700

    solve triangle

--- TriangleMaking/solve/cpp/TriangleMaking.cpp
+++ TriangleMaking/solve/cpp/TriangleMaking.cpp
@@ -2,11 +2,20 @@
 #include <string>
 using namespace std;
 
+#define SORT(c) sort((c).begin(),(c).end())
+
 class TriangleMaking {
 public:
   int maxPerimeter(int a,
                    int b,
                    int c) {
-    return 0;
+    vector<int> ns = {a, b, c};
+    SORT(ns);
+    
+    while (ns[2] >= ns[0]+ns[1]) {
+      ns[2]-=1;
+    }
+
+    return ns[0]+ns[1]+ns[2];
   }
 };
