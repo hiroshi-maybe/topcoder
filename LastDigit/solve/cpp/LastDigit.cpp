6833e34f851b58f48da72a4e66a64753fe82366e
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-10-03 18:02:37 -0700

    solve the problem after reading problem analysis
    https://apps.topcoder.com/wiki/display/tc/SRM+699

--- LastDigit/solve/cpp/LastDigit.cpp
+++ LastDigit/solve/cpp/LastDigit.cpp
@@ -1,10 +1,50 @@
 #include <vector>
 #include <string>
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <set>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
 using namespace std;
+
+typedef long long ll;
+
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define FORR(x,arr) for(auto& x:arr)
 
 class LastDigit {
 public:
-  int64 findX(int64 S) {
-    return 0;
+  ll sumup(ll x) {
+    ll s = 0;
+    while (x > 0) {
+      s += x;
+      x /= 10;
+    }
+
+    return s;
+  }
+  
+  ll findX(ll S) {
+    ll s = 0;
+    ll e = 1000000000L * 1000000000;
+    ll mid = 0;
+
+    while (s<e) {
+      mid = (s + e) >> 1;
+      ll sum = sumup(mid);
+      if (sum==S) { return mid; }
+      if (sum<S) s = mid+1;
+      if (sum>S) e = mid;
+    }
+
+    return -1;    
   }
 };
