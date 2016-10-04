bdbb0148cc8b747dea4d9003103b0740a56f09d0
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-10-03 17:56:59 -0700

    solve UpDownHiking again

--- UpDownHiking/solve/cpp/UpDownHiking.cpp
+++ UpDownHiking/solve/cpp/UpDownHiking.cpp
@@ -1,12 +1,36 @@
 #include <vector>
 #include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <set>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
 using namespace std;
+
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define FORR(x,arr) for(auto& x:arr)
 
 class UpDownHiking {
 public:
   int maxHeight(int N,
                 int A,
                 int B) {
-    return 0;
+    int mh = 0;
+    REP(i, N) {
+      int ud = i;
+      int dd = N-i;
+
+      mh = max(mh, min(ud * A, dd * B));
+    }
+
+    return mh;
   }
 };
