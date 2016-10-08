4556142510b0c3c6c96656d1b43ffb0840194506
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-10-08 01:39:36 -0700

    solve Arrfix

--- Arrfix/solve/cpp/Arrfix.cpp
+++ Arrfix/solve/cpp/Arrfix.cpp
@@ -1,12 +1,78 @@
-#include <vector>
+#include <sstream>
 #include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <set>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
 using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define FORR(x,arr) for(auto& x:arr)
+#define EXIST(s,e) ((s).find(e)!=(s).end())
+
+// 0:40 start
+// 0:59 end
 
 class Arrfix {
 public:
-  int mindiff(vector<int> const &A,
+  int mindiff(vector<int> &A,
               vector<int> const &B,
               vector<int> const &F) {
-    return 0;
+    map<int,int> f;
+    FORR(n,F) {
+      if (EXIST(f, n)) {
+	f[n] += 1;
+      } else {
+	f[n] = 1;
+      }
+    }
+
+    int dif = 0;
+    int remainF = F.size();
+
+    vector<bool> reps;
+    REP(i,A.size()) {
+      reps.push_back(false);
+    }
+    REP(i,A.size()) {
+      int a = A[i];
+      int b = B[i];
+
+      if (a==b) { continue; }
+      if (EXIST(f, b) && f[b]>0) {
+	f[b]-=1;
+	remainF-=1;
+	A[i] = b;
+	reps[i] = true;
+      } else {
+	dif += 1;
+      }
+    }
+
+    REP(i,A.size()) {
+      int a = A[i];
+      int b = B[i];
+
+      if (!reps[i] && a==b && EXIST(f, b) && f[b]>0) {
+	f[b]-=1;
+	remainF-=1;
+	reps[i] = true;
+      }
+    }
+
+    return max(dif, remainF);
   }
 };
