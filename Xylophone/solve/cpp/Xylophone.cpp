ebbecc37d8eb210c56880ae207b0403abecee62d
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-10-14 00:39:10 -0700

    improve Xylophone reading top rated code

--- Xylophone/solve/cpp/Xylophone.cpp
+++ Xylophone/solve/cpp/Xylophone.cpp
@@ -21,26 +21,16 @@ typedef long long ll;
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
 #define REP(i,n)  FOR(i,0,n)
 #define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
 
 class Xylophone {
 public:
   int countKeys(vector<int> const &keys) {
-    vector<int> notes(7, 0);
-    REP(i, keys.size()) {
-      int k = keys[i];
-      notes[note(k)] = 1;
+    set<int> notes;
+    FORR(k, keys) {
+      notes.insert(k%7);
     }
 
-    int sum = 0;
-    REP(i, notes.size()) {
-      int n = notes[i];
-      if (n) { sum += 1; }
-    }
-
-    return sum;
-  }
-
-  int note(int key) {
-    return key % 7;
+    return SZ(notes);
   }
 };
