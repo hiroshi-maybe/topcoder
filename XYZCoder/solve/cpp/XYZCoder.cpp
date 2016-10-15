047f5e8a1b570b69c8281e7594a8bddc8c6950c0
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-10-15 13:53:46 -0700

    http://yu3mars.hatenablog.com/entry/2016/10/14/190000

--- XYZCoder/solve/cpp/XYZCoder.cpp
+++ XYZCoder/solve/cpp/XYZCoder.cpp
@@ -1,11 +1,62 @@
+#include <iostream>
+#include <algorithm> // max,min
 #include <vector>
 #include <string>
+#include <sstream>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <set>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+
 using namespace std;
+
+typedef long long ll;
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  FOR(i,0,n)
+#define FORR(x,arr) for(auto& x:arr)
+
+// dp
+#define DP(dp,i,j) ll dp[i][j]
+#define FILL(dp,x) memset(dp, x, sizeof(dp))
+
+// debug 1
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define dump2(x,y)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dumpAR(ar,n) REP(i,(n)) { cerr << ar[i] << ','; } cerr << endl;
+
+long mod = 1000000007;
+DP(dp,10001,101);
 
 class XYZCoder {
 public:
   int countWays(int room,
                 int size) {
-    return 0;
+    FILL(dp,0);
+
+    int n = room * size;
+    dp[0][0] = 1;
+    FOR(i,1,n+1) {
+      REP(j,room+1) {
+	if (j>0 && i>=j*size) {
+	  dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) % mod;
+	} else {
+	  dp[i][j] = dp[i-1][j];
+	}
+      }
+    }
+
+    ll ans = dp[n][room];
+    FOR(i,1,room+1) {
+      ans = (ans * i) % mod;
+    }
+
+    return (int)ans;
   }
 };
