bd8f4efeb9a2becc5482f66a6bc340055d2c0365
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-10-16 22:48:35 -0700

    macro update

--- cpp-macro.h
+++ cpp-macro.h
@@ -17,21 +17,25 @@
 using namespace std;
 
 // type alias
-typedef unsigned long ul;
-typedef long long ll;
+typedef unsigned long UL;
+typedef long long LL;
 typedef vector< int > VI;
 const int INF = 1e9;
 
 // repetition
 #define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n)  FOR(i,0,n)
+#define REP(i,n)  for(int i=0;i<(n);++i)
 #define FORR(x,arr) for(auto& x:arr)
-
-// container util
 #define SZ(a) int((a).size())
+#define REPS(i,arr)  for(int i=0; i<(arr).size(); ++i)
+
+// vector
 #define PUSH(s, e) (s).push_back(e)
+#define POP(s) (s).pop_back()
 #define REVERSE(c) reverse((c).begin(),(c).end())
 #define SORT(c) sort((c).begin(),(c).end())
+#define CONCAT(a,b) (a).insert((a).begin(), (b).begin(),(b).end())
+
 #define EXIST(s,e) ((s).find(e)!=(s).end())
 #define DEFAULT(s,e,x) if (!EXIST(s, e)) { s[e]=x; }
 #define ITER(i,s) for(auto i=s.begin(); i!=s.end(); i++)
@@ -40,8 +44,12 @@ const int INF = 1e9;
 typedef pair< int , int > PII;
 typedef vector<PII> VPII;
 #define MP make_pair
-#define FI first 
-#define SE second
+#define _1 first
+#define _2 second
+
+// priority queue
+#define MAX_PQ(T) priority_queue<T>
+#define MIN_PQ(T) priority_queue<T,vector<T>,greater<T>>
 
 // binary search
 #define LB(a,n,k) lower_bound(a,a+(n),(k))
@@ -50,11 +58,11 @@ typedef vector<PII> VPII;
 // debug cerr
 #define dump(x)  cerr << #x << " = " << (x) << endl;
 #define dump(x,y)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
-#define dumpAR(ar,n) REP(i,(n)) { cerr << ar[i] << ','; } cerr << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cerr << x << ','; } cerr << endl;
 // debug cout
 #define dump(x)  cout << #x << " = " << (x) << endl;
 #define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
-#define dumpAR(ar,n) REP(i,(n)) { cout << ar[i] << ','; } cout << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
 
 // dp
 #define DP(dp,i,j) int dp[i][j]
