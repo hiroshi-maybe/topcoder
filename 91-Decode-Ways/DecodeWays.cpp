9b92bf510a91d51550a184c95e36aa1906577c50
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-10-23 16:59:24 -0700

    add another solution of 91. Decode Ways

--- 91-Decode-Ways/DecodeWays.cpp
+++ 91-Decode-Ways/DecodeWays.cpp
@@ -57,4 +57,42 @@ public:
     //dumpAR(dp);
     return dp[L];
   }
+
+  int numDecodingsV2(string s) {
+    int L = SZ(s);
+    if (L==0) return 0;
+
+    VI dp(L, 0);
+
+    dp[0] = 1<=(s[0]-'0') && (s[0]-'0')<=9 ? 1 : 0;
+
+    FOR(i,1,L) {
+      int n = (s[i-1]-'0')*10 + (s[i]-'0');
+      if (n<1) {
+	// "00"
+	return 0;
+      }
+      if (n<10) {
+	dp[i] = dp[i-1];
+	continue;
+      }
+      if (n==10 || n==20) {
+	dp[i] = i==1 ? 1 : dp[i-2];
+	continue;
+      }
+      if (11<=n && n<=26) {
+	dp[i] = dp[i-1] + (i==1 ? 1 : dp[i-2]);
+	continue;
+      }
+
+      // 30,40...
+      if (n % 10==0) return 0;
+
+      if (27<=n) {
+	dp[i] = dp[i-1];
+      }
+    }
+    //dumpAR(dp);
+    return dp[L-1];
+  }
 };
