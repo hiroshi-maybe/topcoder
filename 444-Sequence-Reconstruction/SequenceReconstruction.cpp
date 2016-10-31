e863592495979d81baab7a383892ce0bc27cadd3
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-10-31 00:09:52 -0700

    add graph aproach in 444. Sequence Reconstruction

--- 444-Sequence-Reconstruction/SequenceReconstruction.cpp
+++ 444-Sequence-Reconstruction/SequenceReconstruction.cpp
@@ -23,6 +23,42 @@ public:
   bool sequenceReconstruction(vector<int>& org, vector<vector<int>>& seqs) {
     if (seqs.empty()) return false;
     int L = SZ(org);
+
+    VVI edges(L+1,VI());
+    VI degree(L+1,0);
+
+    FORR(seq, seqs) {
+      REP(i, SZ(seq)) {
+	int n = seq[i];
+	if (n<1 || n>L) return false;
+	if (i<SZ(seq)-1) PUSH(edges[n], seq[i+1]);
+	if (i>0) degree[n]++;
+      }
+    }
+
+    queue<int> Q;
+    FOR(i,1,L+1) {
+      if (degree[i]==0) Q.push(i);
+    }
+
+    int i=0;
+    while (!Q.empty()) {
+      if (SZ(Q)>1) return false;
+      int n = Q.front(); Q.pop();
+      if (org[i]!=n) return false;
+      FORR(v, edges[n]) {
+	degree[v]--;
+	if (degree[v]==0) Q.push(v);
+      }
+      i++;
+    }
+
+    return i==L;
+  }
+  
+  bool sequenceReconstructionV1(vector<int>& org, vector<vector<int>>& seqs) {
+    if (seqs.empty()) return false;
+    int L = SZ(org);
     MAPII pos;
     REP(i,L) pos[org[i]] = i;
 
