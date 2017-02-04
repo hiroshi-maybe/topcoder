c4e0048fb6739a47275de073b759c1ffbac0b100
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-02-04 13:43:34 -0800

    Create Morris in-order traversal in library

---
+++ lib/BTree.cpp
@@ -0,0 +1,24 @@
+// Morris traversal
+void inorder(TreeNode* root) {
+    while (root) {
+        if (!root->left) {
+            proc(root);
+            root = root->right;
+        } else {
+            TreeNode* pre = root->left;
+            while(pre->right!=NULL&&pre->right!=root) pre = pre->right;
+
+            if (!pre->right) {
+                // create back link
+                pre->right = root;
+                root = root->left;
+            } else {
+                // reached parent
+                pre->right = NULL;
+
+                proc(root, pick);
+                root = root->right;
+            }
+        }
+    }
+}
