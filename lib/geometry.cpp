dbc5ab2cc11d62db37c3c5ffd83c62e452b14c26
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-06-17 01:18:09 -0700

    add CLRS 33.3 find convex hull by graham scan

--- lib/geometry.cpp
+++ lib/geometry.cpp
@@ -41,6 +41,11 @@ bool isVertical(Vector u, Vector v) {
   return dotProduct(u,v)==0;
 }
 
+int length(pair<int,int> p1, pair<int,int> p2) {
+  int dx = p1.first-p2.first, dy = p1.second-p2.second;
+  return dx*dx + dy*dy;
+}
+
 // CLRS 33.1
 // p1->p2 intersects p3->p4
 bool onSegment(pair<int,int> p1, pair<int,int> p2, pair<int,int> pi) {
@@ -91,6 +96,49 @@ bool isRightTurn(pair<int,int> p1, pair<int,int> p2, pair<int,int> origin) {
   return isCounterClockwise(p1,p2,origin);
 }
 
+// CLRS 33.3 GRAHAM-SCAN(Q)
+pair<int,int> p0;
+bool comp(const pair<int,int> a, const pair<int,int> b) {
+  int crossprod = crossProduct(a, b, p0);
+  if (crossprod!=0) return crossprod > 0;
+  return length(a,p0) < length(b,p0); // tie for colinear points
+}
+vector<pair<int,int>> findConvexHull(vector<pair<int,int>> ps) {
+  if (ps.size()<3) return {};
+
+  // find p0 (bottom left point)
+  int p0_i = 0;
+  for(int i=1; i<ps.size(); ++i) {
+    if (ps[i].second <= ps[p0_i].second && ps[i].first <= ps[p0_i].first) p0_i = i;
+  }
+  swap(ps[0], ps[p0_i]);
+  p0=ps[0];
+
+  // sort by polar angle
+  sort(ps.begin(), ps.end(), &comp);
+
+  // remove all but furthest point if points have same angle
+  int N=1;
+  for (int i=1; i<ps.size(); ++i) {
+    while(i<ps.size()-1&&isColinear(ps[i],ps[i+1],p0)) ++i;
+    ps[N++] = ps[i];
+  }
+
+  if (N<3) return {};
+
+  int si=0;
+  vector<pair<int,int>> S(N);
+  S[si++] = ps[0];
+  S[si++] = ps[1];
+  S[si] = ps[2];
+
+  for(int i=3; i<N; ++i) {
+    while(!isLeftTurn(S[si],ps[i],S[si-1])) --si;
+    S[++si] = ps[i];
+  }
+  S.erase(S.begin()+si+1, S.end());
+  return S;
+}
 
 int main(int argc, char const *argv[]) {
   pair<int,int> origin={0,0}, p1={1,0}, p2={0,1}, p3={-1,0}, p4={0,-1};
@@ -102,4 +150,12 @@ int main(int argc, char const *argv[]) {
   assert(isLeftTurn(p1, p2, origin));
   assert(isRightTurn(p2, p1, origin));
   assert(intersect(p1, p4, p2, p4));
+
+  // http://www.geeksforgeeks.org/convex-hull-set-2-graham-scan/
+  vector<pair<int,int>> ps = findConvexHull({{0,3},{1,1},{2,2},{4,4},{0,0},{1,2},{3,1},{3,3}});
+  vector<pair<int,int>> exp = {{0,0},{3,1},{4,4},{0,3}};
+  assert(ps.size()==exp.size());
+  for(int i=0; i<ps.size(); ++i) {
+    assert(ps[i].first==exp[i].first&&ps[i].second==exp[i].second);
+  }
 }
