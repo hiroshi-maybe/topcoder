9325170ecd62f505172dd9a44d8a8095a6370501
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-07-08 16:46:22 -0700

    remove argument of modulo because it's way too slow

--- lib/math.cpp
+++ lib/math.cpp
@@ -25,39 +25,59 @@ tuple<int,int,int> gcd_extended(int a, int b) {
   return make_tuple(d2,y2,x2-(a/b)*y2);
 }
 
-// CLRS 31.6 and https://kimiyuki.net/blog/2017/03/27/srm-711-med/
-// compute (a^b) `mod` mod, mod=1e9+7 by default
+// CLRS 31.6
+// compute (a^b) `mod` 1e9+7
 // O(lg b)
-LL powmod(LL a, LL b, LL mod=MOD) {
-  assert(0<=a && a<mod);
+LL powmod(LL a, LL b) {
+  assert(0<=a && a<MOD);
   assert(0<=b);
 
   LL res=1;
   for(LL mask=1; mask<=b; mask<<=1) {
-    if(b&mask) res*=a, res%=mod;
-    a*=a; a%=mod;
+    if(b&mask) res*=a, res%=MOD;
+    a*=a; a%=MOD;
   }
   return res;
 }
 
 // https://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Computation
 // Modular multiplicative inverse by Euler's theorem
-// a^-1 (`mod` mod) = a^(mod-2), a should be coprime to mod, mod=1e9+7 by default
+// a^-1 (`mod` mod) = a^(mod-2), a should be coprime to mod, mod=1e9+7
 // O(lg m)
-LL modinv(LL a, LL mod=MOD) {
-  return powmod(a, mod-2);
+LL modinv(LL a) {
+  return powmod(a, MOD-2);
 }
 
-// memoized factorial(n) (mod m)
+// memoized factorial(n) (`mod` 1e9+7)
 // O(n) before cache warmup
-LL factmod(LL n, LL mod=MOD) {
+LL factmod(LL n) {
   static vector<LL> memo(1,1);
   if(memo.size()<=n) {
     LL l=memo.size();
     memo.resize(n+1);
-    for(LL i=l; i<=n; ++i) memo[i]=memo[i-1]*i, memo[i]%=mod;
+    for(LL i=l; i<=n; ++i) memo[i]=memo[i-1]*i, memo[i]%=MOD;
   }
   return memo[n];
+}
+
+// n chooses k
+// C(n,k) = n!/((n-k)!*k!)
+LL choose(LL n, LL k) {
+  if (n<k) return 0;
+  k = min(n-k,k);
+  LL res=factmod(n);
+  res *= modinv(factmod(n-k)), res%=MOD;
+  res *= modinv(factmod(  k)), res%=MOD;
+  
+  return res;
+}
+
+// multichoose
+// H(n,k) = C(n+k-1,k)
+LL multichoose(LL n, LL k) {
+  if(n==0&&k==0) return 1;
+
+  return choose(n+k-1,k);
 }
 
 // main
@@ -70,15 +90,14 @@ int main(int argc, char const *argv[]) {
   assert(get<1>(t)==-6);
   assert(get<2>(t)==11);
 
-  int m1 = powmod(7,560,561);
-  assert(m1==1);
-  
   int m2 = powmod(7,560);
   assert(m2==108725231);
   
   LL m = 560;
-  LL mi = modinv(m,MOD);
+  LL mi = modinv(m);
   assert((m*mi)%MOD==1);
   
   assert(factmod(560)==597965522);
+  assert(choose(771,50)==275127687);
+  assert(multichoose(771,50)==954694855);
 }
