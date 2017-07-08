a4b16188eda9bc8b7540e095a8536d1ca6b1dd15
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-07-08 15:46:18 -0700

    add factorial modular in library

--- lib/math.cpp
+++ lib/math.cpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <tuple>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 typedef long long LL;
@@ -25,26 +26,38 @@ tuple<int,int,int> gcd_extended(int a, int b) {
 }
 
 // CLRS 31.6 and https://kimiyuki.net/blog/2017/03/27/srm-711-med/
-// compute (a^b) mod n, n=1e9+7 by default
+// compute (a^b) `mod` mod, mod=1e9+7 by default
 // O(lg b)
-LL powmod(LL a, LL b, LL n=MOD) {
-  assert(0<=a && a<n);
+LL powmod(LL a, LL b, LL mod=MOD) {
+  assert(0<=a && a<mod);
   assert(0<=b);
 
   LL res=1;
   for(LL mask=1; mask<=b; mask<<=1) {
-    if(b&mask) res*=a, res%=n;
-    a*=a; a%=n;
+    if(b&mask) res*=a, res%=mod;
+    a*=a; a%=mod;
   }
   return res;
 }
 
 // https://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Computation
 // Modular multiplicative inverse by Euler's theorem
-// a^-1 (mod m) = a^(m-2), a should be coprime to m, m=1e9+7 by default
+// a^-1 (`mod` mod) = a^(mod-2), a should be coprime to mod, mod=1e9+7 by default
 // O(lg m)
-LL modinv(LL a, LL m=MOD) {
-  return powmod(a, m-2);
+LL modinv(LL a, LL mod=MOD) {
+  return powmod(a, mod-2);
+}
+
+// memoized factorial(n) (mod m)
+// O(n) before cache warmup
+LL factmod(LL n, LL mod=MOD) {
+  static vector<LL> memo(1,1);
+  if(memo.size()<=n) {
+    LL l=memo.size();
+    memo.resize(n+1);
+    for(LL i=l; i<=n; ++i) memo[i]=memo[i-1]*i, memo[i]%=mod;
+  }
+  return memo[n];
 }
 
 // main
@@ -66,4 +79,6 @@ int main(int argc, char const *argv[]) {
   LL m = 560;
   LL mi = modinv(m,MOD);
   assert((m*mi)%MOD==1);
+  
+  assert(factmod(560)==597965522);
 }
