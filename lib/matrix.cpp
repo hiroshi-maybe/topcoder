d4a4996654b96bdeeb8320cf449dcabb9ec85d28
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-07-09 15:34:00 -0700

    make powmat more composable with identity matrix

--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -6,34 +6,42 @@ using namespace std;
 typedef long long LL;
 
 const int MX=105;
-LL A[MX][MX], S[MX][MX];
+LL A[MX][MX], B[MX][MX];
+constexpr int MOD = 1e9+7;
 
-void timeMat(int N, LL A[MX][MX], LL B[MX][MX]) {
+// matrix multiplication
+// X = XxY, X: NxN matrix, Y: NxN matrix, N<MX
+void timeMat(LL X[MX][MX], LL Y[MX][MX], int N) {
   LL tmp[MX][MX]; memset(tmp,0,sizeof(tmp));
   
   for(int i=0; i<N; ++i) {
     for(int j=0; j<N; ++j) {
-      for(int k=0; k<N; ++k) tmp[i][j] += A[i][k]*B[k][j];
+      for(int k=0; k<N; ++k) tmp[i][j] += (X[i][k]*Y[k][j])%MOD, tmp[i][j]%=MOD;
     }
   }
   
-  for(int i=0; i<N; ++i) for(int j=0; j<N; ++j) A[i][j]=tmp[i][j];
+  for(int i=0; i<N; ++i) for(int j=0; j<N; ++j) X[i][j]=tmp[i][j];
 }
 
 // power matrix O(N^3*lg n)
-// S = S*A^n (S=A=nxn matrix)
+// B = A^n, B: NxN matrix, A: NxN matrix, N<MX
 //
-// prequisite: S and A are initialized
+// prequisite: A is initialized
 void powerMat(LL n, int N) {
+  assert(N<=MX);
   assert(n>=0);
+  
+  // B = I (identity matrix)
+  memset(B,0,sizeof B);
+  for(int i=0; i<N; ++i) B[i][i]=1;
   
   LL X[MX][MX];
   for(int i=0; i<N; ++i) for(int j=0; j<N; ++j) X[i][j]=A[i][j];
   
   while(n) {
     // invariant: X = A^(2^i)
-    if (n&1) timeMat(N,S,X);
-    timeMat(N,X,X);
+    if (n&1) timeMat(B,X,N); // B=B*X
+    timeMat(X,X,N); // X=X^2
     n>>=1;
   }
 }
@@ -41,8 +49,10 @@ void powerMat(LL n, int N) {
 /***********************   test code below   ***********************/
 
 int main(int argc, char const *argv[]) {
-  memset(S,0,sizeof(S));
+  memset(B,0,sizeof(B));
   memset(A,0,sizeof(A));
+  LL S[MX][MX];
+  memset(S,0,sizeof(S));
   
   S[0][0]=1; S[0][1]=2;
   S[1][0]=3; S[1][1]=4;
@@ -51,6 +61,7 @@ int main(int argc, char const *argv[]) {
   A[1][0]=21; A[1][1]=16;
   
   powerMat(3,2);
+  timeMat(S,B,2);
   
   assert(S[0][0]==197);
   assert(S[0][1]==142);
