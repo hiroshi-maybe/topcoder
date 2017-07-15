a601e6dac065ae0e41c8b69afb9d8040d591a689
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-07-15 16:48:51 -0700

    solve Initials SRM 698 div2 easy

---
+++ Initials/Initials.cpp
@@ -0,0 +1,198 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// debug cerr
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define dump2(x,y)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cerr << endl;
+
+/**
+ 
+ 7/15/2017
+ 
+ 9:36-9:41 (242.41 pt)
+ 
+ 16:45 add solutions by split/stringstream
+ 
+ */
+
+vector<string> split(const string &s, char c) {
+  vector<string> res;
+  
+  istringstream ss(s);
+  string token;
+  while(getline(ss,token,c)) {
+    res.push_back(token);
+  }
+  
+  return res;
+}
+
+// O(N) time
+class Initials {
+public:
+  string getInitials(string name) {
+    string res="";
+    stringstream ss(name);
+    string token;
+    while(ss >> token) {
+      if(!token.empty()) res.push_back(token[0]);
+    }
+    return res;
+  }
+};
+
+class Initials_split {
+public:
+  string getInitials(string name) {
+    vector<string> ts=split(name,' ');
+    string res;
+    FORR(t,ts) if(SZ(t)) res.push_back(t.front());
+    return res;
+  }
+};
+
+class Initials_org {
+  public:
+  string getInitials(string name) {
+    string res(1,name.front());
+    FOR(i,1,SZ(name)) {
+      if(name[i-1]==' ') res.push_back(name[i]);
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("Initials.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string name, string __expected) {
+    time_t startClock = clock();
+    Initials *instance = new Initials();
+    string __result = instance->getInitials(name);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string name;
+        from_stream(name);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(name, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1500136533;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "Initials (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
