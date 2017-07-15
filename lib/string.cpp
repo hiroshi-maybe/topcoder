d083f66b031ffef386f06578d7a285eb649219fc
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-07-15 16:28:45 -0700

    bugfix of split() and add test

--- lib/string.cpp
+++ lib/string.cpp
@@ -1,9 +1,15 @@
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+using namespace std;
+
 string join(const vector<string> &strs, string delimitor) {
   int L = strs.size();
   if (L==0) { return delimitor; }
 
   string res = strs[0];
-  FOR(i,1,L) {
+  for(int i=1; i<L; ++i) {
     res += delimitor+strs[i];
   }
 
@@ -12,18 +18,44 @@ string join(const vector<string> &strs, string delimitor) {
 
 vector<string> split(const string &s, char c) {
   vector<string> ar;
-
+  
   string::size_type i = 0;
   string::size_type j = s.find(c);
-
+  
+  if(j == string::npos) return {s};
+  
   while (j != string::npos) {
     ar.push_back(s.substr(i, j-i));
     i = ++j;
     j = s.find(c, j);
-
+    
     if (j == string::npos)
       ar.push_back(s.substr(i, s.length()));
   }
-
+  
   return ar;
+}
+
+// test
+
+void assertVec(vector<string> actual, vector<string> expected) {
+  assert(actual.size()==expected.size());
+  for(int i=0; i<actual.size(); ++i) {
+    assert(actual[i]==expected[i]);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  auto ss1 = split("x", ',');
+  assertVec(ss1, {"x"});
+  
+  auto ss2 = split("x1,y2,z", ',');
+  assertVec(ss2, {"x1","y2","z"});
+  
+  auto ss3 = split("x1,,y2,z,", ',');
+  assertVec(ss3, {"x1","","y2","z",""});
+  
+  assert(join(ss1,",")=="x");
+  assert(join(ss2," ")=="x1 y2 z");
+  assert(join(ss3,":")=="x1::y2:z:");
 }
