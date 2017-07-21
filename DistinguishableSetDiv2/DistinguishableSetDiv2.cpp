5b615786ea40527e62b53b46b684c5802f7d42f9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-07-20 23:56:28 -0700

    solve DistinguishableSetDiv2 SRM 694 div2 medium

---
+++ DistinguishableSetDiv2/DistinguishableSetDiv2.cpp
@@ -0,0 +1,187 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// debug cerr
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define dump2(x,y)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cerr << endl;
+
+/**
+ 
+ 7/20/2017
+ 
+ 8:24-8:37 passed system test (424.01 pt)
+ 
+ Other solutions
+ - using sort without hashset
+  - https://community.topcoder.com/stat?c=problem_solution&rm=329027&rd=16766&pm=14305&cr=40168291
+  - https://community.topcoder.com/stat?c=problem_solution&rm=329023&rd=16766&pm=14305&cr=40027652
+ 
+ */
+
+// O(N*M*2^M) time (≈5*10^5)
+class DistinguishableSetDiv2 {
+  public:
+  int count(vector<string> A) {
+    int N=SZ(A),M=SZ(A[0]);
+    
+    int res=0;
+    REP(mask,1<<M) {
+      unordered_set<string> S;
+      FORR(a, A) {
+        string b="";
+        REP(i,M) if(mask>>i&1) b.push_back(a[i]);
+        S.insert(b);
+      }
+      
+      if(SZ(S)==N) ++res;
+    }
+    
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("DistinguishableSetDiv2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> answer, int __expected) {
+    time_t startClock = clock();
+    DistinguishableSetDiv2 *instance = new DistinguishableSetDiv2();
+    int __result = instance->count(answer);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> answer;
+        from_stream(answer);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(answer, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1500564280;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "DistinguishableSetDiv2 (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
