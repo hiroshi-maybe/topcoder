4e70337a8b76cfaeccb975eea2569573d4727567
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-07-25 22:11:13 -0700

    solve SimilarUserDetection SRM 698 div2 easy

---
+++ SimilarUserDetection/SimilarUserDetection.cpp
@@ -0,0 +1,183 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// debug cerr
+#define dump(x)  cerr << #x << " = " << (x) << endl;
+#define dump2(x,y)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cerr << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cerr << endl;
+
+/**
+ 
+ 7/25/2017
+ 
+ 8:34-8:39 passed system test (247.73 pt)
+ 
+ O(l*N^2) time, O(1) space solutions
+ - https://community.topcoder.com/stat?c=problem_solution&rm=328614&rd=16710&pm=14240&cr=40102659
+ - https://community.topcoder.com/stat?c=problem_solution&rm=328613&rd=16710&pm=14240&cr=40167792
+ - https://community.topcoder.com/stat?c=problem_solution&rm=328618&rd=16710&pm=14240&cr=40179542
+ 
+ */
+
+// O(N*l) time, O(N*l) space
+class SimilarUserDetection {
+  public:
+  string found="Similar handles found";
+  string notfound="Similar handles not found";
+  string haveSimilar(vector<string> handles) {
+    unordered_set<string> S;
+    FORR(s,handles) {
+      FORR(c,s) {
+        if(c=='O') c='0';
+        if(c=='l'||c=='I') c='1';
+      }
+      S.insert(s);
+    }
+    return SZ(S)==SZ(handles)?notfound:found;
+  }
+};
+
+// CUT begin
+ifstream data("SimilarUserDetection.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> handles, string __expected) {
+    time_t startClock = clock();
+    SimilarUserDetection *instance = new SimilarUserDetection();
+    string __result = instance->haveSimilar(handles);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> handles;
+        from_stream(handles);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(handles, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1500996883;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "SimilarUserDetection (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
