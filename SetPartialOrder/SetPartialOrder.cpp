498daf45ea5e9aa73b4f65a1f11f55fe2bc7e9a4
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-08-11 23:39:37 -0700

    solve SetPartialOrder SRM 672 div2 easy

---
+++ SetPartialOrder/SetPartialOrder.cpp
@@ -0,0 +1,210 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/**
+ 
+ 8/11/2017
+ 
+ 9:05-9:10 (239.34 pt)
+ 
+ editorial:
+ - https://apps.topcoder.com/wiki/display/tc/SRM+672
+ 
+ 23:30-23:38 add solution with built-in set operation
+ 
+ */
+
+class SetPartialOrder {
+public:
+  string eq="EQUAL", lt="LESS", gt="GREATER", na="INCOMPARABLE";
+  string compareSets(vector<int> a, vector<int> b) {
+    set<int> A(a.begin(),a.end()),B(b.begin(),b.end());
+
+    set<int> AB;
+    set_intersection(A.begin(),A.end(),B.begin(),B.end(),inserter(AB,AB.begin()));
+    
+    if(AB==A&&AB==B) return eq;
+    if(AB==A) return lt;
+    if(AB==B) return gt;
+    return na;
+  }
+};
+
+
+class SetPartialOrder_org {
+  public:
+  string eq="EQUAL", lt="LESS", gt="GREATER", na="INCOMPARABLE";
+  string compareSets(vector<int> a, vector<int> b) {
+    SETI A(a.begin(),a.end()),B(b.begin(),b.end());
+    
+    bool x=subset(A,B),y=subset(B,A);
+    
+    if(x&&y) return eq;
+    if(x) return lt;
+    if(y) return gt;
+    return na;
+  }
+  
+  bool subset(SETI sub, SETI super) {
+    FORR(n,sub) if(super.count(n)==0) return false;
+    return true;
+  }
+};
+
+// CUT begin
+ifstream data("SetPartialOrder.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> a, vector<int> b, string __expected) {
+    time_t startClock = clock();
+    SetPartialOrder *instance = new SetPartialOrder();
+    string __result = instance->compareSets(a, b);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> a;
+        from_stream(a);
+        vector<int> b;
+        from_stream(b);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(a, b, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1502467483;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "SetPartialOrder (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
