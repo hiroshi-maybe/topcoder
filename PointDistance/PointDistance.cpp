153c188dc2eb76ef8ad0fc94b15772bd81d350cf
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-08-18 09:59:21 -0700

    solve PointDistance SRM 667 div2 easy

---
+++ PointDistance/PointDistance.cpp
@@ -0,0 +1,218 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/**
+ 
+ 8/17/2017
+ 
+ 9:11-9:21 passed system test (221.33 pt)
+ 
+ editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+667
+    - 4 corners (-100,-100),(-100,100),(100,-100),(-100,-100) satisfy C!=A AND C!=B
+    - find (C_X,C_Y) s.t. abs(C_X-A_X)>abs(C_X-B_X) AND abs(C_Y-A_Y)>abs(C_Y-B_Y)
+ 
+ 8/18/2017
+  - add simpler solution of editorial
+ 
+ */
+
+class PointDistance {
+public:
+  vector<int> findPoint(int x1, int y1, int x2, int y2) {
+    int x3=x1<=x2?100:-100,y3=y1<=y2?100:-100;
+    return {x3,y3};
+  }
+};
+
+class PointDistance_org {
+  public:
+  int dist(int x1,int y1,int x2,int y2) {
+    int dx=x1-x2,dy=y1-y2;
+    return dx*dx+dy*dy;
+  }
+  vector<int> findPoint(int x1, int y1, int x2, int y2) {
+    VI res;
+    for(int i=-100; i<=100; ++i) for(int j=-100; j<=100; ++j) {
+      if(i!=x1&&i!=x2&&j!=y1&&j!=y2&&dist(x1,y1,i,j)>dist(x2,y2,i,j)) {
+        res.push_back(i),res.push_back(j);
+        return res;
+      }
+    }
+    
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("PointDistance.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(int x1, int y1, int x2, int y2, vector<int> __expected) {
+    time_t startClock = clock();
+    PointDistance *instance = new PointDistance();
+    vector<int> __result = instance->findPoint(x1, y1, x2, y2);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int x1;
+        from_stream(x1);
+        int y1;
+        from_stream(y1);
+        int x2;
+        from_stream(x2);
+        int y2;
+        from_stream(y2);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(x1, y1, x2, y2, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1502986253;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "PointDistance (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
