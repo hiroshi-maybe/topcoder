79cbecf19287721ffe53cb5f379107ba244f6995
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-08-23 22:11:15 -0700

    add cross product helpers in geometry library

--- lib/geometry.cpp
+++ lib/geometry.cpp
@@ -11,7 +11,7 @@ public:
   Vector(pair<int,int> p1, pair<int,int> p2): Vector(p1.first-p2.first, p1.second-p2.second) {}
   Vector(int x, int y): x(x), y(y) {}
     
-  int length() {
+  int distance() {
       return x*x + y*y;
   }
 };
@@ -25,23 +25,39 @@ pair<int,int> makePair(vector<int> v) {
   return {v[0],v[1]};
 }
 
-int crossProduct(Vector u, Vector v) {
+/*
+ 
+ Cross product ×
+ 
+ u×v = u.x * v.y - u.y * v.x
+     = |u|*|v|*sin𝛩
+     = area of parallelogram formed by u and v
+ 
+ 1) u×v = 0
+   u and v are colinear
+ 2) u×v > 0
+   u->v is clockwise
+ 3) u×v < 0
+   u->v is anti-clockwise
+
+ */
+int det(Vector u, Vector v) {
   return u.x*v.y - u.y*v.x;
 }
-int crossProduct(pair<int,int> p1, pair<int,int> p2, pair<int,int> origin) {
+int det(pair<int,int> p1, pair<int,int> p2, pair<int,int> origin) {
   auto vs = makeVectors(p1,p2,origin);
-  return crossProduct(vs.first,vs.second);
+  return det(vs.first,vs.second);
 }
-int dotProduct(Vector u, Vector v) {
+int dot(Vector u, Vector v) {
   return u.x*v.x + u.y*v.y;
 }
 
 // 90 degree
 bool isVertical(Vector u, Vector v) {
-  return dotProduct(u,v)==0;
+  return dot(u,v)==0;
 }
 
-int length(pair<int,int> p1, pair<int,int> p2) {
+int distance(pair<int,int> p1, pair<int,int> p2) {
   int dx = p1.first-p2.first, dy = p1.second-p2.second;
   return dx*dx + dy*dy;
 }
@@ -56,10 +72,10 @@ bool onSegment(pair<int,int> p1, pair<int,int> p2, pair<int,int> pi) {
   return xmin<=pi.first && pi.first<=xmax && ymin<=pi.second && pi.second<=ymax;
 }
 bool intersect(pair<int,int> p1, pair<int,int> p2, pair<int,int> p3, pair<int,int> p4) {
-  int d1 = crossProduct(p3,p4,p1),
-      d2 = crossProduct(p3,p4,p2),
-      d3 = crossProduct(p1,p2,p3),
-      d4 = crossProduct(p1,p2,p4);
+  int d1 = det(p3,p4,p1),
+      d2 = det(p3,p4,p2),
+      d3 = det(p1,p2,p3),
+      d4 = det(p1,p2,p4);
 
   if (d1*d2<0 && d3*d4<0) return true;
   if (d1==0 && onSegment(p3,p4,p1)) return true;
@@ -73,17 +89,17 @@ bool intersect(pair<int,int> p1, pair<int,int> p2, pair<int,int> p3, pair<int,in
 // true: org->p1 is clockwise against org->p2
 bool isClockwise(pair<int,int> p1, pair<int,int> p2, pair<int,int> origin) {
   auto vpair = makeVectors(p1,p2,origin);
-  return crossProduct(vpair.first, vpair.second)>0;
+  return det(vpair.first, vpair.second)>0;
 }
 bool isCounterClockwise(pair<int,int> p1, pair<int,int> p2, pair<int,int> origin) {
   auto vpair = makeVectors(p1,p2,origin);
-  return crossProduct(vpair.first, vpair.second)<0;
+  return det(vpair.first, vpair.second)<0;
 }
 // CLRS 33.1
 // org->p1 and org->p2 form same direction or opposite direction
 bool isColinear(pair<int,int> p1, pair<int,int> p2, pair<int,int> origin) {
   auto vpair = makeVectors(p1,p2,origin);
-  return crossProduct(vpair.first, vpair.second)==0;
+  return det(vpair.first, vpair.second)==0;
 }
 // CLRS 33.1
 // true: p1->p2 is left turn for origin
@@ -97,11 +113,15 @@ bool isRightTurn(pair<int,int> p1, pair<int,int> p2, pair<int,int> origin) {
 }
 
 // CLRS 33.3 GRAHAM-SCAN(Q)
-pair<int,int> p0;
-bool comp(const pair<int,int> a, const pair<int,int> b) {
-  int crossprod = crossProduct(a, b, p0);
-  if (crossprod!=0) return crossprod > 0;
-  return length(a,p0) < length(b,p0); // tie for colinear points
+
+// sort points by plar angle
+// smaller scalar comes first for tie
+void sortByPolarAngle(vector<pair<int, int>>& ps, pair<int, int> origin) {
+  sort(ps.begin(), ps.end(), [&](const pair<int,int> a, const pair<int,int> b) {
+    int d = det(a,b,origin);
+    if(d!=0) return d > 0;
+    return distance(a, origin) < distance(b, origin);
+  });
 }
 vector<pair<int,int>> findConvexHull(vector<pair<int,int>> ps) {
   if (ps.size()<3) return {};
@@ -112,10 +132,10 @@ vector<pair<int,int>> findConvexHull(vector<pair<int,int>> ps) {
     if (ps[i].second <= ps[p0_i].second && ps[i].first <= ps[p0_i].first) p0_i = i;
   }
   swap(ps[0], ps[p0_i]);
-  p0=ps[0];
+  pair<int, int> p0=ps[0];
 
   // sort by polar angle
-  sort(ps.begin(), ps.end(), &comp);
+  sortByPolarAngle(ps, p0);
 
   // remove all but furthest point if points have same angle
   int N=1;
@@ -140,6 +160,23 @@ vector<pair<int,int>> findConvexHull(vector<pair<int,int>> ps) {
   return S;
 }
 
+// check if point is surrounded by given points
+// if p is on segment, return true
+// O(N*lg N) time
+bool surrounded(pair<int,int> p, vector<pair<int, int>>& ps) {
+  sortByPolarAngle(ps, p);
+  vector<int> ds;
+  int N=ps.size();
+  for(int i=0; i<N; ++i) {
+    int d = det(ps[i], ps[(i+1)%N], p);
+    if(d==0) continue; // ignore colinear points or p is on segment
+    d = d<0;
+    if(ds.size() && ds.back()!=d) return false;
+    ds.push_back(d);
+  }
+  return true;
+}
+
 int main(int argc, char const *argv[]) {
   pair<int,int> origin={0,0}, p1={1,0}, p2={0,1}, p3={-1,0}, p4={0,-1};
 
@@ -158,4 +195,11 @@ int main(int argc, char const *argv[]) {
   for(int i=0; i<ps.size(); ++i) {
     assert(ps[i].first==exp[i].first&&ps[i].second==exp[i].second);
   }
+  
+  vector<pair<int,int>> surrounding = {{1,1},{-1,1},{1,-1},{-1,-1}};
+  assert(surrounded(origin, surrounding));
+  vector<pair<int,int>> onsegment = {{1,0},{0,1},{-1,0}};
+  assert(surrounded(origin, onsegment));
+  vector<pair<int,int>> notsurrounding = {{1,1},{2,1},{1,2},{2,2}};
+  assert(!surrounded(origin, notsurrounding));
 }
