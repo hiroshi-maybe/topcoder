d016e6927dda9ff65a8d93e9e1d60b60dcf4664a
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-08-27 14:58:42 -0700

    solve BridgeBuildingDiv2 SRM 661 div2 medium

---
+++ BridgeBuildingDiv2/BridgeBuildingDiv2.cpp
@@ -0,0 +1,202 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 8/24/2017
+ 
+ 22:20-22:29 designed
+ 22:29-22:40 (350.50 pt)
+ 
+ editorials:
+  - http://kmjp.hatenablog.jp/entry/2015/06/15/0930
+  - 
+ 
+ */
+
+int mx[24][24];
+class BridgeBuildingDiv2 {
+  public:
+  const int Inf=10000;
+  int minDiameter(vector<int> A, vector<int> B, int K) {
+    int res=Inf;
+    int N=SZ(A)+1,V=2*N;
+    REP(mask,1<<N) {
+      if(__builtin_popcount(mask)!=K) continue;
+      ZERO(mx);
+      REP(i,2*N)REP(j,2*N) if(i!=j) mx[i][j]=Inf;
+      REP(i,SZ(A)) mx[i][i+1]=mx[i+1][i]=A[i];
+      REP(i,SZ(B)) mx[N+i][N+i+1]=mx[N+i+1][N+i]=B[i];
+      
+      REP(i,N) if(mask&(1<<i)) mx[i][N+i]=mx[N+i][i]=0;
+      
+      REP(k,V)REP(i,V)REP(j,V) mx[i][j]=min(mx[i][j],mx[i][k]+mx[k][j]);
+      
+      int d=0;
+      REP(i,V)REP(j,V) d=max(d,mx[i][j]);
+      res=min(res,d);
+    }
+    
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("BridgeBuildingDiv2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> a, vector<int> b, int K, int __expected) {
+    time_t startClock = clock();
+    BridgeBuildingDiv2 *instance = new BridgeBuildingDiv2();
+    int __result = instance->minDiameter(a, b, K);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> a;
+        from_stream(a);
+        vector<int> b;
+        from_stream(b);
+        int K;
+        from_stream(K);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(a, b, K, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1503638401;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "BridgeBuildingDiv2 (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
