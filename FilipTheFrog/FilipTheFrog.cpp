5190beb2c677daf675588adc83b1d22342c69f8a
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-08-29 00:33:09 -0700

    solve FilipTheFrog SRM 659 div2 easy

---
+++ FilipTheFrog/FilipTheFrog.cpp
@@ -0,0 +1,216 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 8/28/2017
+ 
+ 8:44-8:55 system test passed (215.04 pt)
+ 
+ editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+659
+ 
+ 24:27-24:30 add two pointer solution of editorial
+ 
+ submit solutions:
+  - many people are using BFS :)
+    - https://community.topcoder.com/stat?c=problem_solution&rm=326307&rd=16462&pm=13779&cr=40002298
+    - https://community.topcoder.com/stat?c=problem_solution&rm=326296&rd=16462&pm=13779&cr=22933742
+  - two pointers
+    - https://community.topcoder.com/stat?c=problem_solution&rm=326297&rd=16462&pm=13779&cr=22874050
+ 
+ */
+
+class FilipTheFrog {
+public:
+  int countReachableIslands(vector<int> ps, int L) {
+    int st=ps[0];
+    sort(ps.begin(),ps.end());
+    int p=find(ps.begin(),ps.end(),st)-ps.begin();
+    
+    int res=1;
+    for(int i=p+1; i<SZ(ps)&&ps[i]-ps[i-1]<=L; ++i) ++res;
+    for(int i=p-1; i>=0&&ps[i+1]-ps[i]<=L; --i) ++res;
+    return res;
+  }
+};
+
+// original BFS solution
+class FilipTheFrog_bfs {
+  public:
+  int countReachableIslands(vector<int> ps, int L) {
+    int res=1,N=SZ(ps);
+    VI viz(N,0);
+    queue<int> Q;
+    Q.emplace(0); viz[0]=true;
+    
+    while(SZ(Q)) {
+      int u=Q.front(); Q.pop();
+      REP(i,N) if(!viz[i]) {
+        if(abs(ps[u]-ps[i])<=L) {
+          Q.emplace(i); ++res; viz[i]=true;
+        }
+      }
+    }
+    
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("FilipTheFrog.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> positions, int L, int __expected) {
+    time_t startClock = clock();
+    FilipTheFrog *instance = new FilipTheFrog();
+    int __result = instance->countReachableIslands(positions, L);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> positions;
+        from_stream(positions);
+        int L;
+        from_stream(L);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(positions, L, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1503935024;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "FilipTheFrog (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
