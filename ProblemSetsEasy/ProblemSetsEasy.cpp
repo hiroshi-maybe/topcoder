011dbd96b6d739faf9818392e35db0a6a71509ab
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-08-31 00:09:43 -0700

    solve ProblemSetsEasy SRM 657 div2 medium

---
+++ ProblemSetsEasy/ProblemSetsEasy.cpp
@@ -0,0 +1,189 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 8/30/2017
+ 
+ 9:51-10:08 ok binary search
+ 10:09-10:15 system test passed (323.21 pt),
+ 
+ editorials:
+  - http://kmjp.hatenablog.jp/entry/2015/04/29/1000
+  - https://apps.topcoder.com/wiki/display/tc/SRM+657
+ 
+ summary:
+  - this solution works for div1 easy too 👍
+  - I wish I came up with this solution earlier
+ 
+ */
+class ProblemSetsEasy {
+  public:
+  bool t(int E, int EM, int M, int MH, int H, int T) {
+    if(T>E) EM-=T-E;
+    if(T>H) MH-=T-H;
+    return EM>=0&&MH>=0&&EM+MH+M>=T;
+  }
+  int maxSets(int E, int EM, int M, int MH, int H) {
+    int ok=min({E,M,H}),ng=3*1e5+1;
+    while(abs(ok-ng)>1) {
+      int mid=(ok+ng)/2;
+      if(t(E,EM,M,MH,H,mid)) ok=mid;
+      else ng=mid;
+    }
+    return ok;
+  }
+};
+
+// CUT begin
+ifstream data("ProblemSetsEasy.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int E, int EM, int M, int MH, int H, int __expected) {
+    time_t startClock = clock();
+    ProblemSetsEasy *instance = new ProblemSetsEasy();
+    int __result = instance->maxSets(E, EM, M, MH, H);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int E;
+        from_stream(E);
+        int EM;
+        from_stream(EM);
+        int M;
+        from_stream(M);
+        int MH;
+        from_stream(MH);
+        int H;
+        from_stream(H);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(E, EM, M, MH, H, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1504111898;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "ProblemSetsEasy (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
