cfd90d15798d0c11f732ad5e93ff0f6ca974899d
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-09-10 15:12:53 -0700

    solve TheGridDivTwo SRM 646 div2 medium

---
+++ TheGridDivTwo/TheGridDivTwo.cpp
@@ -0,0 +1,216 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+vector< pair < int, int > >  moves = { {0,1},{0,-1},{1,0},{-1,0} };
+
+/*
+ 
+ 9/10/2017
+ 
+ 9:30-9:45 TLE for sample 3
+ 9:50 optimization (set -> native array) and system test passed (343.01 pt)
+ 
+ editorial:
+  - http://tatanaideyo.hatenablog.com/entry/2015/01/29/034637
+  - https://apps.topcoder.com/wiki/display/tc/SRM+646
+ 
+ key:
+  - shortest distance in a graph whose edges are same cost => BFS
+ 
+ summary:
+  - straightforward. I solved grid BFS problem yesterday in leetcode contest
+  - set is slow. native array is fast.
+ 
+ */
+int offset=1000;
+int viz[2005][2005];
+class TheGridDivTwo {
+public:
+  int find(vector<int> X, vector<int> Y, int K) {
+    set<II> B;
+    REP(i,SZ(X)) B.emplace(X[i],Y[i]);
+    
+    queue<III> Q; Q.emplace(0,0,0);
+    ZERO(viz);
+    viz[0+offset][0+offset]=1;
+    
+    int res=0;
+    while(SZ(Q)) {
+      int x,y,d;
+      tie(x,y,d)=Q.front(); Q.pop();
+      res=max(res,x);
+      if(d==K) continue;
+      
+      FORR(m,moves) {
+        int x2=x+m.first,y2=y+m.second;
+        if(viz[x2+offset][y2+offset]) continue;
+        if(B.count({x2,y2})) continue;
+        
+        viz[x2+offset][y2+offset]=1;
+        Q.emplace(x2,y2,d+1);
+      }
+    }
+    
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("TheGridDivTwo.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> x, vector<int> y, int k, int __expected) {
+    time_t startClock = clock();
+    TheGridDivTwo *instance = new TheGridDivTwo();
+    int __result = instance->find(x, y, k);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> x;
+        from_stream(x);
+        vector<int> y;
+        from_stream(y);
+        int k;
+        from_stream(k);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(x, y, k, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1505071959;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TheGridDivTwo (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
