73559239f402e132c8ab6da3277dff3340f4f3da
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-09-19 23:59:52 -0700

    solve QuadraticLaw SRM 635 div2 medium

---
+++ QuadraticLaw/QuadraticLaw.cpp
@@ -0,0 +1,199 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 9/19/2017
+ 
+ 8:30-8:40 system test passed (447.67 pt)
+ 23:50-23:54 add brute-force solution
+ 
+ res = max { x : f(x)<=d, f(x)=x^2+x x>=0 }
+ 
+ 0<=x<=y => f(x)<=f(y)
+ => f monotonically increases
+ => binary search
+ 
+ editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+635
+    - introducing brute force solution with O(√d) time
+    - introducing quadratic formula solution
+  - http://kmjp.hatenablog.jp/entry/2014/10/05/1000
+    - binary search
+  - http://sucrose.hatenablog.com/entry/2014/10/05/230214
+    - brute force
+  - http://torus711.hatenablog.com/entry/20141005/1412522367
+    - greedy
+ 
+ */
+
+// brute force, O(√d) time
+class QuadraticLaw {
+public:
+  long long getTime(long long d) {
+    LL x=0;
+    while(x*x+x<=d) ++x;
+    return x-1;
+  }
+};
+
+
+// binary search, O(lg d) time
+class QuadraticLaw_binarySearch {
+  public:
+  long long getTime(long long d) {
+    LL good=0,bad=1e9+5;
+    while(abs(good-bad)>1) {
+      LL mid=good+(bad-good)/2;
+      if(mid*mid+mid<=d) good=mid;
+      else bad=mid;
+    }
+    
+    return good;
+  }
+};
+
+// CUT begin
+ifstream data("QuadraticLaw.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(long long d, long long __expected) {
+    time_t startClock = clock();
+    QuadraticLaw *instance = new QuadraticLaw();
+    long long __result = instance->getTime(d);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        long long d;
+        from_stream(d);
+        next_line();
+        long long __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(d, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1505835013;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "QuadraticLaw (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
