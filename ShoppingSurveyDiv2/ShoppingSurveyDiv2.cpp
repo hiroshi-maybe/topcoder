f99771cbe98578ae99d3f4f0fa964fa76c40766b
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-09-21 00:26:38 -0700

    solve ShoppingSurveyDiv2 SRM 634 div2 medium

---
+++ ShoppingSurveyDiv2/ShoppingSurveyDiv2.cpp
@@ -0,0 +1,205 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 9/20/2017
+ 
+ 8:21-8:42 system test passed (345.03 pt)
+ 
+ editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+634
+  - http://torus711.hatenablog.com/entry/20140927/1411815200
+  - http://kmjp.hatenablog.jp/entry/2014/09/27/0900
+ 
+ 24:20-24:24 add solution of @kmjp and official editorial
+ 
+ */
+
+class ShoppingSurveyDiv2 {
+public:
+  int minValue(int N, vector<int> s) {
+    int M=SZ(s);
+    REP(n,N+1) {
+      int left=0,nn=N-n;
+      FORR(m,s) if(m>n) {
+        assert(nn>=m-n);
+        left+=m-n;
+      }
+      if((M-1)*nn>=left) return n;
+    }
+    return 0;
+  }
+};
+
+class ShoppingSurveyDiv2_org {
+  public:
+  int minValue(int N, vector<int> s) {
+    int M=SZ(s);
+    
+    int res=N,x=0;
+    REP(i,M) {
+      res=s[i]-x;
+      x=N-res;
+    }
+    return max(res,0);
+  }
+};
+
+// CUT begin
+ifstream data("ShoppingSurveyDiv2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int N, vector<int> s, int __expected) {
+    time_t startClock = clock();
+    ShoppingSurveyDiv2 *instance = new ShoppingSurveyDiv2();
+    int __result = instance->minValue(N, s);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int N;
+        from_stream(N);
+        vector<int> s;
+        from_stream(s);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, s, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1505920887;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "ShoppingSurveyDiv2 (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
