51980d9342dcb3b2c398368eb5b0b397da27d159
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-09-22 22:42:12 -0700

    solve PotentialGeometricSequence SRM 632 div2 medium

---
+++ PotentialGeometricSequence/PotentialGeometricSequence.cpp
@@ -0,0 +1,204 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 9/22/2017
+ 
+ 9:15-9:43 analysis
+ 9:44-9:49 implementation. system test passed (267.30 pt)
+ 
+ q*a[i]=a[i+1] => a*2^d[i]=b*2^[d+1]
+ 
+ We can select arbitrary number for `a` or `b`. We can just give 1.
+ Now condition that makes geometric sequence is 2^(d[i]+p)==2^d[i+1]
+ => d[i]+p = d[i+1] <=> d[i+1]-d[i]=p
+ It turns out that we can check if d[i..j] forms arithmetic sequence
+ 
+ editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+632
+    - proof
+  - http://kmjp.hatenablog.jp/entry/2014/09/05/0930
+  - http://sucrose.hatenablog.com/entry/2014/09/05/235244
+ 
+ key:
+  - prove that arithmetic sequence satisfiles problem statement
+
+ summary:
+  - analysis and observation problem
+  - it took long time to find property of arithmetic sequence 👎
+    - eventually figured out from samples
+    - I should have tried sample earlier without transforming formulas first
+ 
+ */
+
+// O(N^2) time
+class PotentialGeometricSequence {
+  public:
+  int numberOfSubsequences(vector<int> D) {
+    int N=SZ(D);
+    int res=N;
+    REP(i,N-1) {
+      int j=i+1,d=D[j]-D[i];
+      while(j<N&&D[j]-D[j-1]==d) ++j;
+      res+=j-i-1;
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("PotentialGeometricSequence.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> d, int __expected) {
+    time_t startClock = clock();
+    PotentialGeometricSequence *instance = new PotentialGeometricSequence();
+    int __result = instance->numberOfSubsequences(d);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> d;
+        from_stream(d);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(d, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1506096935;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "PotentialGeometricSequence (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
