5537329e1cbc76cb8697835bf76a68dfcb6bf4f7
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-09-26 00:22:22 -0700

    solve DengklekGaneshAndTree SRM 629 div2 easy

---
+++ RectangleCoveringEasy/RectangleCoveringEasy.cpp
@@ -0,0 +1,173 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 9/25/2017
+ 
+ 8:41-8:47 system test passed (237.46 pt)
+ 
+ editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+629
+ 
+ */
+class RectangleCoveringEasy {
+  public:
+  bool ok(int x1, int y1, int x2, int y2) {
+    return (x1<=x2&&y1<y2)||(x1<x2&&y1<=y2);
+  }
+  int solve(int holeH, int holeW, int boardH, int boardW) {
+    return ok(holeH,holeW,boardH,boardW)||ok(holeH,holeW,boardW,boardH)?1:-1;
+  }
+};
+
+// CUT begin
+ifstream data("RectangleCoveringEasy.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int holeH, int holeW, int boardH, int boardW, int __expected) {
+    time_t startClock = clock();
+    RectangleCoveringEasy *instance = new RectangleCoveringEasy();
+    int __result = instance->solve(holeH, holeW, boardH, boardW);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int holeH;
+        from_stream(holeH);
+        int holeW;
+        from_stream(holeW);
+        int boardH;
+        from_stream(boardH);
+        int boardW;
+        from_stream(boardW);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(holeH, holeW, boardH, boardW, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1506354057;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "RectangleCoveringEasy (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
