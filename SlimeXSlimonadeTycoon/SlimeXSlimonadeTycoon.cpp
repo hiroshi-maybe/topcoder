12fe84ef1694a76254a935725d07d82ba8eb7838
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-10-09 01:25:16 -0700

    solve SlimeXSlimonadeTycoon SRM 617 div2 medium

---
+++ SlimeXSlimonadeTycoon/SlimeXSlimonadeTycoon.cpp
@@ -0,0 +1,208 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 10/8/2017
+ 
+ 10:04-10:16 design
+ 10:16-10:28 implement and system test passed (325.92 pt)
+ 
+ editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+617
+  - http://torus711.hatenablog.com/entry/20140421/1398096617
+   - queue
+ 
+ submit solutions:
+  - simple O(N^2) solution
+   - https://community.topcoder.com/stat?c=coder_room_stats&rd=15850&cr=22756223
+   - https://community.topcoder.com/stat?c=problem_solution&rm=321802&rd=15850&pm=11281&cr=23168966
+  - queue
+   - https://community.topcoder.com/stat?c=problem_solution&rm=321823&rd=15850&pm=11281&cr=23289104
+ 
+ */
+// greedy, O(N) time
+class SlimeXSlimonadeTycoon {
+  public:
+  int sell(vector<int> A, vector<int> B, int K) {
+    deque<II> S;
+    int N=SZ(A);
+    int res=0,sum=0;
+    REP(i,N) {
+      if(SZ(S)&&(i-S.front().first)>=K) sum-=S.front().second, S.pop_front();
+      S.emplace_back(i,A[i]); sum+=A[i];
+      int sell=min(sum,B[i]);
+//      dump4(i,sum,B[i],sell);
+      res+=sell; sum-=sell;
+      while(SZ(S)&&sell>0) {
+        int x=min(S.front().second,sell);
+        sell-=x; S.front().second-=x;
+        if(S.front().second==0) S.pop_front();
+      }
+//      dump3(i,sell,SZ(S));
+      assert(sell==0);
+    }
+    
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("SlimeXSlimonadeTycoon.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> morning, vector<int> customers, int stale_limit, int __expected) {
+    time_t startClock = clock();
+    SlimeXSlimonadeTycoon *instance = new SlimeXSlimonadeTycoon();
+    int __result = instance->sell(morning, customers, stale_limit);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> morning;
+        from_stream(morning);
+        vector<int> customers;
+        from_stream(customers);
+        int stale_limit;
+        from_stream(stale_limit);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(morning, customers, stale_limit, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1507482262;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "SlimeXSlimonadeTycoon (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
