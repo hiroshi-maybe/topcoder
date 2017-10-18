9fa288ac9f53fb297a0739f044e6686400d75958
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-10-18 00:47:07 -0700

    solve DivideByZero SRM 610 div2 easy

---
+++ DivideByZero/DivideByZero.cpp
@@ -0,0 +1,208 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 10/17/2017
+ 
+ 8:34-9:03 analysis
+ 9:03-9:08 implement and system test passed (122.37 pt)
+ 
+ editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+610
+  - http://topcoder.g.hatena.ne.jp/kagamiz/20140305
+  - http://torus711.hatenablog.com/entry/20140226/p2
+ 
+ summary:
+  - It took time to find algorithm where analysis of running time is more obvious
+ 
+ */
+class DivideByZero {
+public:
+  int CountNumbers(vector<int> ns) {
+    SETI S;
+    FORR(n,ns) S.emplace(n);
+    while(true) {
+      SETI S2(S.begin(),S.end());
+      FORR(n,S) FORR(m,S) if(n>m) S2.emplace(n/m);
+      if(SZ(S)==SZ(S2)) break;
+      S=S2;
+    }
+
+    return SZ(S);
+  }
+};
+
+class DivideByZero_wrong {
+  public:
+  int CountNumbers(vector<int> ns) {
+    SETI S;
+    set<int, greater<int>> Q;
+    FORR(n,ns) Q.emplace(n), S.emplace(n);
+    while(SZ(Q)) {
+      auto it=Q.begin();
+      int cur=*it; Q.erase(it);
+//      dump(cur);
+      S.emplace(cur);
+      FORR(s,S) if(cur>s&&cur/s!=cur) Q.emplace(cur/s);
+    }
+    dumpAR(S);
+    return SZ(S);
+  }
+};
+
+// CUT begin
+ifstream data("DivideByZero.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> numbers, int __expected) {
+    time_t startClock = clock();
+    DivideByZero *instance = new DivideByZero();
+    int __result = instance->CountNumbers(numbers);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> numbers;
+        from_stream(numbers);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(numbers, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1508254159;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "DivideByZero (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
