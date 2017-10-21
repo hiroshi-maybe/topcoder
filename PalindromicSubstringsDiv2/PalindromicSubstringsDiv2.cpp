0dd698d196f92bf87940fe33d5d835300d38ce22
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-10-20 23:02:53 -0700

    solve PalindromicSubstringsDiv2 SRM 607 div2 medium

---
+++ PalindromicSubstringsDiv2/PalindromicSubstringsDiv2.cpp
@@ -0,0 +1,272 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 10/20/2017
+ 
+ 8:48-8:57 running time analysis mistake...(452.33 pt)
+ 9:19 resubmit (225.28 pt)
+ 
+ editorial:
+  - http://torus711.hatenablog.com/entry/20140203/p3
+   - O(L) space simple solution
+  - https://apps.topcoder.com/wiki/display/tc/SRM+607
+ 
+ 22:50-22:56 add O(1) space solution
+ 
+ submit solutions:
+  - O(1) space
+    - https://community.topcoder.com/stat?c=problem_solution&rm=320463&rd=15840&pm=12967&cr=22682720
+    - https://community.topcoder.com/stat?c=problem_solution&rm=320463&rd=15840&pm=12967&cr=23285916
+    - https://community.topcoder.com/stat?c=problem_solution&rm=320476&rd=15840&pm=12967&cr=23177326
+    - https://community.topcoder.com/stat?c=problem_solution&rm=320474&rd=15840&pm=12967&cr=23127720
+ 
+ key:
+  - Once start is determined, it's possible to count palidromes in linear time
+ 
+ summary:
+ - Somehow I mistook calculation 50*50=250 :(
+ 
+ */
+
+class PalindromicSubstringsDiv2 {
+public:
+  string S;
+  int N;
+  int count(vector<string> S1, vector<string> S2) {
+    string S="";
+    FORR(s,S1) S+=s;
+    FORR(s,S2) S+=s;
+    this->S=S;
+    this->N=SZ(S);
+    
+    int res=0;
+    REP(i,N) {
+      res+=f(i,i);
+      res+=f(i,i+1);
+    }
+    return res;
+  }
+  
+  int f(int l, int r) {
+    int res=0;
+    while(0<=l&&r<N) {
+      if(S[l--]!=S[r++]) return res;
+      ++res;
+    }
+    return res;
+  }
+};
+
+int dp[5001][5001];
+class PalindromicSubstringsDiv2_org {
+public:
+  string S;
+  int N;
+  int count(vector<string> S1, vector<string> S2) {
+    string S="";
+    FORR(s,S1) S+=s;
+    FORR(s,S2) S+=s;
+    this->S=S;
+    this->N=SZ(S);
+    
+    ZERO(dp);
+    REP(i,N) {
+      dp[i][i]=1;
+      if(i<N)dp[i][i+1]=1;
+    }
+    REP(l,N) REP(i,N) if(dp[i][i+l]&&i-1>=0&&i+l<N) {
+      if(S[i-1]==S[i+l]) dp[i-1][i+l+1]=1;
+    }
+    int res=0;
+    REP(i,N)FOR(j,i+1,N+1) {
+//       dump3(i,j,dp[i][j]);
+      res+=dp[i][j];
+    }
+    return res;
+  }
+};
+
+class PalindromicSubstringsDiv2_tle {
+  public:
+  
+  bool ok(string s) {
+    int l=0,r=SZ(s)-1;
+    while(l<r) {
+      if(s[l++]!=s[r--]) return false;
+    }
+    return true;
+  }
+  
+  int count(vector<string> S1, vector<string> S2) {
+    string S="";
+    FORR(s,S1) S+=s;
+    FORR(s,S2) S+=s;
+    
+    int N=SZ(S);
+    int res=0;
+    REP(i,N) FOR(j,i+1,N+1) {
+      res+=ok(S.substr(i,j-i));
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("PalindromicSubstringsDiv2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> S1, vector<string> S2, int __expected) {
+    time_t startClock = clock();
+    PalindromicSubstringsDiv2 *instance = new PalindromicSubstringsDiv2();
+    int __result = instance->count(S1, S2);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> S1;
+        from_stream(S1);
+        vector<string> S2;
+        from_stream(S2);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(S1, S2, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1508514481;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "PalindromicSubstringsDiv2 (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
