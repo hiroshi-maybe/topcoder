fa2c25e61c446f8c330b0e829b19851cbff3dd5c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-10-25 00:09:48 -0700

    add Ford-Fulkerson method in maxflow library

--- lib/maxflow.cpp
+++ lib/maxflow.cpp
@@ -9,33 +9,47 @@ using namespace std;
  
  Flow prolems
  
- 1. Max-flow problem: https://en.wikipedia.org/wiki/Maximum_flow_problem
-  - maximize flow from source to sink in given capacity constraint
-  - max flow equals to
-    - Max-flow min-cut theorem: https://en.wikipedia.org/wiki/Max-flow_min-cut_theorem
-  - Solutions:
-    - The Ford-Fulkerson method in O(F*E) time
-     - greedily 
-    - Dinic's algorithm in O(E*V^2) time
+ 1. Max-flow problem
+ 
+  - Maximize flow from source to sink in given capacity constraint.
+  - Max-flow equals to min-cut
+  - bipartite matching is a special case of max-flow problem
+ 
+  Algorithms:
+   - Naiive Ford-Fulkerson method in O(F*E) time
+    - iteratively increases the value of the flow by finding a path by dfs
+   - Dinic's algorithm in O(E*V^2) time
+    - optimized Ford-Fulkerson method by pre-computing shortest path from source
 
- 2. Minimum-cost flow problem: https://en.wikipedia.org/wiki/Minimum-cost_flow_problem
+  References:
+   - https://en.wikipedia.org/wiki/Maximum_flow_problem
+   - Max-flow min-cut theorem: https://en.wikipedia.org/wiki/Max-flow_min-cut_theorem
+   - CLRS 26.2 The Ford-Fulkerson method
+   - CLRS 26.3 Maximum bipartite matching
+ 
+ 2. Minimum-cost flow problem
+ 
   - flow `d` is given while computation of flow is goal in max-flow problem
   - compute min-cost which achieves flow `d`
   - special case of other problems
    - if there is no capacity constraint, it's reduced to the shortest path problem
    - if cost is zero, it's reduced to the max flow problem
-  - Solutions:
-   - Dijkstra's algorithm in O(F*E*lg V) time
-   - linear programming as shown in CLRS 29.2 Formulating problems as linear programs
+ 
+  Algorithms:
+  - Dijkstra's algorithm in O(F*E*lg V) time
+  - linear programming as shown in CLRS 29.2 Formulating problems as linear programs
+ 
+ References:
+  - https://en.wikipedia.org/wiki/Minimum-cost_flow_problem
  
  */
 
 /*
  
- Max flow algorithm Dinic, O(E*V^2)
+ Dinic's max flow algorithm, O(E*V^2)
  
  Usage:
-  // Don't miss updating MAX_V constant
+  // Don't miss to update MAX_V constant
   
   // initialize
   Dinic<int> f;
@@ -48,74 +62,130 @@ using namespace std;
   flow = f.maxFlow(source, sink)
  
  */
-#define MAX_V 100
+const int MAX_V=100;
 template<class V> class Dinic {
 public:
   void addEdge(int from, int to, V cap) {
     E[from].push_back((Edge){  to, (int)E[  to].size()  , cap});
-    E[  to].push_back((Edge){from, (int)E[from].size()-1,   0}); // residual graph
+    E[  to].push_back((Edge){from, (int)E[from].size()-1,   0}); // cancellation edge in residual graph
   }
   
   V maxFlow(int s, int t) {
-    V flow = 0;
-    while (true) {
+    V res=0;
+    while(true) {
       bfs(s);
-      if (level[t] < 0) return flow; // t is unreachable
-      
-      memset(iter, 0, sizeof(iter));
-      int f;
-      while ((f = dfs(s, t, numeric_limits<V>::max())) > 0) {
-        flow += f;
-      }
+      if(dist[t]<0) return res; // t is unreachable
+      memset(iter,0,sizeof(iter));
+      V f;
+      while((f=dfs(s,t,Inf))>0) res+=f;
     }
     
-    return flow;
+    return res;
   }
 private:
+  const V Inf=numeric_limits<V>::max();
   struct Edge { int to, rev; V cap; };
   vector<Edge> E[MAX_V]; // Graph
-  int level[MAX_V]; // distance from `s`
+  int dist[MAX_V]; // distance from `s`
   int iter[MAX_V];
   
   // initialize shoftest path and store in `level`
   void bfs(int s) {
-    memset(level, -1, sizeof(level));
+    memset(dist,-1,sizeof(dist));
     
     queue<int> Q;
-    level[s] = 0;
+    dist[s]=0;
     Q.push(s);
     
-    while (!Q.empty()) {
-      int u = Q.front(); Q.pop();
+    while(Q.size()) {
+      int u=Q.front(); Q.pop();
       
-      for (int i = 0; i < E[u].size(); i++) {
-        Edge &e = E[u][i];
-        int v = e.to;
-        if (e.cap<=0) continue;
-        if (level[v]>=0) continue; // visited
+      for(int i=0; i<E[u].size(); ++i) {
+        Edge &e=E[u][i];
+        int v=e.to;
+        if(e.cap<=0) continue;
+        if(dist[v]>=0) continue; // visited
         
-        level[v] = level[u] + 1;
+        dist[v]=dist[u]+1;
         Q.push(v);
       }
     }
   }
   
   // find augmenting path in residual network and update f
-  int dfs(int u, int t, V f) {
-    if (u == t) return f;
-    for (int &i = iter[u]; i < E[u].size(); i++) {
-      Edge &e = E[u][i];
-      V v = e.to;
-      if (e.cap <= 0) continue;
-      if (level[u] >= level[v]) continue;
+  V dfs(int u, int t, V f) {
+    if(u==t) return f;
+    for(int &i=iter[u]; i<E[u].size(); ++i) { // visit E[u] only once
+      Edge &e=E[u][i];
+      int v=e.to;
+      if(e.cap<=0) continue;
+      if(dist[u]>=dist[v]) continue;
       
-      int d = dfs(v, t, min(f, e.cap));
-      if (d <= 0) continue;
-      e.cap -= d;
-      E[v][e.rev].cap += d;
+      V d=dfs(v,t,min(f,e.cap));
+      if(d<=0) continue;
+      e.cap-=d;
+      E[v][e.rev].cap+=d; // cancellation increases
       return d;
     }
     
+    return 0;
+  }
+};
+
+/*
+ 
+ Naiive Ford-Fulkerson method to compute max flow, O(F*E) time
+ 
+ Usage:
+  // Don't miss to update MAX_V constant
+ 
+  // initialize
+  FordFulkerson<int> ff;
+ 
+  // configure edges with capacity
+  ff.addEdge(u0,v0,c0);
+  ff.addEdge(u1,v1,c1);
+ 
+  // compute flow
+  flow = ff.maxFlow(source, sink)
+ 
+ */
+template<class V> class FordFulkerson {
+public:
+  void addEdge(int from, int to, V cap) {
+    E[from].push_back((Edge){  to, (int)E[  to].size()  , cap});
+    E[  to].push_back((Edge){from, (int)E[from].size()-1,   0}); // cancellation edge in residual graph
+  }
+  
+  V maxFlow(int s, int t) {
+    V res=0;
+    while(true) {
+      memset(viz,0,sizeof(viz));
+      V f=dfs(s,t,Inf);
+      if(f==0) return res;
+      res+=f;
+    }
+    return res;
+  }
+private:
+  const V Inf=numeric_limits<V>::max();
+  struct Edge { int to, rev; V cap; };
+  vector<Edge> E[MAX_V]; // Graph
+  bool viz[MAX_V];
+  
+  // find augmenting path in residual network and update f
+  V dfs(int v, int t, V f) {
+    if(v==t) return f;
+    viz[v]=true;
+    for(int i=0; i<E[v].size(); ++i) if(!viz[E[v][i].to]&&E[v][i].cap>0) {
+      Edge &e=E[v][i];
+      V d=dfs(e.to,t,min(f,e.cap));
+      if(d==0) continue;
+      
+      e.cap-=d;
+      E[e.to][e.rev].cap+=d;
+      return d;
+    }
     return 0;
   }
 };
@@ -132,12 +202,16 @@ int main(int argc, char const *argv[]) {
   };
   
   Dinic<int> f;
+  FordFulkerson<int> ff;
   for(int u=0; u<C.size(); ++u) {
     for(int v=0; v<C.size(); ++v) {
       if (C[u][v]==0) continue;
       f.addEdge(u,v,C[u][v]);
+      ff.addEdge(u,v,C[u][v]);
     }
   }
   int maxf = f.maxFlow(0,C.size()-1);
   assert(maxf == 23);
+  int maxff = ff.maxFlow(0,C.size()-1);
+  assert(maxff == 23);
 }
