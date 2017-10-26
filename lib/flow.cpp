188bd020966f1e1bad7fb4ea18c236211dbe7db0
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-10-26 00:10:43 -0700

    add min-cost flow to library

--- lib/flow.cpp
+++ lib/flow.cpp
@@ -31,16 +31,24 @@ using namespace std;
  
   - flow `d` is given while computation of flow is goal in max-flow problem
   - compute min-cost which achieves flow `d`
+  - build residual graph with (cap, cost) and increase f following shortest path of cost
+   - initial (cap, cost) of reverse edge is (0, -cost)
+   - edge is valid path only when cap>0
   - special case of other problems
    - if there is no capacity constraint, it's reduced to the shortest path problem
    - if cost is zero, it's reduced to the max flow problem
+  - minimum cost bipartite matching is a special case of minimum-flow problem
+   - Ant book 3-5 network flow
  
   Algorithms:
   - Dijkstra's algorithm in O(F*E*lg V) time
   - linear programming as shown in CLRS 29.2 Formulating problems as linear programs
  
  References:
-  - https://en.wikipedia.org/wiki/Minimum-cost_flow_problem
+  - Minimum cost flow
+    - https://en.wikipedia.org/wiki/Minimum-cost_flow_problem
+  - Minimum cost bipartite matching
+    - http://theory.stanford.edu/~tim/w16/l/l5.pdf
  
  */
 
@@ -150,6 +158,7 @@ private:
   flow = ff.maxFlow(source, sink)
  
  */
+// const int MAX_V=100;
 template<class V> class FordFulkerson {
 public:
   void addEdge(int from, int to, V cap) {
@@ -221,6 +230,7 @@ private:
   int matching = BM.solve()
  
  */
+// const int MAX_V=100;
 class MaxBipartiteMatching {
 public:
   MaxBipartiteMatching(int V) : V(V) {}
@@ -260,6 +270,87 @@ private:
     }
     return false;
   }
+};
+
+/*
+ 
+ Solver of minimum-cost flow problem, O(F*E*lg V) time
+ 
+  - build residual graph with (cap, cost) and increase f following shortest path of cost
+  - Ant book 3-5 network flow
+ 
+ Usage:
+ 
+  MinCostFlow f(V);
+  f.addEdge(u0,v0,cap0,cost0);
+  f.addEdge(u1,v1,cap1,cost1);
+ 
+  int mcost=f.solve(s,t,F);
+
+ */
+// const int MAX_V=100;
+struct MinCostFlow {
+public:
+  MinCostFlow(int V) : V(V) {}
+  
+  void addEdge(int from, int to, int cap, int cost) {
+    E[from].push_back((Edge){  to, static_cast<int>(E[to].size()),     cap,  cost});
+    E[  to].push_back((Edge){from, static_cast<int>(E[from].size()-1),   0, -cost}); // rev edge with (cap,cost)=(0,-cost)
+  }
+  
+  int solve(int s, int t, int f) {
+    int res=0;
+    fill(h,h+V,0);
+    while(f>0) {
+      priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> Q;
+      fill(dist,dist+V,Inf);
+      dist[s]=0;
+      Q.push({0,s});
+      while(Q.size()) {
+        auto p=Q.top(); Q.pop();
+        int u=p.second;
+        if(dist[u]<p.first) continue;
+        for(int i=0; i<E[u].size(); ++i) {
+          auto &e=E[u][i];
+          int v=e.to;
+          if(e.cap<=0) continue;
+          int dd=dist[u]+e.cost+h[u]-h[v];
+          if(dist[v]<=dd) continue;
+          dist[v]=dd;
+          prev_v[v]=u; prev_e[v]=i;
+          Q.push({dist[v],v});
+        }
+      }
+      
+      // capacity is exhausted. no longer incrase flow
+      if(dist[t]==Inf) return -1;
+      
+      for(int u=0; u<V; ++u) h[u]+=dist[u];
+      
+      // increase flow as much as possible on the shortest path
+      int d=f;
+      for(int v=t; v!=s; v=prev_v[v]) {
+        // backtrack shortest path
+        d=min(d,E[prev_v[v]][prev_e[v]].cap);
+      }
+      f-=d;
+      res+=d*h[t];
+      for(int v=t; v!=s; v=prev_v[v]) {
+        auto &e=E[prev_v[v]][prev_e[v]];
+        e.cap-=d;
+        E[v][e.rev].cap+=d;
+      }
+    }
+    return res;
+  }
+private:
+  const int Inf = numeric_limits<int>::max();
+  int V;
+  struct Edge { int to, rev, cap, cost; };
+  vector<Edge> E[MAX_V]; // Graph
+  int h[MAX_V]; // potential
+  int dist[MAX_V];
+  int prev_v[MAX_V], prev_e[MAX_V];
 };
 
 int main(int argc, char const *argv[]) {  
@@ -306,4 +397,22 @@ int main(int argc, char const *argv[]) {
   for(int v=5; v<9; ++v) f2.addEdge(v,sink,1);
   for(auto e : es) f2.addEdge(e.first,e.second,1);
   assert(f2.maxFlow(source,sink)==mm); // 3
+  
+  // Ant book 3-5 network flow
+  vector<vector<tuple<int,int,int>>> G = {
+    // u->{v,cap,cost}
+    { {1,10,2}, {2,2,4} },
+    { {2, 6,6}, {3,6,2} },
+    { {4, 5,2} },
+    { {2,3,3}, {4,8,6} },
+    { },
+  };
+  
+  MinCostFlow f(G.size());
+  for(int u=0; u<G.size(); ++u) for(auto t : G[u]) {
+    int v,cap,cost;
+    tie(v,cap,cost)=t;
+    f.addEdge(u,v,cap,cost);
+  }
+  assert(f.solve(0,4,9)==80);
 }
