e017361c2b1f56200831f679b42f8969868c21ac
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-11-03 01:30:15 -0700

    solve BigFatInteger2 SRM 599 div2 medium

---
+++ BigFatInteger2/BigFatInteger2.cpp
@@ -0,0 +1,206 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+vector<LL> primeFactors(LL n) {
+  assert(n>=1);
+  vector<LL> res;
+  for(LL p=2; p*p<=n; ++p) while(n%p==0) {
+    res.push_back(p);
+    n/=p;
+  }
+  if(n>1) res.push_back(n);
+  return res;
+}
+
+/*
+ 
+ 11/2/2017
+ 
+ 19:36-19:46 system test passed (443.08 pt)
+ 
+ editorial:
+  - http://topcoder.g.hatena.ne.jp/evima/20131210/1386698305
+   - writer's editorial
+   - writer's comment: https://twitter.com/evima0/status/408293429181825024
+  - http://kmjp.hatenablog.jp/entry/2013/12/05/1130
+  - https://apps.topcoder.com/wiki/display/tc/SRM+599
+  - http://torus711.hatenablog.com/entry/20131204/p2
+ 
+ Key:
+  - prime decomposition
+ 
+ Summary:
+  - great quick analysis and coding 👍
+ 
+ */
+class BigFatInteger2 {
+  public:
+  string yes= "divisible",no="not divisible";
+  string isDivisible(int A, int B, int C, int D) {
+    vector<LL> ps1=primeFactors(A),ps2=primeFactors(C);
+    unordered_map<LL,LL> M1,M2;
+    REP(i,SZ(ps1)) M1[ps1[i]]++;
+    REP(i,SZ(ps2)) M2[ps2[i]]++;
+    FORR(kvp,M1) kvp.second*=B;
+    FORR(kvp,M2) kvp.second*=D;
+    
+    FORR(kvp,M2) {
+      LL p=kvp.first,n=kvp.second;
+      if(M1.count(p)==0) return no;
+      if(M1[p]<n) return no;
+    }
+    
+    return yes;
+  }
+};
+
+// CUT begin
+ifstream data("BigFatInteger2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int A, int B, int C, int D, string __expected) {
+    time_t startClock = clock();
+    BigFatInteger2 *instance = new BigFatInteger2();
+    string __result = instance->isDivisible(A, B, C, D);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int A;
+        from_stream(A);
+        int B;
+        from_stream(B);
+        int C;
+        from_stream(C);
+        int D;
+        from_stream(D);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(A, B, C, D, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1509676564;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "BigFatInteger2 (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
