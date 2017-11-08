8aacf896eaa785b61a06a0d5c62e32324e7dd636
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-11-08 01:30:00 -0800

    solve FoxAndClassroom SRM 594 div2 medium

---
+++ AstronomicalRecordsEasy/AstronomicalRecordsEasy.cpp
@@ -0,0 +1,201 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 11/7/2017
+ 
+ 9:07-9:24 analysis
+ 9:25-9:31 1WA (320.44 pt)
+ 10:26-10:28 system test passed (missing case in analysis)
+ 
+ editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+594
+ 
+ Summary:
+  - I missed to check all the pairs (1WA)
+  - I should have thought I should try all the pairs (fair runtime)
+  - However I skipped it without proof just because code passed samples 👎
+ 
+ */
+int gcd(int a,int b) { return b==0?a:gcd(b,a%b); }
+int lcm(int a, int b) { return a*b/gcd(a,b); }
+class AstronomicalRecordsEasy {
+  public:
+  int N,M;
+  int f(int a, int b, VI &A, VI &B) {
+    VI AA=A,BB=B;
+    SETI S;
+    REP(i,N) AA[i]*=a,S.emplace(AA[i]);
+    REP(i,M) BB[i]*=b,S.emplace(BB[i]);
+    return SZ(S);
+  }
+  int minimalPlanets(vector<int> A, vector<int> B) {
+    this->N=SZ(A),this->M=SZ(B);
+    int res=N+M;
+    REP(i,N) REP(j,M) {
+      int x=lcm(A[i],B[j]);
+      int a=x/A[i],b=x/B[j];
+      res=min(res,f(a,b,A,B));
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("AstronomicalRecordsEasy.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> A, vector<int> B, int __expected) {
+    time_t startClock = clock();
+    AstronomicalRecordsEasy *instance = new AstronomicalRecordsEasy();
+    int __result = instance->minimalPlanets(A, B);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> A;
+        from_stream(A);
+        vector<int> B;
+        from_stream(B);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(A, B, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1510074446;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "AstronomicalRecordsEasy (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
