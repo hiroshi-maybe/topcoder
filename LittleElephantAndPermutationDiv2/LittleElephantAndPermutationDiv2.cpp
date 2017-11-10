7b46a93b4be013da62dcbe01816d84776f35db4f
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-11-09 21:56:46 -0800

    solve LittleElephantAndPermutationDiv2 SRM 592 div2 medium

---
+++ LittleElephantAndPermutationDiv2/LittleElephantAndPermutationDiv2.cpp
@@ -0,0 +1,191 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 11/9/2017
+ 
+ 8:28-8:44 system test passed (388.34 pt)
+ 
+ P1(A): Generate permutation of sequence
+ P2(A,B): Compute ∑ max(A[i],B[i])
+ 
+ T(P1)=10!≈4*10^6
+ T(P2)=10
+ 
+ T(P1(A))*T(P1(B))*T(P2)=2*10^14>10^9.
+ 
+ Actually we don't need to solve both P1 twice
+ because permutation of A does not affect result of P2(A,B).
+ 
+ P = P1(A)*P2(A,B)*P3(N), P3: computation of N!
+ T(P)=T(P1)*T(P2)*T(P3)=10!*10*10≈4*10^8
+ 
+ Editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+592
+  - http://torus711.hatenablog.com/entry/20130927/p2
+ 
+ */
+class LittleElephantAndPermutationDiv2 {
+  public:
+  long long getNumber(int N, int K) {
+    VI B(N);
+    REP(i,N)B[i]=i+1;
+    LL res=0;
+    do {
+      int sum=0;
+      REP(i,N) sum+=max(i+1,B[i]);
+      res+=sum>=K;
+    } while(next_permutation(B.begin(),B.end()));
+    
+    FOR(n,1,N+1) res*=n;
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("LittleElephantAndPermutationDiv2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int N, int K, long long __expected) {
+    time_t startClock = clock();
+    LittleElephantAndPermutationDiv2 *instance = new LittleElephantAndPermutationDiv2();
+    long long __result = instance->getNumber(N, K);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int N;
+        from_stream(N);
+        int K;
+        from_stream(K);
+        next_line();
+        long long __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, K, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1510244896;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "LittleElephantAndPermutationDiv2 (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
