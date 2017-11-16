4a48e6b4409b8cabce802014c346f50d5a0dba8b
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-11-15 22:23:52 -0800

    solve JumpFurther SRM 587 div2 medium

---
+++ JumpFurther/JumpFurther.cpp
@@ -0,0 +1,194 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 11/15/2017
+ 
+ 9:29-9:45 analysis
+ 9:46-9:49 submit and system test passed
+ 
+ DP does not work due to large N. So I looked for greedy algorithm.
+ 
+ If ∑ { i: i=1..N } does not hit `NG` step, that's optimal step.
+ Suppose p-th step is NG.
+ 
+ ∑ { i: i=1..p } = p(p+1)/2 = NG
+ We want to remove as small step as possible.
+ It's obviously first step `1`.
+ Next question is that is it possible to hit NG again after removing step `1`?
+ The answer is no. If first step is removed, p-th step is p(p+1)/2-1 = NG-1 ≠ NG
+ 
+ Editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+587
+  - http://kmjp.hatenablog.jp/entry/2013/08/03/0900
+  - http://emkcsharp.hatenablog.com/entry/SRM587/JumpFurther
+ 
+ Key:
+  - Use all the steps or remove step 1
+ 
+ Summary:
+  - proved correctness of my greedy by myself 👍
+ 
+ */
+class JumpFurther {
+public:
+  int furthest(int N,
+               int NG) {
+    bool ok=true;
+    int sum=0;
+    FOR(i,1,N+1) {
+      sum+=i;
+      if(sum==NG) ok=false;
+    }
+    //    cout << sum<<endl;
+    return ok?sum:sum-1;
+  }
+};
+
+// CUT begin
+ifstream data("JumpFurther.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int N, int badStep, int __expected) {
+    time_t startClock = clock();
+    JumpFurther *instance = new JumpFurther();
+    int __result = instance->furthest(N, badStep);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int N;
+        from_stream(N);
+        int badStep;
+        from_stream(badStep);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, badStep, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1510800392;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "JumpFurther (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
