c64abc636ed5d565b7317e784250834456643ce1
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-12-10 17:32:33 -0800

    solve TheExperimentDiv2 SRM 576 div2 easy

---
+++ TheExperimentDiv2/TheExperimentDiv2.cpp
@@ -0,0 +1,202 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 12/10/2017
+ 
+ 15:36-15:43 system test passed (238.81 pt)
+ 
+ Editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+576
+  - http://tatsyblog.sakura.ne.jp/wordpress/contests/topcoder/950/
+   - Search from spongy to drop counter. Cool idea.
+  - http://ekaing.hatenablog.com/entry/2013/04/12/201613
+ 
+ */
+class TheExperimentDiv2 {
+  public:
+  vector<int> determineHumidity(vector<int> X, int L, vector<int> Y) {
+    int N=SZ(X);
+    VI T(N,-1);
+    int M=SZ(Y);
+    for(int i=M-1; i>=0; --i) {
+      for(int x=Y[i]; x<Y[i]+L; ++x) T[x]=i;
+    }
+    
+    VI res(M,0);
+    REP(i,N) if(T[i]>=0) res[T[i]]+=X[i];
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("TheExperimentDiv2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(vector<int> intensity, int L, vector<int> leftEnd, vector<int> __expected) {
+    time_t startClock = clock();
+    TheExperimentDiv2 *instance = new TheExperimentDiv2();
+    vector<int> __result = instance->determineHumidity(intensity, L, leftEnd);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> intensity;
+        from_stream(intensity);
+        int L;
+        from_stream(L);
+        vector<int> leftEnd;
+        from_stream(leftEnd);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(intensity, L, leftEnd, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1512948946;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 256 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TheExperimentDiv2 (256 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
