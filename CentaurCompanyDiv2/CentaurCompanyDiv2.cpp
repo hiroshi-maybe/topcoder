0c3f119f31d459c611416689275c6f806db0c164
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-12-16 16:40:43 -0800

    solve CentaurCompanyDiv2 SRM 570 div2 hard

---
+++ CentaurCompanyDiv2/CentaurCompanyDiv2.cpp
@@ -0,0 +1,208 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 12/16/2017
+ 
+ 15:23-15:41 analysis
+ 15:42-15:50 system test passed (586.64 pt)
+ 
+ Editorial:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+570
+  - http://kmjp.hatenablog.jp/entry/2013/02/15/1000
+  - https://topcoder.g.hatena.ne.jp/firewood/20130222/1361508122
+  - http://ekaing.hatenablog.com/entry/2013/04/02/180925
+ 
+ Key:
+  - tree needs to be connected
+  - typical tree DP
+ 
+ Summary:
+  - I missed to reset edges again 😵
+ 
+ */
+LL memo[52];
+VI E[52];
+class CentaurCompanyDiv2 {
+  public:
+  LL ans=1;
+  LL f(int u, int pre) {
+    LL &res=memo[u];
+    if(res>=0) return res;
+    
+    res=1LL;
+    FORR(v,E[u]) if(v!=pre) res*=f(v,u)+1LL;
+    ans+=res;
+    return res;
+  }
+  long long count(vector<int> A, vector<int> B) {
+    MINUS(memo);
+    REP(i,52) E[i].clear();
+    REP(i,SZ(A)) {
+      int u=A[i],v=B[i];
+      E[u].push_back(v),E[v].push_back(u);
+    }
+    
+    f(1,-1);
+    return ans;
+  }
+};
+
+// CUT begin
+ifstream data("CentaurCompanyDiv2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> a, vector<int> b, long long __expected) {
+    time_t startClock = clock();
+    CentaurCompanyDiv2 *instance = new CentaurCompanyDiv2();
+    long long __result = instance->count(a, b);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> a;
+        from_stream(a);
+        vector<int> b;
+        from_stream(b);
+        next_line();
+        long long __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(a, b, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1513466587;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 1000 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "CentaurCompanyDiv2 (1000 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
