fd37f6bb123fd6a4e019f068e3e96a2569168825
Hiroshi Kori <hiroshi.maybe@gmail.com>
2017-12-17 16:10:44 -0800

    solve TheDeviceDiv2 SRM 569 div2 medium

---
+++ TheDeviceDiv2/TheDeviceDiv2.cpp
@@ -0,0 +1,212 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+
+// type alias
+typedef long long LL;
+
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+
+// repetition
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+
+// debug cout
+#define dump(x)  cout << #x << " = " << (x) << endl;
+#define dump2(x,y)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl;
+#define dump3(x,y,z)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl;
+#define dump4(x,y,z,a)  cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl;
+#define dumpAR(ar) FORR(x,(ar)) { cout << x << ','; } cout << endl;
+
+/*
+ 
+ 12/17/2017
+ 
+ 12:49-13:14 system test passed (314.85 pt)
+ 
+ i) (1,1)
+  1 AND 1 = 1
+  1 XOR 1 = 0 ✅
+  1  OR 1 = 1
+ 
+ ii) (0,0)
+  0 AND 0 = 0
+  0 XOR 0 = 0
+  0  OR 0 = 0
+ 
+ iii) (0,1)
+  0 AND 1 = 0 ✅
+  0 XOR 1 = 1
+  0  OR 1 = 1
+ 
+ If 1 XOR 1 = 0, operator is `XOR`.
+ If 0 AND 1 = 0, operator is `AND`.
+ Otherwise operator is `OR`
+ 
+ So we need two 1s and one 0 to determine operator in each bit.
+ 
+ Editorial:
+  - http://kmjp.hatenablog.jp/entry/2013/02/11/0930
+ 
+ Key:
+  - Each bit is independent
+  - Two 1s and one 0 are needed
+ 
+ Summary:
+  - Problem statement was not clear for me
+  - I spent most of time to understand the problem
+ 
+ */
+class TheDeviceDiv2 {
+  public:
+  string yes="YES",no="NO";
+  string identify(vector<string> P) {
+    int N=SZ(P),M=SZ(P[0]);
+    REP(j,M) {
+      int c0=0,c1=0;
+      REP(i,N) (P[i][j]=='1'?c1:c0)++;
+      if(c0>=1&&c1>=2) continue;
+      return no;
+    }
+    return yes;
+  }
+};
+
+// CUT begin
+ifstream data("TheDeviceDiv2.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> plates, string __expected) {
+    time_t startClock = clock();
+    TheDeviceDiv2 *instance = new TheDeviceDiv2();
+    string __result = instance->identify(plates);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> plates;
+        from_stream(plates);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(plates, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1513543773;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TheDeviceDiv2 (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
