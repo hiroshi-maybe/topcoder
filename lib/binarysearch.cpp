cb275a48de69315b5966738373cf9e5f3f2c7715
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-02-04 18:01:49 -0800

    add ternary search to library

--- lib/binarysearch.cpp
+++ lib/binarysearch.cpp
@@ -14,10 +14,20 @@ bool lte(int n, int k) {
   return n<=k;
 }
 
-// max: [ok,ng), min: (ng,ok]
-// move ok as long as `c` holds
+/*
+ 
+ Binary search, O(lg N) time
+ 
+ max: [ok,ng), min: (ng,ok]
+ move ok as long as `c` holds
+ 
+ References:
+  - https://twitter.com/meguru_comp/status/697008509376835584
+    - Meguru style
+  - https://qiita.com/hamko/items/794a92c456164dcc04ad
+ 
+ */
 int meguru(vector<int> ns, int ok, int ng, int k) {
-  
   while(abs(ok-ng)>1) {
     int mid = (ng+ok)/2;
     bool c = ok<ng ? ns[mid]<=k : ns[mid]>=k;
@@ -30,6 +40,70 @@ int meguru(vector<int> ns, int ok, int ng, int k) {
 
   bool c = ok<ng ? ns[ok]<=k : ns[ok]>=k;
   return c ? ok : -1;
+}
+
+/*
+ 
+ Ternary search to find index of a peak of real numbers, O(lg N) time
+ 
+  - Find an index of peak
+  - Highest value must be UNIQUE to expect deterministic result (Unimodality)
+  - If there are multiple peaks, any of them is returned
+  - Numbers must be STRICTLY increasing or decreasing
+  - Typically used for "real number" to guarantee strictly increasing/decreasing and unimodality
+ 
+ References:
+  - https://en.wikipedia.org/wiki/Ternary_search
+  - https://en.wikipedia.org/wiki/Unimodality
+  - https://www.hackerearth.com/practice/algorithms/searching/ternary-search/tutorial/
+  - http://naoyat.hatenablog.jp/entry/2012/01/04/231801
+  - http://d.hatena.ne.jp/ir5/20090630/1246349028
+  - http://d.hatena.ne.jp/nodchip/20090303/1236058357
+ 
+ Used problems:
+  - https://github.com/k-ori/leetcode/blob/master/162-Find-Peak-Element/FindPeak.cpp
+ 
+ */
+int tsearch(vector<double> ns) {
+  int N=ns.size(), l=0, r=N-1;
+  if(N==1) return 0;
+  if(N==2) return ns[0]>ns[1]?0:1;
+  while(abs(r-l)>2) {
+    int ll=l+(r-l)/3,rr=r-(r-l)/3;
+    if(ns[ll]<ns[rr]) l=ll;
+    else r=rr;
+  }
+  if(ns[l]>ns[l+1]&&ns[l]>ns[r]) return l;
+  if(ns[l+1]>ns[r]) return l+1;
+  return r;
+}
+
+/*
+ 
+ Ternary search to find index of a peak of integers, O(lg N) time
+ 
+  - Find an index of peak
+  - Highest value must be UNIQUE to expect deterministic result (Unimodality)
+  - If there are multiple peaks, any of them is returned
+  - Numbers must be STRICTLY increasing or decreasing
+  - Use binary search to find max value which in strictly increasing part
+
+ Ternary search on integers
+  - http://codeforces.com/blog/entry/43440
+  - http://codeforces.com/blog/entry/11497
+  - https://apps.topcoder.com/forums/?module=Thread&threadID=670169&start=0
+
+ Used problems:
+ - https://github.com/k-ori/leetcode/blob/master/162-Find-Peak-Element/FindPeak.cpp
+ 
+ */
+int tsearch_int(vector<int> ns) {
+  int N=ns.size(), good=0, bad=N;
+  while(abs(good-bad)>1) {
+    int mid=(good+bad)/2;
+    (ns[mid-1]<ns[mid]?good:bad)=mid;
+  }
+  return good;
 }
 
 void test(vector<int> ns, int k) {
@@ -59,4 +133,28 @@ int main(int argc, char const *argv[]) {
   test(ns,0);
   test(ns,7);
   test(ns,8);
+  
+  vector<double> unimodal1 = {0,2,3,5,4,3,2,1,0,-1,-2,-3};
+  vector<int> unimodal1i = {0,2,3,5,4,3,2,1,0,-1,-2,-3};
+  assert(tsearch(unimodal1)==3);
+  assert(tsearch_int(unimodal1i)==3);
+  vector<double> unimodal2 = {12,11,10,9,8,7,6,5,4,3,2,1};
+  vector<int> unimodal2i = {12,11,10,9,8,7,6,5,4,3,2,1};
+  assert(tsearch(unimodal2)==0);
+  assert(tsearch_int(unimodal2i)==0);
+  vector<double> unimodal3 = {1,2,3,4,5,6,7,8,9,10,11,12};
+  vector<int> unimodal3i = {1,2,3,4,5,6,7,8,9,10,11,12};
+  assert(tsearch(unimodal3)==11);
+  assert(tsearch_int(unimodal3i)==11);
+  vector<double> non_unimodal = {1,2,3,10,13,23,33,43,43,43,0,-10};
+  vector<int> non_unimodali = {1,2,3,10,13,23,33,43,43,43,0,-10};
+
+  assert(7<=tsearch(non_unimodal)&&tsearch(non_unimodal)<=9); // returns 8
+  assert(7<=tsearch_int(non_unimodali)&&tsearch_int(non_unimodali)<=9); // returns 7
+  
+  // Ternary search does not work for this case
+  // vector<double> non_strict = {1,1,1,1,1,1,1,2};
+  // vector<int> non_stricti = {1,1,1,1,1,1,1,2};
+  // assert(tsearch(non_strict)==7);
+  // assert(tsearch_int(non_stricti)==7);
 }
