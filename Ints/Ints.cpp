0e678270a5ecf592ea3fa2d1e28ef070997330f3
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-02-08 22:38:21 -0800

    solve FoxAndIntegers SRM 535 div2 easy

---
+++ Ints/Ints.cpp
@@ -0,0 +1,219 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 2/8/2018
+ 
+ 8:44-8:48 ACC (245.25 pt)
+ 
+ Editorials:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+535
+  - http://topcoder.g.hatena.ne.jp/agw/20120303/1331179913
+  - http://rkx1209.hatenablog.com/entry/2012/11/07/214858
+    - brute-force
+  - http://64.hatenadiary.com/entry/2012/04/02/005919
+  - http://lepton.hatenablog.jp/entry/20120304/1330843995
+ 
+ 18:35-18:38 add brute-force solution
+ 
+ */
+class FoxAndIntegers {
+public:
+  vector<int> get(int P, int Q, int R, int S) {
+    FORE(A,-60,60) FORE(B,-60,60) FORE(C,-60,60) {
+      if(P!=A-B) continue;
+      if(Q!=B-C) continue;
+      if(R!=A+B) continue;
+      if(S!=B+C) continue;
+      return {A,B,C};
+    }
+    return {};
+  }
+};
+
+class FoxAndIntegers_org {
+  public:
+  vector<int> get(int P, int Q, int R, int S) {
+    int A=(P+R)/2,B=(R-P)/2,C=(S-Q)/2;
+    if(P!=A-B) return {};
+    if(Q!=B-C) return {};
+    if(R!=A+B) return {};
+    if(S!=B+C) return {};
+    return {A,B,C};
+  }
+};
+
+// CUT begin
+ifstream data("Ints.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(int AminusB, int BminusC, int AplusB, int BplusC, vector<int> __expected) {
+    time_t startClock = clock();
+    FoxAndIntegers *instance = new FoxAndIntegers();
+    vector<int> __result = instance->get(AminusB, BminusC, AplusB, BplusC);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int AminusB;
+        from_stream(AminusB);
+        int BminusC;
+        from_stream(BminusC);
+        int AplusB;
+        from_stream(AplusB);
+        int BplusC;
+        from_stream(BplusC);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(AminusB, BminusC, AplusB, BplusC, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1518108287;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "Ints (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
