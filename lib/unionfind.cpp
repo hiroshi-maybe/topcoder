1f5fce797ab5f6e389c22a5c94a22c56c9a6542a
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-02-10 16:01:19 -0800

    Enhance union-find library

--- lib/unionfind.cpp
+++ lib/unionfind.cpp
@@ -1,59 +1,84 @@
-/********** union-find lib **********/
+#include <iostream>
+#include <sstream>
+#include <cassert>
+#include <vector>
 
-int groupNum;
-vector < int > rank;
-vector < int > parent;
-void initUnionSet(int n) {
-  groupNum = n+1;
-  rank = vector < int >(n+1);
-  parent = vector < int >(n+1);
-  for(int i=0; i<=n; i++) {
-    rank[i]=0;
-    parent[i]=i;
-  }
-}
-
+using namespace std;
 /*
+ 
+ Union-find algorithm, O(α(N)) time per operation
+ 
+  - Popular algorithm to manage disjoint set
+ 
+ References:
+  - https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+  - Ant book 2-4 data structures
+  - CLRS 21 Data Structures for Disjoint Sets
+ 
+ Usage:
+ 
+  UF uf(10);
+  uf.unite(0,1);
+  uf.unite(1,2);
+  cout << uf.same(0,1) << endl; // true
+ 
+ */
 
-iterative
-
-int find(int n) {
-  while(n != parent[n]) {
-    // shorten path
-    parent[n] = parent[parent[n]];
-    n = parent[n];
+struct UF {
+public:
+  int groupNum;
+  UF(int N) {
+    uf=vector<int>(N);
+    rank=vector<int>(N);
+    for(int i=0; i<N; ++i) uf[i]=i,rank[i]=0;
+    groupNum=N;
   }
-
-  return n;
-}
-*/
-
-int find(int i) {
-  if (uf[i]==i) return i;
-  return uf[i]=find(uf[i]);
-}
-
-bool unioned(int p, int q) {
-  return find(p)==find(q);
-}
-
-void dounion(int p, int q) {
-  int n = find(p);
-  int m = find(q);
-
-  if (n==m) return;
-
-  groupNum--;
-
-  if (rank[n]==rank[m]) {
-    parent[n] = m;
-    rank[m]++;
-    return;
+  
+  int find(int i) {
+    return uf[i]==i?i:uf[i]=find(uf[i]);
   }
-
-  if (rank[n]<rank[m]) {
-    parent[n] = m;
-  } else {
-    parent[m] = n;
+  
+  bool same(int x, int y) {
+    return find(x)==find(y);
   }
+  
+  void unite(int x, int y) {
+    x=find(x);
+    y=find(y);
+    if(x==y) return;
+    groupNum--;
+    
+    if (rank[x]==rank[y]) {
+      uf[x]=y;
+      rank[y]++;
+      return;
+    }
+    
+    if (rank[x]<rank[y]) {
+      uf[x]=y;
+    } else {
+      uf[y] =x;
+      if(rank[x]==rank[y]) ++rank[x];
+    }
+  }
+private:
+  vector<int> rank;
+  vector<int> uf;
+};
+
+int main(int argc, char const *argv[]) {
+  UF uf(10);
+  uf.unite(1,3);
+  assert(uf.same(1,3));
+  assert(uf.groupNum==9);
+  uf.unite(4,6);
+  uf.unite(0,2);
+  uf.unite(7,8);
+  uf.unite(0,1);
+  uf.unite(4,5);
+  uf.unite(1,2);
+  assert(uf.same(0,3));
+  assert(uf.same(4,6));
+  assert(uf.same(7,8));
+  assert(uf.groupNum==4);
 }
