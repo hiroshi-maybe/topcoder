b24be9024dbc9621cd46f26224e0b534005a0c7c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-02-11 22:40:47 -0800

    Add segment tree to library

--- lib/range-query.cpp
+++ lib/range-query.cpp
@@ -7,6 +7,109 @@ using namespace std;
 
 /*
  
+ Range minimum query by segment tree, O(N) time to build, O(lg N) time to query or update
+ 
+  - Data structure to query in range
+  - Segment tree is applicable as long as result to be queried is associative
+   - Range sum query
+   - Range GCD query
+  - Update `int operate(a,b)` method to reuse for diffirent query
+  - Needs 2*X-1 space, where X = min { n : N<=2^n }
+   - X leaves + X-1 parents
+   - Data is populated in 0-indexed
+ 
+  References:
+   - Ant book 3-3 data structures
+   - https://www.slideshare.net/hcpc_hokudai/rmq-47663507
+   - https://www.slideshare.net/iwiwi/ss-3578491?next_slideshow=1
+   - https://en.wikipedia.org/wiki/Segment_tree
+   - https://www.topcoder.com/community/data-science/data-science-tutorials/range-minimum-query-and-lowest-common-ancestor/
+ 
+  Usage:
+ 
+   SegmentTree T(X.size());
+   T.build(X);
+   cout << T.query(3,17) << endl;
+   T.update(12,5);
+   cout << T.query(3,17) << endl;
+
+  Used problems:
+   - https://github.com/k-ori/leetcode/blob/master/307-Range-Sum-Query/RangeSumQuery.cpp (RSQ)
+ 
+ */
+struct SegmentTree {
+public:
+  int N;
+  SegmentTree(int N): N(N) {
+    int n=1;
+    // Make vector size power of 2
+    while(n<N) n<<=1;
+    T.resize(2*n-1,iV);
+  }
+  
+  // Initialize tree with `ns`
+  void build(vector<int> &ns) {
+    assert(ns.size()<=T.size());
+    buildTree(ns,0,0,N);
+  }
+  
+  void update(int i, int x) {
+    updateTree(i,x,0,0,N);
+  }
+  
+  // query in range [L,R)
+  int query(int L, int R) {
+    return queryTree(L,R,0,0,N);
+  }
+  
+  // ⚠️ Override point
+  // Note that operation should be associative A`op`(B`op`C) == (A`op`B)`op`C
+  const int iV=1e9;
+  int operate(int a, int b) {
+    return min(a,b); // RMQ
+  }
+private:
+  vector<int> T; // T[1..N] has values (1-based index)
+  // T[i] manages query result in index range [l,r)
+  void buildTree(vector<int> &ns, int i, int l, int r) {
+    if (l==r-1) { T[i]=ns[l]; return; }
+    
+    int mid=l+(r-l)/2;
+    buildTree(ns,2*i+1,l,mid); // left child
+    buildTree(ns,2*i+2,mid,r); // right child
+    
+    T[i]=operate(T[2*i+1],T[2*i+2]);
+  }
+  
+  void updateTree(int p, int x, int i, int l, int r) {
+    if (l==r-1) { T[i]=x; return; }
+    
+    int mid=l+(r-l)/2;
+    if(p<mid) updateTree(p,x,2*i+1,l,mid);
+    else       updateTree(p,x,2*i+2,mid,r);
+    
+    T[i]=operate(T[2*i+1],T[2*i+2]);
+  }
+  
+  int queryTree(int L, int R, int i, int l, int r) {
+    // out of range
+    if (r<=L||R<=l) return iV;
+    
+    // match
+    if (L<=l&&r<=R) return T[i];
+    
+    int mid=l+(r-l)/2;
+    int res=iV;
+    int a=queryTree(L,R,2*i+1,l,mid);
+    int b=queryTree(L,R,2*i+2,mid,r);
+    res = operate(res,a);
+    res = operate(res,b);
+    return res;
+  }
+};
+
+/*
+ 
  Binary Indexed Tree, build: O(N*lg N) time, query: O(lg N) time, N=Upper bound of the range
  
  - Data structure to query sum in a range
@@ -30,6 +133,7 @@ using namespace std;
 
  Used problems:
   - https://leetcode.com/problems/global-and-local-inversions/
+  - https://github.com/k-ori/leetcode/blob/master/307-Range-Sum-Query/RangeSumQuery.cpp
  
  */
 struct BIT {
@@ -52,7 +156,7 @@ public:
   }
   
   // query ∑ { sum[i] : i=l..r }
-  int query2(int l, int r) {
+  int query(int l, int r) {
     assert(l<=r&&0<=l&&r<N);
     return query(r)-query(l-1);
   }
@@ -65,6 +169,12 @@ public:
       T[i]+=x;
       i+=lsone(i);
     }
+  }
+  
+  // sum[i]=x
+  void update(int i, int x) {
+    int v1=query(i)-query(i-1);
+    add(i,x-v1);
   }
   
   // compute total inversions
@@ -97,4 +207,12 @@ int main(int argc, char const *argv[]) {
   int V=*max_element(ns.begin(),ns.end());
   f=BIT(V);
   assert(f.inversions(ns)==3);
+  
+  // Range minimum query
+  SegmentTree T(N);
+  T.build(ns);
+  assert(T.query(2,8)==1);
+  assert(T.query(3,8)==2);
+  T.update(5,-1);
+  assert(T.query(2,8)==-1);
 }
