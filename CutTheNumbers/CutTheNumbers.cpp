9786551d0e321dfee2e321ba431e5335e6d0cd83
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-03-15 01:51:45 -0700

    solve CutTheNumbers SRM 513 div2 hard

---
+++ CutTheNumbers/CutTheNumbers.cpp
@@ -0,0 +1,211 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE false
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 3/13/2018
+ 
+ 8:49-8:57 anslysis
+ 8:58-9:11 ACC (669.50 pt)
+ 
+ Editorials:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+513
+   - impressive idea to mark cells with 'V' or 'H'
+  - http://kmjp.hatenablog.jp/entry/2014/02/13/0930
+  - http://d.hatena.ne.jp/kusano_prog/20110726/1311693974
+  - http://area.hateblo.jp/entry/2013/08/27/020048
+ 
+ Summary:
+  - solved in 22 mins quickly. Good job 👍
+  - Interesting problem in either brute-force or dp 😄
+ 
+ */
+int dp[1<<16];
+class CutTheNumbers {
+  public:
+  int R,C;
+  int pos(int i, int j) {
+    return i*C+j;
+  }
+  int maximumSum(vector<string> B) {
+    this->R=SZ(B),this->C=SZ(B[0]);
+    ZERO(dp);
+    REP(mask,1<<(R*C)) REP(i,R) REP(j,C) if(((mask>>pos(i,j))&1)==0) {
+      int x=0;
+      int mask2=mask;
+      dump4(mask,i,j,dp[mask]);
+      FOR(ii,i,R) {
+        if(mask&(1<<pos(ii,j))) break;
+        x=10*x+B[ii][j]-'0';
+        mask2|=1<<pos(ii,j);
+        SMAX(dp[mask2],dp[mask]+x);
+      }
+      x=0,mask2=mask;
+      FOR(jj,j,C) {
+        if(mask&(1<<pos(i,jj))) break;
+        x=10*x+B[i][jj]-'0';
+        mask2|=1<<pos(i,jj);
+        SMAX(dp[mask2],dp[mask]+x);
+      }
+    }
+    dump2((1<<(R*C))-1,dp[(1<<(R*C))-1]);
+    return dp[(1<<(R*C))-1];
+  }
+};
+
+// CUT begin
+ifstream data("CutTheNumbers.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> board, int __expected) {
+    time_t startClock = clock();
+    CutTheNumbers *instance = new CutTheNumbers();
+    int __result = instance->maximumSum(board);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> board;
+        from_stream(board);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(board, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1520999337;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 1000 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "CutTheNumbers (1000 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
