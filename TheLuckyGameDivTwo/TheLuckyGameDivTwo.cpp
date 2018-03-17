1a24c5130462e9d70c6e4b77e1f18d530e0b3c5b
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-03-17 00:04:17 -0700

    solve TheLuckyGameDivTwo SRM 510 div2 medium

---
+++ TheLuckyGameDivTwo/TheLuckyGameDivTwo.cpp
@@ -0,0 +1,204 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE false
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 3/16/2018
+ 
+ 9:20-9:42 ACC (337.68 pt)
+ 
+ Editorials:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+510
+  - http://d.hatena.ne.jp/kusano_prog/20111015/1318647642
+    - memoization
+  - http://purple-jwl.hatenablog.com/entry/20130324/1364127825
+  - http://kg86.hatenablog.com/entry/20130703/1372848578
+ 
+ Summary:
+  - Nice to figure out that searching second player every time doesn't work and to apply cumulative sum
+ 
+ */
+int cum[4750];
+class TheLuckyGameDivTwo {
+  public:
+  int L;
+  int lucky(int n) {
+    bool ok=true;
+    while(n) {
+      int d=n%10;
+      ok&=d==4||d==7;
+      n/=10;
+    }
+    return ok;
+  }
+  int f(int l, int r) {
+    const int Inf=1e5;
+    int res=Inf;
+    FORE(n,l,r) if(n+L<=r) res=min(res,cum[n+L]-cum[n]);
+    dump3(l,r,res);
+    return res==Inf?0:res;
+  }
+  int find(int a, int b, int len1, int len2) {
+    int luck[4748]={0};
+    FORE(n,1,4747) luck[n]=lucky(n);
+    ZERO(cum);
+    FORE(n,1,4747) cum[n+1]=cum[n]+luck[n];
+    
+    this->L=len2;
+    int res=0;
+    FORE(n,a,b) if(n+len1<=b+1) res=max(res,f(n,n+len1));
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("TheLuckyGameDivTwo.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int a, int b, int jLen, int bLen, int __expected) {
+    time_t startClock = clock();
+    TheLuckyGameDivTwo *instance = new TheLuckyGameDivTwo();
+    int __result = instance->find(a, b, jLen, bLen);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int a;
+        from_stream(a);
+        int b;
+        from_stream(b);
+        int jLen;
+        from_stream(jLen);
+        int bLen;
+        from_stream(bLen);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(a, b, jLen, bLen, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1521217244;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TheLuckyGameDivTwo (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
