bab53a957c9cc92d79bfc84c88497d505766b6a9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-03-28 20:53:52 -0700

    solve TheCowDivTwo SRM 502 div2 hard

---
+++ TheCowDivTwo/TheCowDivTwo.cpp
@@ -0,0 +1,212 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 3/28/2018
+ 
+ 8:56-9:08 analysis
+ 9:09-9:16 implement
+ 9:17-9:31 debug, fixed two bugs (cnt<=K, reset dp2) and submit. ACC (515.62 pt)
+ 
+ Editorials:
+  - https://apps.topcoder.com/wiki/display/tc/SRM+502
+  - http://kmjp.hatenablog.jp/entry/2014/04/19/1400
+  - http://d.hatena.ne.jp/kusano_prog/20110408/1302294640
+  - http://area.hateblo.jp/entry/2013/08/09/031119
+  - http://topcoder.g.hatena.ne.jp/firewood/20110415/1302887143
+
+ Key:
+  - Subset sum DP with reduced search space by %N
+ 
+ Summary:
+  - Submit was delayed due to 2 Implementation mistake 😞
+   - cnt<=K is needed not to miss cases the last element is not picked up
+   - DP memory reset when reusing two dp tables
+  - Analysis was straighforward 👍
+ 
+ */
+const LL MOD=1000000007;
+LL dp[48][1001];
+class TheCowDivTwo {
+  public:
+  int find(int N, int K) {
+    ZERO(dp);
+    dp[0][0]=1;
+    REP(i,N) {
+      LL dp2[48][1001]={};
+      REPE(cnt,K) REP(sum,N) if(dp[cnt][sum]>0) {
+        (dp2[cnt][sum]+=dp[cnt][sum])%=MOD;
+        if(cnt<47) (dp2[cnt+1][(sum+i)%N]+=dp[cnt][sum])%=MOD;
+      }
+      swap(dp,dp2);
+    }
+    
+    return (int)dp[K][0];
+  }
+};
+/*
+LL dp2[10][48][1001];
+class TheCowDivTwo {
+public:
+  int find(int N, int K) {
+    ZERO(dp2);
+    dp2[0][0][0]=1;
+    REP(i,N) REPE(cnt,K) REP(sum,N) if(dp2[i][cnt][sum]>0) {
+      (dp2[(i+1)][cnt][sum]+=dp2[i][cnt][sum])%=MOD;
+      (dp2[(i+1)][cnt+1][(sum+i)%N]+=dp2[i][cnt][sum])%=MOD;
+    }
+    
+    REPE(i,N) REPE(cnt,K) REP(sum,N) dump4(i,cnt,sum,dp2[i][cnt][sum]);
+    
+    return (int)dp2[N][K][0];
+  }
+};*/
+
+// CUT begin
+ifstream data("TheCowDivTwo.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int N, int K, int __expected) {
+    time_t startClock = clock();
+    TheCowDivTwo *instance = new TheCowDivTwo();
+    int __result = instance->find(N, K);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int N;
+        from_stream(N);
+        int K;
+        from_stream(K);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, K, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1522252572;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 1000 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TheCowDivTwo (1000 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
