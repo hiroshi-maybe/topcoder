2d9d985be8b51fd58ebba9ac0c13a42fa130fd84
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-06-08 18:06:00 +0900

    solve TopXorer SRM723 div1 easy

---
+++ TopXorer/TopXorer.cpp
@@ -0,0 +1,232 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 6/8/2018
+ 
+ 14:40-15:06 1WA
+ 15:07-15:15 4WA. pause..
+ 17:20-17:33 Figure out new algorithm and got AC
+ 
+ Editorials:
+  - greedy
+   - http://kmjp.hatenablog.jp/entry/2017/11/21/0900
+   - http://tsutaj.hatenablog.com/entry/2018/02/27/235639
+   - http://hamayanhamayan.hatenablog.jp/entry/2017/11/19/034028
+  - dp
+   - https://twitter.com/satanic0258/status/931954742049193984
+   - https://twitter.com/_olphe/status/931954218298064896
+   - https://twitter.com/beet_aizu/status/931954033052459008
+ 
+ Key:
+  - MSB bit can be selected greedily
+  - If ON bit is skipped, LSBs can be all ON
+ 
+ Summary:
+  - Analysis is too rough (many WAs)
+  - Strategy to take MSBs greedily is fine. However taking just MSB is not sufficient ("10 6" test case failed)
+
+ 
+ */
+
+int f(int x) {
+  int res=-1;
+  REPE(k,31) if((x>>k)&1) res=k;
+  return res;
+}
+
+class TopXorer {
+public:
+  int maximalRating(vector<int> X) {
+    int res=0;
+    REPE(k,31) {
+      int cnt=0;
+      FORR(x,X) if((x>>k)&1) ++cnt;
+      if(cnt==1) res|=1<<k;
+      else if(cnt>1) res|=(1<<(k+1))-1;
+    }
+    return res;
+  }
+};
+
+class TopXorer_wrong {
+  public:
+  int maximalRating(vector<int> X) {
+    sort(X.rbegin(),X.rend());
+    int res=X.front(),cur=31;
+    FORR(x,X) {
+      int a=f(x);
+      dump2(x,a);
+      if(a>=0) {
+        if(a==cur) {
+          res|=x;
+          cur=a-1;
+        } else if(a==cur+1) {
+          res|=(1<<a)-1;
+        } else if(a<cur) {
+          res|=x;
+          cur=a-1;
+        }
+      }
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("TopXorer.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> x, int __expected) {
+    time_t startClock = clock();
+    TopXorer *instance = new TopXorer();
+    int __result = instance->maximalRating(x);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> x;
+        from_stream(x);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(x, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1528436401;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TopXorer (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
