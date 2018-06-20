f7062eb401d69f9aa0997a7e7d017956e47d3b2e
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-06-20 15:22:19 +0900

    solve InterleavingParenthesis SRM 718 div1 easy

---
+++ InterleavingParenthesis/InterleavingParenthesis.cpp
@@ -0,0 +1,196 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 6/20/2018
+ 
+ 14:16-14:46 1WA (141.80 pt)
+ 14:49 add additional balance check and got AC
+ 
+ Editorials:
+  - http://kmjp.hatenablog.jp/entry/2017/07/14/0900
+  - http://hamko.hatenadiary.jp/entry/2017/07/13/114326
+  - http://fluffyowl.hatenablog.com/entry/2017/07/30/231606
+  - https://ei1333.hateblo.jp/entry/2017/07/13/133101
+ 
+ Summary:
+  - Straightforward dp problem
+  - 1WA due to corner case 😞
+ 
+ */
+
+// 14:16-14:46 1WA(141.80 pt)
+// 14:49 add balance check and got AC
+const LL MOD=1e9+7;
+LL dp[2501][2501];
+class InterleavingParenthesis {
+  public:
+  int countWays(string S1, string S2) {
+    int N1=SZ(S1),N2=SZ(S2);
+    ZERO(dp);
+    dp[0][0]=1;
+    vector<int> cum1(N1+1),cum2(N2+1);
+    cum1[0]=0,cum2[0]=0;
+    REP(i,N1) cum1[i+1]=cum1[i]+(S1[i]=='('?1:-1);
+    REP(i,N2) cum2[i+1]=cum2[i]+(S2[i]=='('?1:-1);
+    
+    if(cum1[N1]+cum2[N2]!=0) return 0;
+//    dumpAR(cum1);
+//    dumpAR(cum2);
+
+    REPE(i,N1)REPE(j,N2) {
+      if(i<N1&&cum1[i+1]+cum2[j]>=0) (dp[i+1][j]+=dp[i][j])%=MOD;
+      if(j<N2&&cum1[i]+cum2[j+1]>=0) (dp[i][j+1]+=dp[i][j])%=MOD;
+    }
+    return dp[N1][N2];
+  }
+};
+
+// CUT begin
+ifstream data("InterleavingParenthesis.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string s1, string s2, int __expected) {
+    time_t startClock = clock();
+    InterleavingParenthesis *instance = new InterleavingParenthesis();
+    int __result = instance->countWays(s1, s2);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string s1;
+        from_stream(s1);
+        string s2;
+        from_stream(s2);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(s1, s2, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1529471763;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "InterleavingParenthesis (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
