44120b62693d7f922034ffcaede197566b1ab4d6
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-09-05 23:28:16 -0700

    solve SRM 428 div1 easy TheLuckyString

---
+++ TheLuckyString/TheLuckyString.cpp
@@ -0,0 +1,210 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 9/5/2018
+ 
+ 22:10-22:54 AC (133.24 pt)
+ 
+ Firstly I wrote code with `next_permutation()` and counted unique string with hashset.
+ However it cased MLE.
+ Actually no need to use hashset because `next_permutation()` guarantees that generated string is unique.
+ I forgot that and wrote manual brute-force with dfs. That works too.
+ 
+ 
+ 
+ Summary:
+  - `next_permutation()` guarantees unique permutation
+  - To avoid duplicated pattern in manual dfs permutation, use the last occurence of each character to swap
+ 
+ */
+
+class TheLuckyString {
+public:
+  int res=0,N;
+  void dfs(int p, string s) {
+    if(p==N) {
+      ++res;
+      return;
+    }
+    int viz[26]={};
+    FOR(i,p,N) {
+      if(p!=0&&s[p-1]==s[i]) continue;
+      if(viz[s[i]-'a']) continue;
+      viz[s[i]-'a']=1;
+      swap(s[p],s[i]);
+      dfs(p+1,s);
+      swap(s[p],s[i]);
+    }
+  }
+  int count(string s) {
+    sort(s.begin(),s.end());
+    N=SZ(s);
+    dfs(0,s);
+    return res;
+  }
+};
+
+class TheLuckyString_mle {
+  public:
+  int count(string s) {
+    unordered_set<string> S;
+    sort(s.begin(),s.end());
+    int N=SZ(s);
+    VI P(N); REP(i,N)P[i]=i;
+    do {
+      string t; REP(i,N) t+=s[P[i]];
+      bool ok=true;
+      REP(i,N-1) if(t[i]==t[i+1]) ok=false;
+      if(ok)S.emplace(t);
+    }while(next_permutation(P.begin(),P.end()));
+    return SZ(S);
+  }
+};
+
+// CUT begin
+ifstream data("TheLuckyString.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string s, int __expected) {
+    time_t startClock = clock();
+    TheLuckyString *instance = new TheLuckyString();
+    int __result = instance->count(s);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string s;
+        from_stream(s);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(s, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1536211239;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "TheLuckyString (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
