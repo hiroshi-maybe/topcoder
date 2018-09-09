5a1fd29f64bee718d088ce7c545f44184fd38925
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-09-09 15:29:19 -0700

    solve SRM 438 div1 easy UnluckyIntervals

---
+++ UnluckyIntervals/UnluckyIntervals.cpp
@@ -0,0 +1,225 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 9/9/2018
+ 
+ 14:01-14:54 analysis
+ 15:11 1WA
+ 15:19 AC
+ 
+ http://apps.topcoder.com/wiki/display/tc/SRM+438
+ http://topcoder.g.hatena.ne.jp/tomerun/20090419/1240165595
+ http://rsujskf.blog32.fc2.com/blog-entry-242.html
+ 
+ */
+
+class UnluckyIntervals {
+  public:
+  const LL Inf=1e15;  
+  LL f(LL x, LL l, LL r) {
+    return (x-l)+(r-x-1)*(x-l+1);
+  }
+  vector<int> getLuckiest(vector<int> S, int N) {
+    set<pair<LL,int>> X;
+    int M=SZ(S);
+    REP(i,M) X.emplace(0,S[i]);
+    vector<LL> Y(S.begin(),S.end());
+    Y.push_back(0);
+    Y.push_back(Inf);
+    sort(Y.begin(),Y.end());
+    REP(i,SZ(Y)-1) {
+      LL l=i==0?1:Y[i-1]+1,r=Y[i+1];
+      FORE(d,1,N) {
+        LL x=Y[i]-d;
+        if(x>=l) {
+          LL fx=f(x,l,Y[i]);
+          X.emplace(fx,x);
+        }
+        x=Y[i]+d;
+        if(x<r) {
+          LL fx=f(x,Y[i]+1,r);
+          X.emplace(fx,x);
+        }
+      }
+    }
+    assert(SZ(X)>=N);
+    vector<int> res;
+    FORR(p,X) {
+      res.push_back(p.second);
+      if(SZ(res)==N) break;
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("UnluckyIntervals.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(vector<int> luckySet, int n, vector<int> __expected) {
+    time_t startClock = clock();
+    UnluckyIntervals *instance = new UnluckyIntervals();
+    vector<int> __result = instance->getLuckiest(luckySet, n);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> luckySet;
+        from_stream(luckySet);
+        int n;
+        from_stream(n);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(luckySet, n, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1536526868;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 300 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "UnluckyIntervals (300 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
