3faac5eec029e6757a6651aca30597ac1c3a2886
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-09-12 22:54:11 -0700

    solve SRM 447 div1 easy KnightsTour

---
+++ KnightsTour/KnightsTour.cpp
@@ -0,0 +1,217 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 9/11/2018
+ 
+ 15:18-15:26 analysis
+ 15:40 AC (168.63 pt)
+ 
+ http://apps.topcoder.com/wiki/display/tc/SRM+447
+ 
+ */
+int dx[8]={1,2, 1, 2,-1,-2,-1,-2};
+int dy[8]={2,1,-2,-1,-2,-1, 2, 1};
+
+class KnightsTour {
+  public:
+  vector<string> B;
+  int acc(int i, int j) {
+    int res=0;
+    REP(k,8) {
+      int ii=i+dx[k],jj=j+dy[k];
+      if(ii<0||ii>=8||jj<0||jj>=8) continue;
+      if(B[ii][jj]!='.') continue;
+      ++res;
+    }
+    return res;
+  }
+  int visitedPositions(vector<string> BB) {
+    this->B=BB;
+    int r=-1,c=-1;
+    REP(i,8)REP(j,8) if(B[i][j]=='K') r=i,c=j;
+    int i=r,j=c;
+    while(true) {
+      B[i][j]='x';
+      set<III> S;
+      REP(k,8) {
+        int ii=i+dx[k],jj=j+dy[k];
+        if(ii<0||ii>=8||jj<0||jj>=8) continue;
+        if(B[ii][jj]!='.') continue;
+        int a=acc(ii,jj);
+        S.emplace(a,ii,jj);
+      }
+      if(S.empty()) break;
+      int a,r,c; tie(a,r,c)=*(S.begin());
+      i=r,j=c;
+    }
+    int res=0;
+    REP(i,8)REP(j,8) res+=B[i][j]=='x';
+    
+    /*
+    REP(i,8){
+      REP(j,8) cout<<B[i][j];
+      cout<<endl;
+    }*/
+    
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("KnightsTour.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> board, int __expected) {
+    time_t startClock = clock();
+    KnightsTour *instance = new KnightsTour();
+    int __result = instance->visitedPositions(board);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> board;
+        from_stream(board);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(board, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1536704282;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "KnightsTour (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
