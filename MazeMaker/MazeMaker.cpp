de2dc8c975a6728f612ae56eda0b001e4e32f0b9
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-09-14 17:40:14 -0700

    solve MazeMaker SRM 453.5 div1 easy

---
+++ MazeMaker/MazeMaker.cpp
@@ -0,0 +1,207 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 9/13/2018
+ 
+ 15:17-15:30 AC (203.02 pt)
+ 
+ http://apps.topcoder.com/wiki/display/tc/SRM+453.5
+ 
+ */
+int D[51][51];
+class MazeMaker {
+  public:
+  int longestPath(vector<string> G, int sr, int sc, vector<int> dr, vector<int> dc) {
+    int N=SZ(G),M=SZ(G[0]);
+    const int Inf=1e5;
+    REP(i,N)REP(j,M) D[i][j]=Inf;
+    queue<II> Q;
+    Q.emplace(sr,sc); D[sr][sc]=0;
+    while(SZ(Q)) {
+      int i,j; tie(i,j)=Q.front(); Q.pop();
+      REP(k,SZ(dr)) {
+        int ii=i+dr[k],jj=j+dc[k];
+        if(ii<0||ii>=N||jj<0||jj>=M) continue;
+        if(G[ii][jj]=='X') continue;
+        int d=D[i][j]+1;
+        if(D[ii][jj]>d) {
+          Q.emplace(ii,jj);
+          D[ii][jj]=d;
+        }
+      }
+    }
+    int res=-1;
+    REP(i,N)REP(j,M) if(G[i][j]!='X') {
+//      dump3(i,j,D[i][j]);
+      SMAX(res,D[i][j]);
+    }
+    return res==Inf?-1:res;
+  }
+};
+
+// CUT begin
+ifstream data("MazeMaker.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> maze, int startRow, int startCol, vector<int> moveRow, vector<int> moveCol, int __expected) {
+    time_t startClock = clock();
+    MazeMaker *instance = new MazeMaker();
+    int __result = instance->longestPath(maze, startRow, startCol, moveRow, moveCol);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> maze;
+        from_stream(maze);
+        int startRow;
+        from_stream(startRow);
+        int startCol;
+        from_stream(startCol);
+        vector<int> moveRow;
+        from_stream(moveRow);
+        vector<int> moveCol;
+        from_stream(moveCol);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(maze, startRow, startCol, moveRow, moveCol, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1536876978;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "MazeMaker (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
