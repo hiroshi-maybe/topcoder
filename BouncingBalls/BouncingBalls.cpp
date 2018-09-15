5904fe0425c2a2af721f73c6e4387b531abf4327
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-09-15 16:19:24 -0700

    add solution of BouncingBalls SRM 458 div1 easy

---
+++ BouncingBalls/BouncingBalls.cpp
@@ -0,0 +1,224 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 9/15/2018
+ 
+ 15:34-16:03 Somehow sample #2 and #4 don't pass. Give up
+ 
+ http://apps.topcoder.com/wiki/display/tc/Member+SRM+458
+ 
+ OK. Built-in sort is not sufficient when tie happens at time T.
+ Also pre-sort is needed to figure out the first position.
+ 
+ 16:15 add solution
+ 
+ */
+class BouncingBalls {
+public:
+  double expectedBounces(vector<int> X, int T) {
+    double res=0;
+    int N=SZ(X);
+    sort(X.begin(),X.end());
+    REP(mask,1<<N) {
+      vector<LL> XX;
+      REP(i,N) {
+        int dir=((mask>>i)&1)?1:-1;
+        XX.emplace_back(X[i]+dir*T);
+      }
+      int cnt=0;
+      REP(j,N) REP(i,j) if(XX[i]>=XX[j]) ++cnt;
+      res+=1.0*cnt/(1<<N);
+    }
+
+    return res;
+  }
+};
+
+class BouncingBalls_wrong {
+  public:
+  double expectedBounces(vector<int> X, int T) {
+    double res=0;
+    int N=SZ(X);
+    LL sum=0;
+//    sort(X.begin(),X.end());
+    REP(mask,1<<N) {
+      vector<pair<LL,int>> XX;
+      REP(i,N) {
+        int dir=((mask>>i)&1)?1:-1;
+        XX.emplace_back(X[i]+dir*T,i);
+      }
+      sort(XX.begin(),XX.end());
+      int cnt=0;
+      REP(j,N) REP(i,j) if(XX[i].second>XX[j].second) ++cnt;
+//      REP(i,N) dump3(i,XX[i].first,XX[i].second);
+      dump2(mask,cnt);
+      sum+=cnt;
+      res+=1.0*cnt/double(1<<N);
+    }
+    dump3(sum,1<<N,1.0*sum/(1<<N));
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("BouncingBalls.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool double_equal(const double &a, const double &b) { return b==b && a==a && fabs(b - a) <= 1e-9 * max(1.0, fabs(a) ); }
+
+bool do_test(vector<int> x, int T, double __expected) {
+    time_t startClock = clock();
+    BouncingBalls *instance = new BouncingBalls();
+    double __result = instance->expectedBounces(x, T);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (double_equal(__expected, __result)) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> x;
+        from_stream(x);
+        int T;
+        from_stream(T);
+        next_line();
+        double __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(x, T, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1537050855;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "BouncingBalls (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
