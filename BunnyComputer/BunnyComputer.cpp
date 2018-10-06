215ced29c64dbf50c5d5e6ca537a44defbc41f9c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-10-06 15:50:43 -0700

    solve BunnyComputer SRM 487 div1 easy

---
+++ BunnyComputer/BunnyComputer.cpp
@@ -0,0 +1,209 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 10/5/2018
+ 
+ 17:52-18:23 analysis
+ 18:32 AC (119.14 pt)
+ 
+ We can make K+1 connected components due to K+1 fixed interval.
+ In each components, we want to maximize total score.
+ 
+ http://apps.topcoder.com/wiki/display/tc/SRM+487
+ Actually no need to do interval dp. We can greedily select based on parity of the array size.
+ 
+ Summary:
+  - Principal part of this problem is K+1 interval
+  - I should have started analyzing from it earlier
+  - I divided into K+1 separated problems leveraging independence of each component
+ 
+ */
+int memo[51][51];
+class BunnyComputer {
+  public:
+  int f(VI &A, int l, int r) {
+    if(r-l<2) return 0;
+    int &res=memo[l][r];
+    if(res>=0) return res;
+    FOR(i,l,r-1) {
+      int b=A[i]+A[i+1];
+      int a=f(A,l,i),c=f(A,i+2,r);
+      SMAX(res,a+b+c);
+    }
+    return res;
+  }
+  int getMaximum(vector<int> P, int K) {
+    int res=0,N=SZ(P);
+    REPE(i,K) {
+      VI A;
+      for(int j=i; j<N; j+=K+1) A.push_back(P[j]);
+      MINUS(memo);
+      int x=f(A,0,SZ(A));
+//      dump2(i,x);
+//      dumpAR(A);
+      res+=x;
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("BunnyComputer.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> preference, int k, int __expected) {
+    time_t startClock = clock();
+    BunnyComputer *instance = new BunnyComputer();
+    int __result = instance->getMaximum(preference, k);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> preference;
+        from_stream(preference);
+        int k;
+        from_stream(k);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(preference, k, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1538787094;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "BunnyComputer (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
