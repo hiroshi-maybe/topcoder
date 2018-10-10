2aa8f9a889c1210d3c8d0c61ed2454c62a3ac9f1
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-10-09 17:51:15 -0700

    solve PermutationSignature SRM 497 div1 easy

---
+++ PermutationSignature/PermutationSignature.cpp
@@ -0,0 +1,228 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector < int > VI;
+typedef unordered_map < int, int > MAPII;
+typedef unordered_set < int > SETI;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// minmax
+#define SMAX(a,b) a = max(a,b)
+#define SMIN(a,b) a = min(a,b)
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 10/8/2018
+ 
+ 16:10-16:45 analysis
+ 17:11 AC
+ 
+ My observation was that first "valley" should be '1'. After that we can fill left 'D's.
+ 
+ http://apps.topcoder.com/wiki/display/tc/SRM+497
+ Solution in the editorial is using property of lexicographically smallest.
+ res[i] should be as small as possible.
+ If S[i]='D', new head should be cur[0]+1. Otherwise it should be '1'.
+ We can do +1 for cur[i] which is >= new head to keep permutation invariant.
+ It's possible to prove that this is optimal because cur[0] is equal to length of 'D' prefix.
+ If we can put a value <cur[0]+1, we cannot fill the prefix.
+ 
+ http://topcoder.g.hatena.ne.jp/namakemono_srm/20110613/1307928650
+ http://rsujskf.blog32.fc2.com/blog-entry-1884.html
+ 
+ Summary:
+  - Greedily put the best and adjust current ones is kind of new for me.
+ 
+ */
+class PermutationSignature {
+  public:
+  void test(VI &X, string S) {
+    REP(i,SZ(S)) {
+      if(X[i]<X[i+1]) assert(S[i]=='I');
+      else assert(S[i]=='D');
+    }
+  }
+  VI res;
+  int n,N;
+  string S;
+  void put(int i) {
+    assert(n<=N+1);
+    assert(res[i]==-1);
+    res[i]=n++;
+  }
+  void left(int i) {
+    while(i>=0&&S[i]=='D') put(i--);
+  }
+  vector<int> reconstruct(string S) {
+    this->S=S;
+    this->N=SZ(S);
+    this->n=1;
+    this->res=VI(N+1,-1);
+    REP(i,N) if(S[i]=='I') {
+      put(i);
+      if(i>0&&S[i-1]=='D') left(i-1);
+    }
+    put(N);
+    left(N-1);
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("PermutationSignature.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(string signature, vector<int> __expected) {
+    time_t startClock = clock();
+    PermutationSignature *instance = new PermutationSignature();
+    vector<int> __result = instance->reconstruct(signature);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string signature;
+        from_stream(signature);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(signature, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1539126540;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "PermutationSignature (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
