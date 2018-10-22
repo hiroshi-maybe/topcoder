24f2f27a5db871354799a8da30f4351f28507d37
Hiroshi Kori <hiroshi.maybe@gmail.com>
2018-10-21 20:56:27 -0700

    solve RowAndCoins SRM 522 div1 easy

---
+++ RowAndCoins/RowAndCoins.cpp
@@ -0,0 +1,197 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef vector <int> VI;
+typedef unordered_map <int,int> MAPII;
+typedef unordered_set <int> SETI;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline bool SMIN(T &l,const T &r){return l<r?l=r,1:0;}
+template<class T> inline bool SMAX(T &l,const T &r){return l>r?l=r,1:0;}
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cout << endl; }
+
+/*
+ 
+ 10/21/2018
+ 
+ 18:31-18:58 AC (151.10 pt)
+ 
+ http://apps.topcoder.com/wiki/display/tc/SRM+522
+ 
+ */
+int memo[2][1<<14];
+const string p1="Alice",p2="Bob";
+class RowAndCoins {
+  public:
+  int N;
+  string S;
+  int f(int p, int mask) {
+    int &res=memo[p][mask];
+    if(res>=0) return res;
+//    dump3(p,mask,__builtin_popcount(mask));
+    if(__builtin_popcount(mask)==N-1) {
+      REP(i,N) if(((mask>>i)&1)==0) {
+//        dump4(p,mask,i,S[i]==('A'+p));
+        return res=(S[i]==('A'+p));
+      }
+      assert(false);
+    }
+    assert(__builtin_popcount(mask)!=N);
+    res=0;
+    REP(i,N) {
+      int mask2=mask;
+      FOR(j,i,N) {
+        if((mask>>j)&1) break;
+        mask2|=1<<j;
+        if(__builtin_popcount(mask2)==N) break;
+        res|=!f(p^1,mask2);
+      }
+    }
+//    dump3(p,mask,res);
+    return res;
+  }
+  string getWinner(string S) {
+    MINUS(memo);
+    this->S=S;
+    N=SZ(S);
+    return f(0,0)?p1:p2;
+  }
+};
+
+// CUT begin
+ifstream data("RowAndCoins.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string cells, string __expected) {
+    time_t startClock = clock();
+    RowAndCoins *instance = new RowAndCoins();
+    string __result = instance->getWinner(cells);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string cells;
+        from_stream(cells);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(cells, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1540171856;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "RowAndCoins (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
