d2b878e275c98fb0752ead8e6e93276a96320a4e
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-01-20 11:55:41 -0800

    solve GooseInZooDivOne SRM 578 div1 easy

---
+++ solutions/GooseInZooDivOne/GooseInZooDivOne.cpp
@@ -0,0 +1,308 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<vector<int>> VVI;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpf(args...) if(TRACE) { fprintf(stdout, ##args); putc('\n',stdout); }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cerr << endl; }
+template<class Iter> void dumpc(Iter begin, Iter end) { if (TRACE) { for(; begin!=end; ++begin) { cout<<*begin<<','; } cout<<endl; } }
+
+/*
+ 
+ 1/20/2019
+ 
+ 11:16-11:38 AC (165.67 pt)
+ 
+ https://apps.topcoder.com/wiki/display/tc/SRM+578
+ http://kmjp.hatenablog.jp/entry/2013/05/03/1000
+ http://ekaing.hatenablog.com/entry/2013/05/04/002250
+ http://tatsyblog.sakura.ne.jp/wordpress/contests/topcoder/251/
+ http://area.hateblo.jp/entry/2014/06/04/075548
+ https://creep06.hatenablog.com/entry/20181104/1541290111
+ 
+ */
+
+const int MOD=1e9+7;
+struct ModInt {
+  unsigned int val;
+  ModInt(): val(0) {}
+  ModInt(int v) { norm(v%MOD); }
+  ModInt(long long v) { norm(v%MOD); }
+  ModInt& norm(long long v) {
+    v=v<0?v%MOD+MOD:v; // negative
+    v=v>=MOD?v-MOD:v; // mod
+    val=(unsigned int)v;
+    return *this;
+  }
+  explicit operator bool() const { return val!=0; }
+  ModInt operator-() const { return ModInt(0)-*this; }
+  ModInt &operator+=(ModInt that) { return norm((long long)val+that.val); }
+  ModInt &operator-=(ModInt that) { return norm((long long)val-that.val); }
+  ModInt &operator*=(ModInt that) { val=(unsigned long long)val*that.val%MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return *this*=that.inv(); }
+  ModInt operator+(ModInt that) const { return ModInt(*this)+=that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this)-=that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this)*=that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this)/=that; }
+  ModInt pow(long long n) const {
+    ModInt x=*this, res=1;
+    while(n>0) {
+      if(n&1) res*=x;
+      x*=x,n>>=1;
+    }
+    return res;
+  }
+  ModInt inv() const { return (*this).pow(MOD-2); }
+  bool operator==(ModInt that) const { return val==that.val; }
+  bool operator!=(ModInt that) const { return val!=that.val; }
+  friend ostream& operator<<(ostream& os, const ModInt& that) { return os<<that.val; }
+};
+LL choose(LL n, LL k) {
+  if(n<k) return 0;
+  
+  const int MAX_N = 1e6+1;
+  assert(k<=MAX_N);
+  static LL fact[MAX_N+1],revfact[MAX_N+1],rev[MAX_N+1];
+  
+  if(rev[1]==0) {
+    rev[1]=1;
+    for(int i=2;i<=MAX_N;i++) rev[i]=rev[MOD%i]*(MOD-MOD/i)%MOD;
+    fact[0]=1,revfact[0]=1;
+    for(int i=1;i<=MAX_N;i++) {
+      fact[i]=fact[i-1]*i%MOD;
+      revfact[i]=revfact[i-1]*rev[i]%MOD;
+    }
+  }
+  return fact[n]*revfact[n-k]%MOD*revfact[k]%MOD;
+}
+struct UF {
+public:
+  int groupNum;
+  UF(int N) {
+    uf=vector<int>(N);
+    rank=vector<int>(N);
+    for(int i=0; i<N; ++i) uf[i]=i,rank[i]=0;
+    groupNum=N;
+  }
+  
+  int find(int i) {
+    return uf[i]==i?i:uf[i]=find(uf[i]);
+  }
+  
+  bool same(int x, int y) {
+    return find(x)==find(y);
+  }
+  
+  void unite(int x, int y) {
+    x=find(x);
+    y=find(y);
+    if(x==y) return;
+    groupNum--;
+    
+    if (rank[x]==rank[y]) {
+      uf[x]=y;
+      rank[y]++;
+      return;
+    }
+    
+    if (rank[x]<rank[y]) {
+      uf[x]=y;
+    } else {
+      uf[y] =x;
+      if(rank[x]==rank[y]) ++rank[x];
+    }
+  }
+private:
+  vector<int> rank;
+  vector<int> uf;
+};
+
+class GooseInZooDivOne {
+  public:
+  int count(vector<string> A, int D) {
+    int N=SZ(A),M=SZ(A[0]);
+    
+    vector<II> X;
+    REP(i,N)REP(j,M) if(A[i][j]=='v') X.emplace_back(i,j);
+    UF uf(SZ(X));
+    int L=SZ(X);
+    REP(j,L)REP(i,j) {
+      II p1=X[i],p2=X[j];
+      int d=abs(p1.first-p2.first)+abs(p1.second-p2.second);
+      if(d<=D) uf.unite(i,j);
+    }
+    map<int,int> cnt;
+    REP(i,L)cnt[uf.find(i)]++;
+    int a=0,b=0;
+    FORR(kvp,cnt) {
+      if(kvp.second%2==0) ++a;
+      else ++b;
+    }
+    ModInt res=ModInt(2).pow(a);
+    ModInt sum(0);
+    for(int x=0; x<=b; x+=2) sum+=ModInt(choose(b,x));
+    res*=sum;
+    res-=ModInt(1);
+    return res.val;
+  }
+};
+
+// CUT begin
+ifstream data("GooseInZooDivOne.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> field, int dist, int __expected) {
+    time_t startClock = clock();
+    GooseInZooDivOne *instance = new GooseInZooDivOne();
+    int __result = instance->count(field, dist);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> field;
+        from_stream(field);
+        int dist;
+        from_stream(dist);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(field, dist, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1548011755;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "GooseInZooDivOne (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
