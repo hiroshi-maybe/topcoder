b64f7e45d19e3ac3db51ab676853685efb54a6d3
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-01-20 08:39:43 -0800

    solve EllysRoomAssignmentsDiv1 SRM 577 div1 easy

---
+++ solutions/EllysRoomAssignmentsDiv1/EllysRoomAssignmentsDiv1.cpp
@@ -0,0 +1,231 @@
+#include <fstream> // for greed plugin
+#include <iostream>
+#include <algorithm> // max,min
+#include <vector>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <iostream>
+#include <utility>
+#include <cctype>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <cstring>
+#include <tuple>
+#include <cassert>
+#include <numeric>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair< int , int > II;
+typedef tuple< int, int, int > III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<vector<int>> VVI;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cout
+#define TRACE true
+#define dump(x) if(TRACE) { cout << #x << " = " << (x) << endl; }
+#define dump2(x,y) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << endl; }
+#define dump3(x,y,z) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << endl; }
+#define dump4(x,y,z,a) if(TRACE) { cout << #x << " = " << (x) << ", " << #y << " = " << (y) << ", " << #z << " = " << (z) << ", " << #a << " = " << (a) << endl; }
+#define dumpf(args...) if(TRACE) { fprintf(stdout, ##args); putc('\n',stdout); }
+#define dumpAR(ar) if(TRACE) { FORR(x,(ar)) { cout << x << ','; } cerr << endl; }
+template<class Iter> void dumpc(Iter begin, Iter end) { if (TRACE) { for(; begin!=end; ++begin) { cout<<*begin<<','; } cout<<endl; } }
+
+/*
+ 
+ 1/19/2019
+ 
+ 18:06-18:28 pause
+ 
+ 1/20/2019
+ 
+ 8:13-8:32 AC
+ 
+ https://apps.topcoder.com/wiki/display/tc/SRM+577
+ http://kmjp.hatenablog.jp/entry/2013/05/03/0900
+ https://creep06.hatenablog.com/entry/20181102/1541101259
+ http://torus711.hatenablog.com/entry/20130426/p1
+ 
+ */
+class EllysRoomAssignmentsDiv1 {
+  public:
+  double getAverage(vector<string> ratings) {
+    VI A;
+    string S;
+    FORR(s,ratings) S+=s;
+    stringstream ss(S);
+    string x;
+    while(getline(ss,x,' ')) A.push_back(stoi(x));
+    int el=A.front();
+    int N=SZ(A);
+    sort(A.rbegin(),A.rend());
+    int R=(N+19)/20;
+    
+    int G=(N+R-1)/R;
+    
+    vector<double> E(G);
+    int elg=-1,lcnt=-1;
+    REP(i,G) {
+      bool hasel=false;
+      double sum=0; int cnt=0;
+      for(int j=R*i; j<N&&j<R*(i+1); ++j) {
+        if(A[j]==el) hasel=true;
+        sum+=A[j],++cnt;
+      }
+      if(hasel) {
+        elg=i;
+        E[i]=el;
+      } else {
+        E[i]=sum/cnt;
+      }
+//      dump3(i,cnt,E[i]);
+      lcnt=cnt;
+    }
+//    dump4(R,G,elg,lcnt);
+    double res=0;
+    if(elg==G-1) {
+      res=accumulate(ALL(E),0.0)/G;
+    } else {
+      double a=accumulate(ALL(E),0.0)/G*lcnt/R;
+      double b=accumulate(E.begin(),E.begin()+G-1,0.0)/(G-1)*(R-lcnt)/R;
+      res=a+b;
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("EllysRoomAssignmentsDiv1.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool double_equal(const double &a, const double &b) { return b==b && a==a && fabs(b - a) <= 1e-9 * max(1.0, fabs(a) ); }
+
+bool do_test(vector<string> ratings, double __expected) {
+    time_t startClock = clock();
+    EllysRoomAssignmentsDiv1 *instance = new EllysRoomAssignmentsDiv1();
+    double __result = instance->getAverage(ratings);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (double_equal(__expected, __result)) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> ratings;
+        from_stream(ratings);
+        next_line();
+        double __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(ratings, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1547949926;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "EllysRoomAssignmentsDiv1 (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
