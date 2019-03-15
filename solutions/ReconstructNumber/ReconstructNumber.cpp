3c931c80de6f509f21e008b2d05af7cdc77054e7
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-03-15 15:32:32 +0900

    solve ReconstructNumber SRM 752 div1 easy

---
+++ solutions/ReconstructNumber/ReconstructNumber.cpp
@@ -0,0 +1,181 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) {  cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+/*
+ 
+ 3/14/2019
+ 
+ 12:21-12:56 AC 129.77 (pt)
+ 
+ https://www.topcoder.com/blog/single-round-match-752-editorials/
+ http://kmjp.hatenablog.jp/entry/2019/03/10/1030
+ http://noimin.hatenablog.com/entry/2019/03/13/231122
+ 
+ */
+int nex[2010][10];
+bool dp[2010][10];
+class ReconstructNumber {
+  public:
+  string smallest(string S) {
+    int N=SZ(S);
+    if(N==0) return "1";
+    ZERO(dp);
+    REPE(i,N)REP(d,10) nex[i][d]=99;
+    REP(d,10) dp[N][d]=true;
+    for(int i=N-1; i>=0; --i)REP(d,10)if(dp[i+1][d]) {
+      VI D;
+      if(S[i]=='=') D.push_back(d);
+      if(S[i]=='!') REP(dd,10)if(d!=dd) D.push_back(dd);
+      if(S[i]=='<') REP(dd,d)D.push_back(dd);
+      if(S[i]=='>') FOR(dd,d+1,10) D.push_back(dd);
+      FORR(dd,D) if(nex[i][dd]>d) dp[i][dd]=true,nex[i][dd]=d;
+    }
+    FOR(d,1,10) if(nex[0][d]<10) {
+      int cur=d;
+      string res(1,'0'+cur);
+      REP(i,N) {
+        res.push_back('0'+nex[i][cur]);
+        cur=nex[i][cur];
+      }
+      assert(SZ(res)==N+1);
+      return res;
+    }
+    return "";
+  }
+};
+
+// CUT begin
+ifstream data("ReconstructNumber.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(string comparisons, string __expected) {
+    time_t startClock = clock();
+    ReconstructNumber *instance = new ReconstructNumber();
+    string __result = instance->smallest(comparisons);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        string comparisons;
+        from_stream(comparisons);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(comparisons, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1552533665;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "ReconstructNumber (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
