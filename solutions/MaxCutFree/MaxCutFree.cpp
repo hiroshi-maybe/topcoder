f0b320d5377c08e6ff92d510b1f7718cda25f002
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-04-11 17:58:49 -0700

    solve MaxCutFree SRM 753 div1 easy

---
+++ solutions/MaxCutFree/MaxCutFree.cpp
@@ -0,0 +1,247 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) {  cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+/*
+ 
+ 4/11/2019
+
+ 10:38-11:10, 16:00-16:10 analysis
+ 16:27 AC
+ 
+ We need to solve something like maximum independent set.
+ However we cannot solve it in a general graph with given constraint.
+ I want to solve it by tree. Ok picking up only bridges make forrest. Solved.
+ 
+ http://kmjp.hatenablog.jp/entry/2019/03/22/0900
+ https://twitter.com/torus711/status/1108407408810823680
+ https://twitter.com/ei1333/status/1108406968316649472
+ https://twitter.com/_TTJR_/status/1108406663533387777
+ https://www.topcoder.com/blog/single-round-match-753-editorials/
+ 
+ */
+
+struct UF {
+public:
+  int N,G/* # of disjoint sets */;
+  UF(int N): N(N) { init(N); }
+  void init(int N) {
+    par=vector<int>(N),sz=vector<int>(N,1);
+    for(int i=0; i<N; ++i) par[i]=i,sz[i]=1;
+    G=N;
+  }
+  int find(int x) { return par[x]==x?x:par[x]=find(par[x]); }
+  int size(int x) { return sz[find(x)]; }
+  bool sameset(int x, int y) { return find(x)==find(y); }
+  int unite(int x, int y) {
+    x=find(x),y=find(y);
+    if(x==y) return x;
+    int p=x,c=y;
+    if(sz[p]<sz[c]) swap(p,c);
+    G--,sz[p]=sz[c]=sz[p]+sz[c];
+    par[c]=p;
+    return p;
+  }
+private:
+  vector<int> par,sz;
+};
+
+const int MAX_N=1000+1;
+VI G[MAX_N];
+int N;
+
+int viz[MAX_N];
+int memo[MAX_N][2];
+int dfs(int u, int paint, int pre) {
+  viz[u]=1;
+  int &res=memo[u][paint];
+  if(res>=0) return res;
+  res=paint;
+  FORR(v,G[u]) if(v!=pre) {
+    if(paint) res+=dfs(v,0,u);
+    else res+=max(dfs(v,1,u),dfs(v,0,u));
+  }
+  return res;
+}
+
+class MaxCutFree {
+  public:
+  int solve(int _N, vector<int> A, vector<int> B) {
+    N=_N;
+    UF uf1(N);
+    REP(i,SZ(A)) {
+      int u=A[i],v=B[i];
+      uf1.unite(u,v);
+    }
+    REP(u,N) G[u].clear();
+    REP(i,SZ(A)) {
+      UF uf2(N);
+      REP(j,SZ(A)) if(i!=j) {
+        int u=A[j],v=B[j];
+        uf2.unite(u,v);
+      }
+      int u=A[i],v=B[i];
+      if(!uf2.sameset(u,v)&&uf1.sameset(u,v)) {
+        G[u].push_back(v),G[v].push_back(u);
+      }
+    }
+    
+    int res=0;
+    ZERO(viz),MINUS(memo);
+    REP(i,N) if(!viz[i]) {
+      int x=max(dfs(i,0,-1),dfs(i,1,-1));
+      res+=x;
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("MaxCutFree.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int n, vector<int> a, vector<int> b, int __expected) {
+    time_t startClock = clock();
+    MaxCutFree *instance = new MaxCutFree();
+    int __result = instance->solve(n, a, b);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int n;
+        from_stream(n);
+        vector<int> a;
+        from_stream(a);
+        vector<int> b;
+        from_stream(b);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(n, a, b, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1555004284;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 300 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "MaxCutFree (300 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
