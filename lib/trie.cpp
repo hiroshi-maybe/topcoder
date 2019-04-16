23e5d72e3cdf6922cabf54a64206c3e4f7d3f4d7
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-04-15 18:47:50 -0700

    Simplify Trie tree library

--- lib/trie.cpp
+++ lib/trie.cpp
@@ -1,83 +1,120 @@
-class TrieNode {
-public:
-    bool hasWord = false;
-    vector<TrieNode *> children;
-    // Initialize your data structure here.
-    TrieNode() {
-        this->children = vector < TrieNode * > (26, NULL);
-    }
-};
-
-class Trie {
-public:
-    Trie() {
-        root = new TrieNode();
-    }
-
-    // Inserts a word into the trie.
-    void insert(string word) {
-        TrieNode *n = root;
-        for(int i=0; i<word.size(); i++) {
-            int w = word[i]-'a';
-            TrieNode *next = n->children[w];
-            if (next==NULL) {
-            	next = new TrieNode();
-            	n->children[w] = next;
-            }
-            n = next;
-        }
-        n->hasWord = true;
-    }
-
-    // Returns if the word is in the trie.
-    bool search(string word) {
-        TrieNode *n = find(word);
-                
-        return n!=NULL ? n->hasWord : false;
-    }
-
-    // Returns if there is any word in the trie
-    // that starts with the given prefix.
-    bool startsWith(string prefix) {
-        TrieNode *n = find(prefix);
-                
-        return n!=NULL;
-    }
-        
-    TrieNode* find(string s) {
-        TrieNode *n = root;
-        for(int i=0; i<s.size(); i++) {
-            int w = s[i]-'a';
-            TrieNode *next = n->children[w];
-            if (next==NULL) {
-            	return NULL;
-            }
-            n = next;
-        }
-                
-        return n;
-    }
-
-    void release() {
-        releaseNode(root);
-    }
-private:
-    TrieNode* root;
-    void releaseNode(TrieNode* n) {
-        if (!n) return;
-        for(auto &c : n->children) releaseNode(c);
-        delete n;
-    }
-};
+#include <bits/stdc++.h>
+using namespace std;
 
 /*
  
- Binary Trie tree specialized to process binary number
+ Trie tree, O(∑len(str)) time
+ 
+ Usage:
+  TrieNode* trie=new TrieNode();
+  REP(i,N) {
+    string w=W[i];
+    reverse(w.begin(),w.end());
+    trie->insert(i,w);
+  }
+  int id=trie->search(query);
+ 
+ Used problems:
+  - https://github.com/hiroshi-maybe/leetcode/blob/master/208-Implement-Trie/Trie.cpp#L23
+   - verifier
+  - https://github.com/hiroshi-maybe/GCJ/blob/master/2019-R1A/AlienRhyme.cpp#L40
+  - https://github.com/hiroshi-maybe/topcoder/blob/master/solutions/MojisBag/MojisBag.cpp#L59
+   - binary trie tree (maximize XOR pair query)
+ 
+ */
+
+class TrieNode {
+public:
+  const char C_SIZE=26;
+  const char BASE='a';
+  int cnt=0; // # of strs in subtree
+  set<int> ids; // str ending here
+  vector<TrieNode*> cs;
+  TrieNode() { this->cs=vector<TrieNode*>(C_SIZE,nullptr); }
+  void release() { for(auto c : cs) if(c!=nullptr) c->release(), delete c; }
+  void insert(int id, string &s, int p=0) {
+    ++cnt;
+    if(p==s.size()) {
+      ids.emplace(id);
+      return;
+    }
+    int i=index(s[p]);
+    auto x=cs[i];
+    if(x==nullptr) x=new TrieNode(),cs[i]=x;
+    x->insert(id,s,p+1);
+  }
+  // true: erase success, false: failure, id:-1=>wildcard
+  bool erase(int id, string &s, int p=0) {
+    if(p==s.size()) {
+      bool res=false;
+      if(ids.count(id)) ids.erase(id),res=true;
+      else if(id==-1) { // wildcard
+        if(ids.size()) res=true,ids.erase(ids.begin());
+        return res;
+      }
+      return res;
+    }
+    int i=index(s[p]);
+    auto x=cs[i];
+    if(x==nullptr) return false;
+    bool res=x->erase(id,s,p+1);
+    if(res) --cnt;
+    return res;
+  }
+  TrieNode* find(string &s, int p=0) {
+    if(p==s.size()) return this;
+    int i=index(s[p]);
+    if(cs[i]==nullptr) return nullptr;
+    return cs[i]->find(s,p+1);
+  }
+  // success: id, failure: -1
+  int search(string s) {
+    TrieNode* n=find(s);
+    return (n!=nullptr&&n->ids.size())?*(n->ids.begin()):-1;
+  }
+  // # of strs with prefix `p`
+  int prefix(string p) {
+    TrieNode* n=find(p);
+    return n!=nullptr?n->cnt:0;
+  }
+private:
+  int index(char c) {
+    int i=c-BASE;
+    assert(0<=i&&i<C_SIZE);
+    return i;
+  }
+};
+
+void test_trie() {
+  TrieNode* trie=new TrieNode();
+  string s1="apple";
+  trie->insert(0,s1);
+  assert(trie->search("apple")==0);
+  assert(trie->search("app")==-1);
+
+  string s2="app";
+  trie->insert(1,s2);
+  assert(trie->prefix("app")==2);
+  assert(trie->search("app")==1);
+  assert(trie->search("appl")==-1);
+  
+  assert(!trie->erase(0,s2));
+  assert(trie->erase(-1,s2));
+  
+  assert(trie->search(s2)==-1);
+  
+  trie->release(), delete trie;
+}
+
+/*
+ 
+ Binary Trie tree specialized to process binary digits
  
  Used problem(s):
   - https://github.com/hiroshi-maybe/topcoder/blob/master/solutions/MojisBag/MojisBag.cpp#L59
  
  */
+/*
 class BinaryTrieNode {
 public:
   int cnt=0;
@@ -167,4 +204,11 @@ private:
     REP(i,2) releaseNode(n->children[i]);
     delete n;
   }
-};
+};*/
+
+int main() {
+  test_trie();
+  return 0;
+}
+
+// $ g++ -std=c++11 -Wall -O2 -D_GLIBCXX_DEBUG trie.cpp && ./a.out
