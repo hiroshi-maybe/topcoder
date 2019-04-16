876b95035faab1431754c82f7e94c0d2561afda4
Hiroshi Kori <hiroshi.maybe@gmail.com>
2019-04-15 18:45:30 -0700

    revise Trie tree

--- solutions/MojisBag/MojisBag.cpp
+++ solutions/MojisBag/MojisBag.cpp
@@ -29,7 +29,7 @@ template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; +
 void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
 template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) {  cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
 template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
-//#define __KUMATRACE__ true
+#define __KUMATRACE__ true
 #ifdef __KUMATRACE__
 #define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
 #define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
@@ -56,110 +56,104 @@ template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, con
  
  */
 
-class BinaryTrieNode {
+class TrieNode {
 public:
-  int cnt=0;
-  set<int> ids;
-  LL val=-1;
-  BinaryTrieNode* children[2];
-  // Initialize your data structure here.
-  BinaryTrieNode() {
-    children[0]=children[1]=nullptr;
+  const char C_SIZE=2;
+  const char BASE='0';
+  int cnt=0; // # of strs in subtree
+  set<int> ids; // str ending here
+  vector<TrieNode*> cs;
+  TrieNode() { this->cs=vector<TrieNode*>(C_SIZE,nullptr); }
+  void release() { for(auto c : cs) if(c!=nullptr) c->release(), delete c; }
+  void insert(int id, string &s, int p=0) {
+    ++cnt;
+    if(p==s.size()) {
+      ids.emplace(id);
+      return;
+    }
+    int i=index(s[p]);
+    auto x=cs[i];
+    if(x==nullptr) x=new TrieNode(),cs[i]=x;
+    x->insert(id,s,p+1);
+  }
+  // true: erase success, false: failure, id:-1=>wildcard
+  bool erase(int id, string &s, int p=0) {
+    if(p==s.size()) {
+      bool res=false;
+      if(ids.count(id)) ids.erase(id),res=true;
+      else if(id==-1) { // wildcard
+        if(ids.size()) res=true,ids.erase(ids.begin());
+        return res;
+      }
+      return res;
+    }
+    int i=index(s[p]);
+    auto x=cs[i];
+    if(x==nullptr) return false;
+    bool res=x->erase(id,s,p+1);
+    if(res) --cnt;
+    return res;
+  }
+  TrieNode* find(string &s, int p=0) {
+    if(p==s.size()) return this;
+    int i=index(s[p]);
+    if(cs[i]==nullptr) return nullptr;
+    return cs[i]->find(s,p+1);
+  }
+  // success: id, failure: -1
+  int search(string s) {
+    TrieNode* n=find(s);
+    return (n!=nullptr&&n->ids.size())?*(n->ids.begin()):-1;
+  }
+  // # of strs with prefix `p`
+  int prefix(string p) {
+    TrieNode* n=find(p);
+    return n!=nullptr?n->cnt:0;
+  }
+  
+  // id, value
+  pair<int,LL> xorcp(string s, int p=0, LL val=0LL) {
+    if(p==s.size()) {
+//      assert(SZ(ids));
+      return {*(ids.begin()), val};
+    }
+    int b=s[p]-'0';
+    auto next=cs[b^1];
+    if(next==nullptr||next->cnt==0) next=cs[b],val=val*2LL+b;
+    else val=val*2LL+(b^1);
+    assert(next!=nullptr);
+    return next->xorcp(s,p+1,val);
+  }
+private:
+  int index(char c) {
+    int i=c-BASE;
+    assert(0<=i&&i<C_SIZE);
+    return i;
   }
 };
 
-class BinaryTrie {
-public:
-  const int MAX_D=30;
-  BinaryTrie() {
-    root=new BinaryTrieNode();
-  }
-  void insert(LL X, int id) {
-    dump("add",X,id);
-    BinaryTrieNode *n=root; n->cnt++;
-    for(int i=MAX_D; i>=0; --i) {
-      int b=(X>>i)&1;
-      assert(n!=nullptr);
-      assert(0<=b&&b<2);
-      BinaryTrieNode *next=n->children[b];
-      if(next==nullptr) {
-        next=new BinaryTrieNode();
-        n->children[b]=next;
-      }
-      assert(next!=nullptr);
-      next->cnt++;
-      n=next;
-    }
-    n->val=X;
-    n->ids.emplace(id);
-  }
-  void erase(LL X, int id) {
-    dump("rem",X,id);
-    BinaryTrieNode *n=root; n->cnt--;
-    for(int i=MAX_D; i>=0; --i) {
-      int b=(X>>i)&1;
-      BinaryTrieNode *next=n->children[b];
-      if(next==nullptr) return;
-      next->cnt--;
-      n=next;
-    }
-    assert(n!=nullptr);
-    n->ids.erase(id);
-  }
-  // id, value
-  pair<int,LL> xorcp(LL X) {
-    BinaryTrieNode *n=root;
-    for(int i=MAX_D; i>=0; --i) {
-      int b=(X>>i)&1;
-      BinaryTrieNode *next=n->children[b^1];
-      if(next==nullptr||next->cnt==0) next=n->children[b];
-      assert(next!=nullptr);
-      n=next;
-    }
-    assert(n!=nullptr);
-    return {*(n->ids.begin()), n->val};
-  }
-  set<int> search(LL q) {
-    BinaryTrieNode *n=find(q);
-    return n!=nullptr?n->ids:set<int>();
-  }
-  
-  BinaryTrieNode* find(LL q) {
-    BinaryTrieNode *n=root;
-    for(int i=MAX_D; i>=0; --i) {
-      int b=(q>>i)&1;
-      BinaryTrieNode *next=n->children[b];
-      if(next==nullptr) return nullptr;
-      n=next;
-    }
-    return n;
-  }
-  
-  void release() {
-    releaseNode(root);
-  }
-private:
-  BinaryTrieNode* root;
-  void releaseNode(BinaryTrieNode* n) {
-    if (!n) return;
-    REP(i,2) releaseNode(n->children[i]);
-    delete n;
-  }
-};
+const int MAX_D=31;
+string tobin(LL x) {
+  string res(MAX_D,'?');
+  REP(i,MAX_D) res[i]='0'+((x>>i)&1);
+  reverse(ALL(res));
+//  dump(x,res);
+  return res;
+}
 
 const LL MOD=1e9+7;
 class MojisBag {
   public:
   int maximumXor(int Q, int B, int A, int R) {
-    BinaryTrie *T=new BinaryTrie();
+    TrieNode* trie=new TrieNode();
     II maxp={-1,-1};
     LL x=A;
     set<int> valid;
     vector<LL> S,Y,Z;
     auto add=[&](LL x, int id) {
+      string xs=tobin(x);
       if(maxp.first!=-1||maxp.second!=-1) {
-        auto p=T->xorcp(x);
-        dump(p.first,p.second);
+        auto p=trie->xorcp(xs);
         if(maxp.first==-1||maxp.second==-1) {
           maxp={id,p.first};
         } else if((x^p.second)>(S[maxp.first]^S[maxp.second])) {
@@ -167,10 +161,11 @@ class MojisBag {
         }
       } else maxp={id,-1};
       
-      T->insert(x,id);
+      trie->insert(id,xs);
       valid.emplace(id);
     };
     REP(i,Q) {
+//      dump(i,x,x%R);
       if(x%R!=0) {
         // add
         add(x,SZ(S));
@@ -180,23 +175,27 @@ class MojisBag {
         int remid=x%SZ(S);
         if(valid.count(remid)) {
           valid.erase(remid);
-          T->erase(S[remid], remid);
+          string s=tobin(S[remid]);
+          trie->erase(remid,s);
           if(remid==maxp.first||remid==maxp.second) {
             maxp={-1,-1};
-            FORR(i,valid) T->erase(S[i],i);
+            FORR(i,valid) {
+              string s=tobin(S[i]);
+              trie->erase(i,s);
+            }
             FORR(i,valid) add(S[i],i);
           }
         }
       }
-      dump(i,maxp.first,maxp.second);
+//      dump(i,maxp.first,maxp.second);
       LL y=(maxp.first==-1||maxp.second==-1)?0:S[maxp.first]^S[maxp.second];
       LL z=i==0?y:(Z.back()*B+y)%MOD;
       Y.push_back(y),Z.push_back(z);
       x=(x*B+A)%MOD;
     }
     
-    dumpc(Y);
-    T->release();
+//    dumpc(Y);
+    trie->release(),delete trie;
     return Z[Q-1];
   }
 };
