ce9c0314441000c00af12e62a3440bba63906e8d
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-04-24 22:30:11 -0700

    solve SRM784 div1 easy ValueDivision

---
+++ solutions/ValueDivision/ValueDivision.cpp
@@ -0,0 +1,239 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) {  cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+
+/*
+ 
+ 4/24/2020 SRM 784 div1 easy
+ 
+ 8:00-8:25 AC
+ 
+ https://kmjp.hatenablog.jp/entry/2020/04/25/0900
+ https://www.topcoder.com/single-round-match-784-editorials/
+ 
+ */
+
+#include <chrono>
+#include <random>
+// mt19937_64 for 64 bit unsigned integer
+//mt19937 rnd(time(nullptr));
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+int genRandNum(int lb, int ub) {
+  assert(lb<ub);
+  // Helper to generate random integer in range [lb, ub)
+  int x = rnd() % (int)(ub - lb);
+  return x + lb;
+}
+vector<int> genRandSeq(int size, int lb, int ub) {
+  if (size==0) return {};
+  vector<int> res(size);
+  generate(res.begin(), res.end(), [&]() {
+    return genRandNum(lb, ub);
+  });
+  return res;
+}
+class ValueDivision {
+  public:
+  vector<int> f(VI &A) {
+    int N=SZ(A);
+    map<int,VI> cnt;
+    REP(i,N) cnt[A[i]].push_back(i);
+    VV<int> xs(SZ(cnt));
+    VI as;
+    FORR(kvp,cnt) {
+      as.push_back(kvp.first);
+      FORR(i,kvp.second) {
+        xs[SZ(as)-1].push_back(i);
+      }
+    }
+    
+    VI res(N,-1);
+    priority_queue<int> Q;
+    for(int i=SZ(as)-1; i>=0; --i) {
+      int a=as[i];
+      FORR(j,xs[i]) Q.emplace(j);
+      int apre=i==0?1:as[i-1];
+      while(a>apre&&SZ(Q)) {
+        int j=Q.top(); Q.pop();
+        res[j]=a;
+        --a;
+      }
+    }
+    while(SZ(Q)) {
+      int i=Q.top(); Q.pop();
+      res[i]=1;
+    }
+    assert(count(ALL(res),-1)==0);
+    dumpc(res);
+    return res;
+  }
+  
+  vector<int> getArray(vector<int> A) {
+/*
+    while(true) {
+      int N=genRandNum(1,100);
+      VI x=genRandSeq(N,1,20);
+      dumpc(x);
+      f(x);
+    }*/
+    
+    return f(A);
+  }
+};
+
+// CUT begin
+ifstream data("ValueDivision.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+template <typename T> string to_string(vector<T> ts) {
+    stringstream s;
+    s << "[ ";
+    for (int i = 0; i < ts.size(); ++i) {
+        if (i > 0) s << ", ";
+        s << to_string(ts[i]);
+    }
+    s << " ]";
+    return s.str();
+}
+
+bool do_test(vector<int> A, vector<int> __expected) {
+    time_t startClock = clock();
+    ValueDivision *instance = new ValueDivision();
+    vector<int> __result = instance->getArray(A);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> A;
+        from_stream(A);
+        next_line();
+        vector<int> __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(A, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1587740419;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "ValueDivision (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
