b5085624de12f2d39eb96517addf86abd6816ba1
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-05-09 16:24:54 -0700

    add solution of SRM785 div1 medium EllysTwoRatings

---
+++ solutions/EllysTwoRatings/EllysTwoRatings.cpp
@@ -0,0 +1,198 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) {  cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+/*
+ 
+ 5/9/2020
+ 
+ 9:35-10:20 give up
+ 
+ 14:20-16:20 Read editorials and add solution
+ 
+ https://www.topcoder.com/single-round-match-785-editorials/
+ 
+ */
+class EllysTwoRatings {
+  public:
+  II range(int a) {
+    int lb=max(1,a-100),hb=min(1000,a+100);
+    return {lb,hb};
+  }
+  double getChance(int N, int A, int B) {
+    VV<double> dp(1001,vector<double>(1001,0));
+    dp[A][B]=1;
+
+    double res=0;
+    REP(i,N) {
+      FORE(b,1,1000) {
+        vector<double> cum(1002);
+        FORE(a,1,1000) {
+          auto ra=range(a);
+          double pa=1.0/(ra.second-ra.first+1);
+          cum[a+1]=cum[a]+dp[a][b]*pa;
+        }
+        FORE(a,1,1000) {
+          auto ra=range(a);
+          dp[a][b]=(cum[ra.second+1]-cum[ra.first]);
+        }
+      }
+      FORE(x,1,1000) res+=dp[x][x],dp[x][x]=0;
+      
+      FORE(a,1,1000) {
+        vector<double> cum(1002);
+        FORE(b,1,1000) {
+          auto rb=range(b);
+          double pb=1.0/(rb.second-rb.first+1);
+          cum[b+1]=cum[b]+dp[a][b]*pb;
+        }
+        FORE(b,1,1000) {
+          auto rb=range(b);
+          dp[a][b]=(cum[rb.second+1]-cum[rb.first]);
+        }
+      }
+      FORE(x,1,1000) res+=dp[x][x],dp[x][x]=0;
+    }
+    return res;
+  }
+};
+
+// CUT begin
+ifstream data("EllysTwoRatings.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool double_equal(const double &a, const double &b) { return b==b && a==a && fabs(b - a) <= 1e-9 * max(1.0, fabs(a) ); }
+
+bool do_test(int N, int A, int B, double __expected) {
+    time_t startClock = clock();
+    EllysTwoRatings *instance = new EllysTwoRatings();
+    double __result = instance->getChance(N, A, B);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (double_equal(__expected, __result)) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int N;
+        from_stream(N);
+        int A;
+        from_stream(A);
+        int B;
+        from_stream(B);
+        next_line();
+        double __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, A, B, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1589042145;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "EllysTwoRatings (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
