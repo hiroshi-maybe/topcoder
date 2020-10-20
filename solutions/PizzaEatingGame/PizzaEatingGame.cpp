286c9584fa0f3a67c4ac034637129019f5386fbf
Hiroshi Kori <hiroshi.maybe@gmail.com>
2020-10-20 09:41:58 -0700

    solve TCO2020 WC div1 easy PizzaEatingGame

---
+++ solutions/PizzaEatingGame/PizzaEatingGame.cpp
@@ -0,0 +1,211 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) {  cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+template <typename Integer>
+Integer genRandNum(Integer lb, Integer ub) {
+  assert(lb<ub);
+  // Helper to generate random integer in range [lb, ub)
+  Integer x = rnd() % (Integer)(ub - lb);
+  return x + lb;
+}
+template <typename Integer>
+vector<Integer> genRandSeq(int size, Integer lb, Integer ub) {
+  if (size==0) return {};
+  vector<Integer> res(size);
+  generate(res.begin(), res.end(), [&]() {
+    return genRandNum(lb, ub);
+  });
+  return res;
+}
+const int MAX_N=501;
+int memo[MAX_N][MAX_N];
+
+/*
+
+ 10/7/2020 TCO 2020 WC
+
+ 8:05-8:38 passed system test
+
+ */
+class PizzaEatingGame {
+  public:
+  int N;
+  VI A;
+  int f(int l, int len) {
+      if(len==0) return 0;
+      int &res=memo[l][len];
+      if(res>=0) return res;
+      //dump(l,len);
+      int sum=0;
+      FOR(i,l,l+len) sum+=A[i%N];
+      FOR(i,l,l+len) {
+          //dump(i-l,l+len-i,len);
+          //assert(i-l<len);
+          //assert(l+len-i-1<len);
+          SMAX(res, sum-(f(l,i-l)+f((i+1)%N,l+len-i-1)));
+      }
+      return res;
+  }
+
+  int _eat(vector<int> _A) {
+      this->A=_A,this->N=SZ(A);
+      MINUS(memo);
+      int res=0,sum=accumulate(ALL(A),0);
+
+      REP(i,N) {
+          SMAX(res, sum-f(i+1,N-1));
+      }
+      return res;
+  }
+
+  int eat(vector<int> A) {
+      return _eat(A);
+  }
+};
+
+// CUT begin
+ifstream data("PizzaEatingGame.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<int> olives, int __expected) {
+    time_t startClock = clock();
+    PizzaEatingGame *instance = new PizzaEatingGame();
+    int __result = instance->eat(olives);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<int> olives;
+        from_stream(olives);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(olives, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1602083137;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 300 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "PizzaEatingGame (300 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
