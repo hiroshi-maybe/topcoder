fbff0ff422bff2ba22cfa6e493f5f7f7018c0a55
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-01-24 13:57:35 -0800

    Add solution of SRM798 div1 medium ExpectedValue

---
+++ solutions/ExpectedValue/ExpectedValue.cpp
@@ -0,0 +1,238 @@
+#include <bits/stdc++.h>
+using namespace std;
+// type alias
+typedef long long LL;
+typedef pair<int,int> II;
+typedef tuple<int,int,int> III;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef unordered_map<int,int> MAPII;
+typedef unordered_set<int> SETI;
+template<class T> using VV=vector<vector<T>>;
+// minmax
+template<class T> inline T SMIN(T& a, const T b) { return a=(a>b)?b:a; }
+template<class T> inline T SMAX(T& a, const T b) { return a=(a<b)?b:a; }
+// repetition
+#define FORE(i,a,b) for(int i=(a);i<=(b);++i)
+#define REPE(i,n)  for(int i=0;i<=(n);++i)
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define REP(i,n)  for(int i=0;i<(n);++i)
+#define FORR(x,arr) for(auto& x:arr)
+#define SZ(a) int((a).size())
+// collection
+#define ALL(c) (c).begin(),(c).end()
+// DP
+#define MINUS(dp) memset(dp, -1, sizeof(dp))
+#define ZERO(dp) memset(dp, 0, sizeof(dp))
+// debug cerr
+template<class Iter> void __kumaerrc(Iter begin, Iter end) { for(; begin!=end; ++begin) { cerr<<*begin<<','; } cerr<<endl; }
+void __kumaerr(istream_iterator<string> it) { (void)it; cerr<<endl; }
+template<typename T, typename... Args> void __kumaerr(istream_iterator<string> it, T a, Args... args) {  cerr<<*it<<"="<<a<<", ",__kumaerr(++it, args...); }
+template<typename S, typename T> std::ostream& operator<<(std::ostream& _os, const std::pair<S,T>& _p) { return _os<<"{"<<_p.first<<','<<_p.second<<"}"; }
+#define __KUMATRACE__ true
+#ifdef __KUMATRACE__
+#define dump(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); __kumaerr(_it, args); }
+#define dumpc(ar) { cerr<<#ar<<": "; FORR(x,(ar)) { cerr << x << ','; } cerr << endl; }
+#define dumpC(beg,end) { cerr<<"~"<<#end<<": "; __kumaerrc(beg,end); }
+#else
+#define dump(args...)
+#define dumpc(ar)
+#define dumpC(beg,end)
+#endif
+template <typename Val, int MAX_N> struct Com {
+  vector<Val> fac,ifac;
+  Com() {
+    fac.resize(MAX_N+1,0),ifac.resize(MAX_N+1,0);
+    fac[0]=1,ifac[0]=1;
+    for(int i=1;i<=MAX_N;i++) fac[i]=fac[i-1]*i;
+    ifac[MAX_N]=Val(1)/fac[MAX_N];
+    for(int i=MAX_N-1; i>=1; --i) ifac[i]=ifac[i+1]*Val(i+1);
+  }
+  Val choose(int n, int k) {
+    if(n<k) return 0;
+    assert(0<=k&&k<=MAX_N);
+    return fac[n]*ifac[n-k]*ifac[k];
+  }
+  Val fact(int k) { return fac[k]; }
+  Val perm(int n, int k) {
+    if(n<k) return 0;
+    assert(0<=k&&k<=MAX_N);
+    return fac[n]*ifac[n-k];
+  }
+  Val multichoose(int n, int k) {
+    if(n==0&&k==0) return 1;
+    return choose(n+k-1,k);
+  }
+};
+const int MOD=1000000007;
+//const int MOD=998244353;
+struct ModInt {
+  unsigned int val;
+  ModInt(): val(0) {}
+  ModInt(int v) { norm(v%MOD); }
+  ModInt(long long v) { norm(v%MOD); }
+  ModInt& norm(long long v) {
+    v=v<0?v%MOD+MOD:v; // negative
+    v=v>=MOD?v-MOD:v; // mod
+    val=(unsigned int)v;
+    return *this;
+  }
+  explicit operator bool() const { return val!=0; }
+  ModInt operator-() const { return ModInt(0)-*this; }
+  ModInt &operator+=(ModInt that) { return norm((long long)val+that.val); }
+  ModInt &operator-=(ModInt that) { return norm((long long)val-that.val); }
+  ModInt &operator*=(ModInt that) { val=(unsigned long long)val*that.val%MOD; return *this; }
+  ModInt &operator/=(ModInt that) { return *this*=that.inv(); }
+  ModInt operator+(ModInt that) const { return ModInt(*this)+=that; }
+  ModInt operator-(ModInt that) const { return ModInt(*this)-=that; }
+  ModInt operator*(ModInt that) const { return ModInt(*this)*=that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this)/=that; }
+  ModInt pow(long long n) const {
+    ModInt x=*this, res=1;
+    while(n>0) {
+      if(n&1) res*=x;
+      x*=x,n>>=1;
+    }
+    return res;
+  }
+  ModInt inv() const { return (*this).pow(MOD-2); }
+  bool operator==(ModInt that) const { return val==that.val; }
+  bool operator!=(ModInt that) const { return val!=that.val; }
+  friend ostream& operator<<(ostream& os, const ModInt& that) { return os<<that.val; }
+};
+/*
+
+ 1/23/2021 SRM798
+
+ 9:33-10:20 give up
+
+ 1/24/2021
+
+ 12:00-13:47 AC after reading editorials
+
+ Editorial: https://docs.google.com/document/d/e/2PACX-1vQhzXqFKIZhr-yJVnkcWT-nj-kor30veS5_Wo1M4Xu6eDKITvIiIXcMlraD-fZ5a4ltVH2GT9x7MHyQ/pub
+
+ https://twitter.com/hanseilimak/status/1353423685747544064
+ https://artofproblemsolving.com/wiki/index.php/Derangement
+ https://www.geeksforgeeks.org/minimum-number-swaps-required-sort-array/
+
+ https://docs.google.com/document/d/1Yq384PEooRVsmlzB103YMLWwE_JhPN8AbxZagNrF_vY/edit#bookmark=id.pb1bh0v6j3k5
+ https://docs.google.com/document/d/1Yq384PEooRVsmlzB103YMLWwE_JhPN8AbxZagNrF_vY/edit#bookmark=id.k08m8insxy65
+
+ */
+
+const int MAX_N=1501;
+Com<ModInt,MAX_N> com;
+ModInt dp[MAX_N];
+ModInt D[MAX_N];
+class ExpectedValue {
+    public:
+    int solve(int N) {
+        ZERO(dp);
+        ZERO(D);
+        D[0]=1,D[1]=0;
+        FORE(n,2,N) D[n]=(D[n-1]+D[n-2])*(n-1);
+        FORE(x,2,N) {
+            FORE(y,2,x) dp[x]+=com.perm(x-1,y-1)*D[x-y]*(dp[x-y]+1);
+            dp[x]/=D[x];
+        }
+        return (ModInt(N)-dp[N]).val;
+    }
+};
+
+// CUT begin
+ifstream data("ExpectedValue.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(int N, int __expected) {
+    time_t startClock = clock();
+    ExpectedValue *instance = new ExpectedValue();
+    int __result = instance->solve(N);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        int N;
+        from_stream(N);
+        next_line();
+        int __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(N, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1611423179;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 500 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "ExpectedValue (500 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
