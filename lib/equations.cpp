28fa45588ddca43a888c9f23946e6b899916697d
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-02-28 18:43:39 -0800

    Parameterize integer type of CRT and used it in ABC193

--- lib/equations.cpp
+++ lib/equations.cpp
@@ -9,113 +9,118 @@ using namespace std;
 typedef long long LL;
 
 /*
- 
+
  Solver of system of modular linear equations,
- 
+
   - compute (b,m) s.t. x≡b(mod m), ∀i A[i]*x ≡ B[i] (mod M[i])
+   - `m` = LCM { M[i] }
   - (0,-1) => no solution (∃i s.t. gcd(A[i],M[i]) ⧷ B[i])
    - There is a solution of a*x≡b(mod m) only when gcd(a,m) divides b
   - Implementtion coming from Ant book 4.1 complex math problems
- 
+
  Proof of correctness:
   Suppose we have x[i] ≡ b[i] (mod m[i]) after solving A[i]*x[i]≡B[i](mod M[i]).
- 
+
   Thus x[i]=b[i]+m[i]*t[i]
- 
+
   We have A[i+1]*x[i]≡B[i+1](mod M[i+1]).
   If d=gcd(A[i+1],M[i+1]), A[i+1]'*x[i]≡B[i+1]' (mod M[i+1]')
   where A[i+1]'=A[i+1]/d, B[i+1]'=B[i+1]/d, M[i+1]'=M[i+1]/d
   x[i] in this formula can be substituted by b[i]+m[i]*t[i]
- 
+
   A[i+1]'*x[i]≡B[i+1]' (mod M[i+1]')
   A[i+1]'*(b[i]+m[i]*t[i])≡B[i+1]' (mod M[i+1]')
   A[i+1]'*m[i]*t[i] ≡ B[i+1]' - A[i+1]'*b[i] (mod M[i+1]')
   p * t[i] ≡ q (mod M[i+1]') where p=A[i+1]'*m[i], q=B[i+1]'-A[i+1]'*b[i]
- 
+
  This can be solved by mod inverse p^-1
   t[i] ≡ p^-1*q (mod M[i+1]')
   Now we get t[i] = M[i+1]' * t[i+1] + p^-1*q
- 
+
  x[i+1]
  = x[i]
  = b[i]+m[i]*t[i]
  = b[i]+m[i]*(M[i+1]' * t[i+1] + p^-1*q)
  = (b[i]+m[i]*p^-1*q) + (m[i]*M[i+1]')*t[i+1]
  = b[i+1] + m[i+1] * t[i+1]
- 
+
  Thus we have recurrence relations:
   b[i+1] = b[i] + m[i] * p^-1*q
   m[i+1] = m[i] * M[i+1]'
- 
+
  We can recursively calculate b[i] and m[i] (base case: x[0]=0, t[0]=1) by DP.
- 
+
  Result of system of modular linear equations is (b[N], m[N]), x ≡ b[N] (mod m[N])
- 
+
  If { m[i] } is relatively prime AND ∀i a[i]==1, m[N] = ∏ M[i] by Chinese Remainder Theorem.
- 
+
  Reference:
   - Ant book 4.1 complex math problems
   - https://en.wikipedia.org/wiki/Chinese_remainder_theorem
   - "CLRS 31.4 Solving modular linear equations" shows solver of single equation
    - this solver is generalized to solve system of equations
- 
+
  Usage:
    // x≡2(mod 3), x≡3(mod 5), x≡2(mod 7)
    vector<int> A={1,1,1};
    vector<int> B={2,3,2};
    vector<int> M={3,5,7};
-   auto p = MLE.solve(A,B,M); // p.first==23, p.second==105
- 
+   ModularLinearEquations<LL> mle;
+   auto p = mle.solve(A,B,M); // p.first==23, p.second==105
+   assert(p.first==23);
+   assert(p.second==105);
+
  Used problems:
   - https://github.com/k-ori/topcoder/blob/646f1e95c346aaaef68428e5825027350d4bf707/QuadraticIdentity/QuadraticIdentity.cpp#L122
- 
+  - https://github.com/hiroshi-maybe/atcoder/blob/4800c7aa222aee403acd9b0012ee94e203116461/solutions/Oversleeping.cpp#L45
+
  */
+template<typename integer>
 struct ModularLinearEquations {
 public:
   ModularLinearEquations() {}
-  
-  pair<int, int> solve(const vector<int>& A, const vector<int>& B,
-                       const vector<int>& M) {
-    int b=0,m=1;
+  pair<integer, integer> solve(const vector<integer>& A, const vector<integer>& B,
+                       const vector<integer>& M) {
+    integer b=0,m=1;
     for(int i=0; i<A.size(); i++) {
-      int p=A[i]*m,q=B[i]-A[i]*b,d=gcd(M[i],p),mi=M[i]/d;
+      integer p=A[i]*m,q=B[i]-A[i]*b,d=gcd(M[i],p),mi=M[i]/d;
       if (q%d!=0) return make_pair(0,-1); // solution not found
-      int t=(_modinv(p/d,mi)*q/d)%(mi);
+      integer t=(_modinv(p/d,mi)*q/d)%(mi);
       b=b+m*t;
       m*=mi;
     }
     b=(b+m)%m; // ensure that x is non-negative
     return make_pair(b%m,m);
   }
-  
+
   /*
-   
+
    compute mod inverse of n, O(lg max(a,m)) time
 
     - compute x s.t. a*x≡1(mod m)
     - a and m should be co-prime (gcd(a,m)==1)
     - from Ant book 4.1 complex math problems
     - CLRS 31.4 Solving modular linear equations
-   
+
    a*x≡1(mod m)
    <=> ∃k, a*x=k*m+1
    <=> a*x-k*m=1 // x can be computed by extgcd(a,m)
-   
+
    Usage:
      int d = modinv(5,11) // d=9
-   
+
    */
-  int _modinv(int a, int m) {
+  integer _modinv(integer a, integer m) {
     assert(gcd(a,m)==1);
-    int x,y;
+    integer x,y;
     _extgcd(a,m,x,y);
     return (m+x%m)%m; // ensure mod inverse is non-negative
   }
-  
+
   /*
-   
+
    extended GCD, O(lg max(a,b)) time
-   
+
     - compute (x,y,z) s.t. a*x+b*y=z, z=gcd(a,b)
     - from Ant book 2.6 elementary math techniques
     - CLRS "31.3 Modular arithmetic" and "31.4 Solving modular linear equations"
@@ -123,47 +128,87 @@ public:
    Usage:
      int x, y;
      int z = extgcd(99,78,x,y); // (x,y)=(-11,14), z=gcd(99,78)=3
-   
+
    */
-  int _extgcd(int a, int b, int& x, int& y) {
+  integer _extgcd(integer a, integer b, integer& x, integer& y) {
     if(b==0) {
       x=1,y=0;
       return a;
     }
-    int d=_extgcd(b, a%b, y, x);
+    integer d=_extgcd(b, a%b, y, x);
     y-=(a/b)*x;
     return d;
   }
 private:
-  int gcd(int a, int b) {
+  int gcd(integer a, integer b) {
     return b==0?a:gcd(b,a%b);
   }
-} MLE;
+};
+
+void test_MLE() {
+  ModularLinearEquations<LL> mle;
+
+  {
+    assert(mle._modinv(5,11)==9);
+    LL x,y;
+    int d=mle._extgcd(99,78,x,y);
+    assert(d==3);
+    assert(x==-11);
+    assert(y==14);
+  }
+
+  {
+    vector<LL> A={1,1,1};
+    vector<LL> B={2,3,2};
+    vector<LL> M={3,5,7};
+    auto p = mle.solve(A,B,M);
+    assert(p.first==23);
+    assert(p.second==105);
+  }
+
+  {
+    vector<LL> A={1,1};
+    vector<LL> B={1350427,3475255};
+    vector<LL> M={4250955,4164018};
+    auto p = mle.solve(A,B,M);
+    assert(p.first==4992365411977);
+    assert(p.second==5900351045730);
+  }
+
+  {
+    vector<LL> A={1,1};
+    vector<LL> B={4,8};
+    vector<LL> M={5,15};
+    auto p = mle.solve(A,B,M);
+    assert(p.first==0);
+    assert(p.second==-1);
+  }
+}
 
 /*
- 
+
  Compute rank by Gaussian elimination in GF(2), O(N^2*lg N) time
- 
+
  GF(2)
    - CLRS "D-2 Permutations defined by matrix-vector multiplication over GF(2)" Problems for Appendix D
    - https://www.cs.umd.edu/~gasarch/TOPICS/factoring/fastgauss.pdf
    - http://personals.ac.upc.edu/enricm/Pubs/pdp2015_slides.pdf
      - great overview of Gaussian elimination in GF(2) ✅
    - https://en.wikipedia.org/wiki/Finite_field#Field_with_four_elements
- 
+
  Usage:
    int rank = gf2_rank<int>(V);
- 
+
  Note of GF(2)
- 
+
  Gaussian Elimination is defined over an algebraic field
    - Infinite fields: 𝐐, 𝐑
    - Finite fields: Galois field with 2 elements a.k.a. GF(2)
- 
+
  In GF(2)={0,1}, algebraic operations are represented as below:
    - addition ≡ XOR ≡ subtraction (+1=-1)
    - multiplication ≡ AND
- 
+
  Implementation remarks:
    - Gaussian elimination in infinite fields can be specialized for GF(2)
    - Gauss-Jordan Elimination can be easily merged into GE
@@ -171,27 +216,27 @@ private:
    - computer arithmetic over GF(2) is always exact
  Applications
    - factoring large integer numbers, cryptography, pattern matching...
- 
+
  Thus Gaussian elimination works in GF(2).
  We can just compute rank of GF(2) matrix.
  If we have linearly independent vectors X_a and X_b,
  Any X[i] can be represented as below:
- 
+
  X[i] = (C_a & X_a) ^ (C_b & X_b), C_x ∈ {0,1}
  like... V[i] = C_a * V_a + C_b * V_b in infinite field
- 
+
  Note that ^(XOR) and &(AND) are algebraic operation in GF(2).
  So we have same property as seen in linear algebra in infinite field.
- 
+
  GE in GF(2) gives us linearly independent vectors.
  Expected result is number of those vectors (= rank of original matrix)
- 
+
  We can do LU-decomposition in GF(2) by reducing MSB.
- 
+
  Used problems:
   - https://github.com/hiroshi-maybe/topcoder/blob/bc4fd010dc1100360b7462bf38a190426084490c/solutions/MixingColors/MixingColors.cpp#L123
   - https://github.com/hiroshi-maybe/codeforces/blob/master/solutions/ZeroXORSubsetLess.cpp#L110
- 
+
  */
 template<class T> void gf2_GE(vector<T>& V) {
   // gaussian elimination in GF(2)
@@ -210,16 +255,16 @@ template<class T> int gf2_rank(vector<T>& V) {
 }
 
 /*
- 
+
  Solver of system of linear equations by Gauss–Jordan elimination, Θ(N^3) time
- 
+
  - implementation from Ant book
- 
+
  Basic steps:
  1. Setup matrices A*x=b where A=NxN, b=Nx1
  2. By row reduction, transform A to echelon form (triangular form) which is U matrix in LU-composition
  3. Compute x by back-substitution
- 
+
  References:
   - https://en.wikipedia.org/wiki/Gaussian_elimination
   - https://en.wikipedia.org/wiki/LU_decomposition
@@ -227,10 +272,10 @@ template<class T> int gf2_rank(vector<T>& V) {
   - Ant book 4.1 more complex math problems
   - CLRS 28.1 Solving systems of linear equations
   - http://kmjp.hatenablog.jp/entry/2014/04/01/0930
- 
+
  Used problems:
   - https://github.com/hiroshi-maybe/topcoder/blob/3b1f63c748035eae69519db69cc95d875550cd23/solutions/SpeedingUpBozosort/SpeedingUpBozosort.cpp#L42
- 
+
  */
 struct GaussJordanElimination {
   const double eps=1e-8;
@@ -241,7 +286,7 @@ struct GaussJordanElimination {
       for(int j=0; j<N; ++j) X[i][j] = A[i][j];
       X[i][N] = b[i];
     }
-    
+
     for(int i=0; i<N; ++i) {
       // invariant: X[p][p]=1 for p=0..i-1
       int pivot=i;
@@ -252,7 +297,7 @@ struct GaussJordanElimination {
       swap(X[i], X[pivot]);
       // solution is undeterministic, or no solution exists
       if (fabs(X[i][i])<eps) return vector<double>();
-      
+
       // X[i][i]=1
       for(int j=i+1; j<=N; ++j) X[i][j]/=X[i][i];
       for(int j=0; j<N; ++j) if (i != j) {
@@ -260,7 +305,7 @@ struct GaussJordanElimination {
         for(int k=i+1; k<=N; ++k) X[j][k]-=X[j][i]*X[i][k];
       }
     }
-    
+
     vector<double> xs(N);
     for(int i=0; i<N; ++i) xs[i]=X[i][N];
     return xs;
@@ -270,21 +315,9 @@ struct GaussJordanElimination {
 int main(int argc, char const *argv[]) {
   vector<int> V = { 534, 251, 76, 468, 909, 410, 264, 387, 102, 982, 199, 111, 659, 386, 151 };
   assert(gf2_rank<int>(V)==10);
-  
-  assert(MLE._modinv(5,11)==9);
-  int x,y;
-  int d=MLE._extgcd(99,78,x,y);
-  assert(d==3);
-  assert(x==-11);
-  assert(y==14);
-  
-  vector<int> A={1,1,1};
-  vector<int> B={2,3,2};
-  vector<int> M={3,5,7};
-  auto p = MLE.solve(A,B,M);
-  assert(p.first==23);
-  assert(p.second==105);
-  
+
+  test_MLE();
+
   // test case from https://en.wikipedia.org/wiki/Gaussian_elimination
   auto xs = GJE.solve({
     {2,1,-1},
@@ -299,3 +332,5 @@ int main(int argc, char const *argv[]) {
     assert(fabs(xs[i]-sols[i])<1e-9);
   }
 }
+
+//$ g++ -std=c++17 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address equations.cpp && ./a.out
--- lib/gcd.cpp
+++ lib/gcd.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <cassert>
-#include <tuple>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long LL;
@@ -17,43 +13,75 @@ void test_gcd() {
 }
 
 /*
- 
- The extended form of Euclid’s algorithm
- 
- computes (gcd,x,y) s.t. d=gcd(a,b)=a*x+b*y, O(lg max(a,b)) time
- 
+
+ The extended form of Euclid’s algorithm, O(lg max(a,b)) time
+
+ computes (gcd,x,y) s.t. g=gcd(a,b)=a*x+b*y
+
    - From CLRS 31.2 Greatest common divisor
    - If gcd(a,b)=1, x=a^-1(mod b) (mod inverse of a in mod b) because a*x≡1(mod b)
      - this is useful to solve modular equation a*x≡b(mod m)
      - see CLRS 31.4 and `lib/equations.cpp`
- 
+
  Proof:
- 
- (dd,xx,yy) = gcde(a,b)
- 
-   d = dd
+
+ (gg,xx,yy) = gcde(a,b)
+
+   g = gg
    = b*xx+(a%B)*yy
    = b*xx+(a-b*floor(a/b))*yy
    = a*yy + b*(xx-floor(a/b)*yy)
        ^      ^
- Thus return (d,yy,xx-floor(a/b)*yy)
- 
+ Thus return (g,yy,xx-floor(a/b)*yy)
+
  Usage:
 
-   int d,x,y;
-   auto t = extended_euclid(899,493);
-   tie(d,x,y) = t; // (d,x,y)=(29,-6,11)
- 
+   int g,x,y;
+   tie (g,x,y) = extended_euclid(899,493);
+   // (g,x,y)=(29,-6,11) s.t. 29 = -6*899 + 11*493
+
  Used problems:
   - https://github.com/hiroshi-maybe/codeforces/blob/master/solutions/InfiniteFence.cpp#L64
- 
+  - https://github.com/hiroshi-maybe/atcoder/blob/4800c7aa222aee403acd9b0012ee94e203116461/solutions/Oversleeping.cpp#L123
+
  */
-tuple<int,int,int> extended_euclid(int a, int b) {
+
+template<typename integer>
+tuple<integer,integer,integer> extended_euclid(integer a, integer b) {
   if (b==0) return make_tuple(a, 1, 0);
-  
-  int d2,x2,y2;
+
+  integer d2,x2,y2;
   tie(d2,x2,y2) = extended_euclid(b, a%b);
   return make_tuple(d2,y2,x2-(a/b)*y2);
+}
+
+/*
+
+  Solver of two equations for Chinese Remainder Theorem, O(lg max(n1,n2)) time
+
+  Constructs (x, M) s.t. x ≡ a1 (mod n1), x ≡ a2 (mod n2)
+
+  Returns (0,-1) if no solutions are found
+  ( e.g. a1 (mod gcd(n1,n2)) !≡ a2 (mod gcd(n1,n2)) )
+
+  https://qiita.com/drken/items/ae02240cd1f8edfc86fd
+
+  Use https://github.com/hiroshi-maybe/topcoder/blob/master/lib/equations.cpp#L73 for more than two equations.
+
+  Used problems:
+   - https://github.com/hiroshi-maybe/atcoder/blob/4800c7aa222aee403acd9b0012ee94e203116461/solutions/Oversleeping.cpp#L132
+
+ */
+template<typename integer>
+pair<integer,integer> crt2(integer a1, integer n1, integer a2, integer n2) {
+  integer g,x,y; tie(g,x,y)=extended_euclid(n1,n2);
+  if(a1%g!=a2%g) return {0,-1};
+  integer lcm=n1/g*n2;
+
+  integer res=a1+(a2-a1)/g*x%(n2/g)*n1;
+  res=(res%lcm+lcm)%lcm;
+
+  return {res,lcm};
 }
 
 void test_extendedeuclid() {
@@ -63,11 +91,40 @@ void test_extendedeuclid() {
   assert(get<2>(t)==11);
 }
 
-// main
+void test_crt2() {
+  {
+    int a1=2, n1=3, a2=3, n2=5;
+    auto [x,g]=crt2(a1,n1,a2,n2);
+    assert(x==8);
+    assert(g==1);
+  }
+
+  {
+    LL a1=2698054, n1=7949779, a2=5550023, n2=7970645;
+    auto [x,g]=crt2(a1,n1,a2,n2);
+    assert(x==35027849890893);
+    assert(g==1);
+  }
+
+  {
+    LL a1=1350427, n1=4250955, a2=3475255, n2=4164018;
+    auto [x,g]=crt2(a1,n1,a2,n2);
+    assert(x==4992365411977);
+    assert(g==3);
+  }
+
+  {
+    LL a1=4, n1=6, a2=8, n2=15;
+    auto [x,g]=crt2(a1,n1,a2,n2);
+    assert(x==0);
+    assert(g==-1);
+  }
+}
 
 int main(int argc, char const *argv[]) {
   test_gcd();
   test_extendedeuclid();
+  test_crt2();
 }
 
-//$ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address gcd.cpp && ./a.out
+//$ g++ -std=c++17 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address gcd.cpp && ./a.out
