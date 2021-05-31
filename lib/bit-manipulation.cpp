d063a599fc0047ca9176ca98de77d438f81081a2
Hiroshi Kori <hiroshi.maybe@gmail.com>
2021-05-31 13:56:53 -0700

    Used fast zeta transform in CF

--- lib/bit-manipulation.cpp
+++ lib/bit-manipulation.cpp
@@ -5,20 +5,18 @@ using namespace std;
 
 // $ g++ -std=c++14 -Wall -O2 -D_GLIBCXX_DEBUG -fsanitize=address bit-manipulation.cpp && ./a.out
 
-int f[1<<20];
-
 /*
- 
+
  Compute  g(s) = ∑ { f(t) : t∈T⊆S (or T⊇s) } for ∀s∈S in O(N*2^N) time
- 
+
  Suppose there is a set S (|S|=N) and subsets T[0],..,T[2^N-1] and we already know f(T[0]),..,f(T[2^n-1]).
  Then fast zeta transform computes g(T[i]) = ∑{f(U),T[i]⊆U}.
   Note that actually computation does not need to be summation ∑ if it's ring (commutative). max, *, gcd works too
- 
+
  Input: f[i] has f(T[i]) where T[i] is subset mask of S
  Output: f[i] = g(T[i]) by summation
  O(N*2^N) time
- 
+
  References:
   - https://topcoder.g.hatena.ne.jp/iwiwi/20120422/1335065228
    - most famous article
@@ -31,12 +29,15 @@ int f[1<<20];
   - http://naoyat.hatenablog.jp/entry/zeta-moebius
    - links
    - clear example of mebius transform with inclusion exclusion principle
- 
+
  Used problems:
   - https://github.com/k-ori/atcoder/blob/master/solutions/OrPlusMax.cpp#L115
   - https://github.com/hiroshi-maybe/leetcode/blob/master/982-triples-with-bitwise-and-equal-to-zero/triples-with-bitwise-and-equal-to-zero.cpp#L62
- 
+  - https://github.com/hiroshi-maybe/codeforces/blob/84ca93b0581bbbec498f8e9658f2782c0e477d80/solutions/LoveHate.cpp#L53
+
  */
+
+int f[1<<20];
 // g(S)=∑f(T):S⊆T
 void ztransform_superset(int N) {
   for(int i=0; i<N; ++i) {
@@ -145,9 +146,9 @@ long long bin2n(string s) {
 }
 
 /*
- 
+
  GCC built-in libraries:
- 
+
   __builtin_popcount(mask) // count number of ON bit
   __builtin_ctz(mask)      // count number of trailing zeros
   __builtin_clz(mask)      // count number of leading zeros
@@ -155,26 +156,26 @@ long long bin2n(string s) {
  __builtin_popcountll(mask)
  __builtin_ctzll(mask)
  __builtin_clzll(mask)
- 
+
  */
 
 /***********************   test code below   ***********************/
 
 int main(int argc, char const *argv[]) {
   test_ztransform();
-  
+
   vector<int> exp3 = {26, 24, 18, 16, 10, 8, 2};
   vector<int> S = submasks(26);
   assert(S.size()==exp3.size());
   for(int i=0; i<S.size(); ++i) assert(exp3[i]==S[i]);
-  
+
   assert(isPowerOfTwo(32));
   assert(!isPowerOfTwo(31));
-  
+
   assert(unsetLSB(26)==24);
   assert(getLSB(26)==2);
   assert(1LL<<__builtin_ctzll(26)==2);
-  
+
   assert(n2bin(2384185791015625LL)=="1000011110000110011110000011001001101110101011001001");
   assert(bin2n("1000011110000110011110000011001001101110101011001001")==2384185791015625LL);
 }
